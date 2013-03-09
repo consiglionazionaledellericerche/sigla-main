316ca35c1d0ed25ceb2f80beb1bb8953db0c3da4
mspasiano <mspasiano@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2013-03-09 17:44:13 +0000

    Gestione delle Contabili BNL

--- sigla-web/src/main/java/it/cnr/contab/doccont00/consultazioni/action/ConsStatoInvioMandatiAction.java
+++ sigla-web/src/main/java/it/cnr/contab/doccont00/consultazioni/action/ConsStatoInvioMandatiAction.java
@@ -3,6 +3,7 @@ package it.cnr.contab.doccont00.consultazioni.action;
 import it.cnr.contab.doccont00.consultazioni.bp.ConsStatoInvioMandatiBP;
 import it.cnr.contab.doccont00.consultazioni.bulk.V_cons_stato_invio_mandatiBulk;
 import it.cnr.jada.action.ActionContext;
+import it.cnr.jada.action.BusinessProcessException;
 import it.cnr.jada.action.Forward;
 import it.cnr.jada.util.action.ConsultazioniAction;
 
@@ -15,8 +16,12 @@ public class ConsStatoInvioMandatiAction extends ConsultazioniAction {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public ConsStatoInvioMandatiAction() {
+		super();
+	}
+
 	@SuppressWarnings("unchecked")
-	public Forward doVisualizzaContabili(ActionContext context) {
+	public Forward doVisualizzaContabili(ActionContext context) throws BusinessProcessException{
 		try {
 			ConsStatoInvioMandatiBP bp = (ConsStatoInvioMandatiBP)context.getBusinessProcess();
 			if (bp.getSelection() != null)
