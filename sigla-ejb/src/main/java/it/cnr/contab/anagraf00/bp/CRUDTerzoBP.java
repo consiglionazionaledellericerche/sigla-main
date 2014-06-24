5f700601581783d0f2a6ca73bb794af8018908e8
gianfranco.gasparro <gianfranco.gasparro@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2014-06-24 13:02:31 +0000

    Fatturazione Elettronica: Corretta anomalia sul controllo per la fatturazione elettronica al salvataggio del terzo

--- sigla-ejb/src/main/java/it/cnr/contab/anagraf00/bp/CRUDTerzoBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/anagraf00/bp/CRUDTerzoBP.java
@@ -175,7 +175,7 @@ public class CRUDTerzoBP extends SimpleCRUDBP {
 		super.save(actioncontext);
 		TerzoBulk terzo = (TerzoBulk) getModel();
 		
-		if (terzo != null && terzo.getCodiceUnivocoUfficioIpa() == null && terzo.getAnagrafico() != null && !terzo.getAnagrafico().getDataAvvioFattElettr().after(it.cnr.jada.util.ejb.EJBCommonServices.getServerDate())){
+		if (terzo != null && terzo.getCodiceUnivocoUfficioIpa() == null && terzo.getAnagrafico() != null && terzo.getAnagrafico().getDataAvvioFattElettr() != null && !terzo.getAnagrafico().getDataAvvioFattElettr().after(it.cnr.jada.util.ejb.EJBCommonServices.getServerDate())){
 			setMessage("Attenzione! L'anagrafica è censita nell'indice delle pubbliche amministrazioni, richiedere tramite helpdesk l'inserimento del codice IPA per questo terzo "+terzo.getCd_terzo()+". Salvataggio eseguito in modo corretto.");
 		}
 	}
