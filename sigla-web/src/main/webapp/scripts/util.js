978a90b38e375e0e981289d15045999aa97edea1
mspasiano <mspasiano@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2014-06-27 09:00:00 +0000

    BUG sulla lista dei messaggi non chiudeva mail il BP

--- sigla-web/src/main/webapp/scripts/util.js
+++ sigla-web/src/main/webapp/scripts/util.js
@@ -10,7 +10,7 @@ document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/themes/alp
 function apriListaMessaggi(url) {
 	var winWidth= (screen.width*50)/100;
 	var winHeight= (screen.height*50)/100;
-    var win = new Window({className:"alphacube", width:winWidth, height:winHeight, title: "Messaggi",url: url, destroyOnClose:true, showEffect:Effect.BlindDown, hideEffect: Effect.SwitchOff, draggable:true, wiredDrag: true, onClose: function() { 
+    var win = new Window({className:"alphacube", width:winWidth, height:winHeight, title: "Messaggi",url: url, destroyOnClose:true, showEffect:Effect.BlindDown, hideEffect: Effect.SwitchOff, draggable:true, wiredDrag: true, onClose: function(event) { 
     	event.getContent().contentDocument.mainForm.comando.value = 'doCloseForm';
   		event.getContent().contentDocument.mainForm.submit();
 	}});
