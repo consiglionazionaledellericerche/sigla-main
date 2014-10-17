793c7d405908d08396e3036409d8d05935a4bbbb
rpucciarelli <rpucciarelli@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2014-10-17 09:58:37 +0000

    BugID:1340 Certificazione dei crediti - Eliminata possibilità di non inserire obbligatoriamente l'inventariazione nel caso di Note credito ........

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/comp/FatturaPassivaComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/comp/FatturaPassivaComponent.java
@@ -2760,10 +2760,13 @@ public OggettoBulk creaConBulk(
 	
 	try {
 		if (fattura_passiva instanceof Fattura_passiva_IBulk ||fattura_passiva instanceof Nota_di_creditoBulk ) {
-			if (fattura_passiva.existARowToBeInventoried()) {
+			if (fattura_passiva.existARowToBeInventoried()) { 
 				verificaEsistenzaEdAperturaInventario(userContext, fattura_passiva);
-				if (hasFatturaPassivaARowNotInventoried(userContext, fattura_passiva) && (fattura_passiva.getStato_liquidazione()==null || fattura_passiva.getStato_liquidazione().compareTo(Fattura_passiva_IBulk.LIQ)==0))
+				if (fattura_passiva instanceof Fattura_passiva_IBulk  && hasFatturaPassivaARowNotInventoried(userContext, fattura_passiva) &&
+					(fattura_passiva.getStato_liquidazione()==null || fattura_passiva.getStato_liquidazione().compareTo(Fattura_passiva_IBulk.LIQ)==0))
 						throw new it.cnr.jada.comp.ApplicationException("Attenzione: è necessario inventariare tutti i dettagli.");
+				if (fattura_passiva instanceof Nota_di_creditoBulk  && hasFatturaPassivaARowNotInventoried(userContext, fattura_passiva))
+							throw new it.cnr.jada.comp.ApplicationException("Attenzione: è necessario inventariare tutti i dettagli.");	
 			}
 		}
 		validaFattura(userContext, fattura_passiva);
