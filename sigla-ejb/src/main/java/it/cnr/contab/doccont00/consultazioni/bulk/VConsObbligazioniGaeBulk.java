80521f533d3835bde435b3ae9e94a60b620a0c5d
gianfranco.gasparro <gianfranco.gasparro@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2015-01-29 14:10:04 +0000

    BugID: 1358 Creati e/o modificati una serie di Servizi REST per Integrazione con Gestione Missioni

---
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/VConsObbligazioniGaeBulk.java
@@ -0,0 +1,21 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 27/01/2015
+ */
+package it.cnr.contab.doccont00.consultazioni.bulk;
+public class VConsObbligazioniGaeBulk extends VConsObbligazioniGaeBase {
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CONS_OBBLIGAZIONI
+	 **/
+	public VConsObbligazioniGaeBulk() {
+		super();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CONS_OBBLIGAZIONI
+	 **/
+	public VConsObbligazioniGaeBulk(java.lang.String cdCds, java.lang.String cdLineaAttivita, java.lang.Integer esercizio, java.lang.Integer esercizioOriginale, java.lang.Long pgObbligazione) {
+		super(cdCds, cdLineaAttivita, esercizio, esercizioOriginale, pgObbligazione);
+	}
+}
