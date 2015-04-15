17998ba7159c1e814b5564c12a42462046e7fdfb
mspasiano <mspasiano@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2015-04-15 10:21:48 +0000

    BUG sulla durata della transazione non chiamava il BP in modalita transazionale

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/actions/CRUDFatturaPassivaElettronicaAction.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/actions/CRUDFatturaPassivaElettronicaAction.java
@@ -271,7 +271,7 @@ public class CRUDFatturaPassivaElettronicaAction extends CRUDAction {
 			CRUDFatturaPassivaBP nbp = null;
 			try {
 				nbp = (CRUDFatturaPassivaBP)context.createBusinessProcess("CRUDFatturaPassivaBP",
-								new Object[] {"M"}
+								new Object[] {"Tr"}
 							);
 				String mode = it.cnr.contab.utenze00.action.GestioneUtenteAction.getComponentSession().
 						validaBPPerUtente(context.getUserContext(),((CNRUserInfo)context.getUserInfo()).getUtente(),
