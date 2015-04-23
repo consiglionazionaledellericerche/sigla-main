e3072b695150b9423abac87e07b0173e2c19e83b
mspasiano <mspasiano@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2015-04-23 11:46:34 +0000

    BUG enconding UTF-8

--- sigla-web/src/main/java/it/cnr/contab/util/servlet/RESTServlet.java
+++ sigla-web/src/main/java/it/cnr/contab/util/servlet/RESTServlet.java
@@ -5,6 +5,7 @@ import it.cnr.contab.config00.ejb.Unita_organizzativaComponentSession;
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
 import it.cnr.contab.utente00.nav.ejb.GestioneLoginComponentSession;
 import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.contab.utenze00.bp.RESTUserContext;
 import it.cnr.contab.utenze00.bulk.CNRUserInfo;
 import it.cnr.contab.utenze00.bulk.UtenteBulk;
 import it.cnr.contab.util.servlet.JSONRequest.Clause;
@@ -28,9 +29,13 @@ import it.cnr.jada.util.action.ConsultazioniBP;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import javax.ejb.EJBException;
@@ -40,8 +45,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
@@ -52,7 +56,7 @@ public class RESTServlet extends HttpServlet{
     private File actionDirFile;    
     private ActionMappings mappings;
     private String COMMAND_POST = "doRestResponse", COMMAND_GET = "doRestInfo";
-    private transient final static Logger logger = LoggerFactory.getLogger(RESTServlet.class);
+	private static final Logger logger = Logger.getLogger(RESTServlet.class);
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -68,6 +72,7 @@ public class RESTServlet extends HttpServlet{
 
 	protected void execute(HttpServletRequest req, HttpServletResponse resp, String command)
 			throws ServletException, IOException {
+		resp.setContentType("application/json");
         String s = req.getServletPath();
         String extension = s.substring(s.lastIndexOf("."));
         if(!restExtension.contains(extension))
@@ -77,17 +82,23 @@ public class RESTServlet extends HttpServlet{
         ActionMapping actionmapping = mappings.findActionMapping(s);
         if(actionmapping == null)
             throw new ServletException("Action not found ["+s+"]");
-        actionmapping.setNeedExistingSession(Boolean.FALSE.toString());
+        UtenteBulk utente = null;
 		try {
-			UtenteBulk utente = authenticate(req, resp);
-			if (utente != null) {
+			if (actionmapping.needExistingSession())
+				utente = authenticate(req, resp);
+			if (utente != null || !actionmapping.needExistingSession()) {
 				JSONRequest jsonRequest = null;
 	            HttpActionContext httpactioncontext = new HttpActionContext(this, req, resp);
 	            httpactioncontext.setActionMapping(actionmapping);
 	            if (command.equals(COMMAND_POST)) {
 	            	jsonRequest = new Gson().fromJson(new JsonParser().parse(req.getReader()), JSONRequest.class);
-		            httpactioncontext.setUserContext(getContextFromRequest(jsonRequest, utente.getCd_utente(),httpactioncontext.getSessionId(), req));
-		            httpactioncontext.setUserInfo(getUserInfo(utente, (CNRUserContext) httpactioncontext.getUserContext()));	            	
+	            	if (actionmapping.needExistingSession()) {
+			            httpactioncontext.setUserContext(getContextFromRequest(jsonRequest, utente.getCd_utente(), httpactioncontext.getSessionId(), req));
+			            httpactioncontext.setUserInfo(getUserInfo(utente, (CNRUserContext) httpactioncontext.getUserContext()));	            		            		
+	            	} else {
+	            		httpactioncontext.setUserContext(new RESTUserContext());
+			            httpactioncontext.setUserInfo(getUserInfo(utente, (CNRUserContext) httpactioncontext.getUserContext()));
+	            	}
 	            }
 	            try{
 	            	BusinessProcess businessProcess;
@@ -96,15 +107,17 @@ public class RESTServlet extends HttpServlet{
 	            	else
 	            		businessProcess = mappings.createBusinessProcess(actionmapping, httpactioncontext);
 	            	if (command.equals(COMMAND_POST)) {
-		            	String mode = loginComponentSession().validaBPPerUtente(httpactioncontext.getUserContext(), utente, 
+	            		Boolean isEnableBP = false;
+	            		if (actionmapping.needExistingSession())
+	            			isEnableBP = loginComponentSession().isBPEnableForUser(httpactioncontext.getUserContext(), utente, 
 		            			CNRUserContext.getCd_unita_organizzativa(httpactioncontext.getUserContext()), businessProcess.getName());
-		            	if (mode == null || !(businessProcess instanceof ConsultazioniBP)) {
+		            	if ((actionmapping.needExistingSession() && !isEnableBP) || !(businessProcess instanceof ConsultazioniBP)) {
 		            		resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		        			resp.getWriter().append("Utente non abilitato ad eseguire la richiesta!");
+		        			resp.getWriter().append("{\"message\" : \"Utente non abilitato ad eseguire la richiesta!\"}");
 		        			return;
 		            	}
 		            	ConsultazioniBP consBP = ((ConsultazioniBP)businessProcess);
-		        		if (jsonRequest.getClauses() != null) {
+		        		if (jsonRequest != null && jsonRequest.getClauses() != null) {
 		        			CompoundFindClause compoundFindClause = new CompoundFindClause();
 		        			for (Clause clause : jsonRequest.getClauses()) {
 		        				compoundFindClause.addClause(clause.getCondition(), clause.getFieldName(), clause.getSQLOperator(), clause.getFieldValue());
@@ -115,9 +128,9 @@ public class RESTServlet extends HttpServlet{
 		            			consBP.search(httpactioncontext,
 		            			consBP.getFindclause(),
 		        				(OggettoBulk) consBP.getBulkInfo().getBulkClass().newInstance()));
-		            	parseRequestParameter(httpactioncontext, jsonRequest, consBP);
-		            	
+		            	parseRequestParameter(httpactioncontext, jsonRequest, consBP);		            	
 	            	}
+	            	httpactioncontext.setBusinessProcess(businessProcess);
 	                req.setAttribute(it.cnr.jada.action.BusinessProcess.class.getName(), businessProcess);
 	            	httpactioncontext.perform(null, actionmapping, command);
 	            }catch(ActionPerformingError actionperformingerror)        {
@@ -138,25 +151,38 @@ public class RESTServlet extends HttpServlet{
 			}
 		} catch (ComponentException e) {
 			logger.error("ComponentException", e);
-			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			resp.getWriter().append(e.getClass().getName() + " - " + e.getMessage());
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);			
+			Gson gson = new Gson();			
+		    Map<String, String> exc_map = new HashMap<String, String>();
+		    exc_map.put("message", e.toString());
+		    exc_map.put("stacktrace", getStackTrace(e));		    
+			resp.getWriter().append(gson.toJson(exc_map));
 		}
 	}
-
+	
+	private String getStackTrace(final Throwable throwable) {
+		final StringWriter sw = new StringWriter();
+		final PrintWriter pw = new PrintWriter(sw, true);
+		throwable.printStackTrace(pw);
+		return sw.getBuffer().toString();
+	}
+	
 	private void parseRequestParameter(HttpActionContext actioncontext,
 			JSONRequest jsonRequest, ConsultazioniBP consBP) throws RemoteException, BusinessProcessException {
-		if (jsonRequest.getMaxItemsPerPage() != null){
-    		consBP.setPageSize(jsonRequest.getMaxItemsPerPage());
-    		consBP.refresh(actioncontext);
-		}
-		if (jsonRequest.getActivePage() != null)
-    		consBP.goToPage(actioncontext, jsonRequest.getActivePage());
-		if (jsonRequest.getOrderBy() != null) {
-			for (OrderBy orderBy : jsonRequest.getOrderBy()) {
-				consBP.setOrderBy(actioncontext, orderBy.name, 
-						orderBy.getType() == null || orderBy.getType().equalsIgnoreCase("ASC")?OrderConstants.ORDER_ASC: OrderConstants.ORDER_DESC);
+		if (jsonRequest != null) {			
+			if (jsonRequest.getMaxItemsPerPage() != null){
+	    		consBP.setPageSize(jsonRequest.getMaxItemsPerPage());
+	    		consBP.refresh(actioncontext);
 			}
-		}
+			if (jsonRequest.getActivePage() != null)
+	    		consBP.goToPage(actioncontext, jsonRequest.getActivePage());
+			if (jsonRequest.getOrderBy() != null) {
+				for (OrderBy orderBy : jsonRequest.getOrderBy()) {
+					consBP.setOrderBy(actioncontext, orderBy.name, 
+							orderBy.getType() == null || orderBy.getType().equalsIgnoreCase("ASC")?OrderConstants.ORDER_ASC: OrderConstants.ORDER_DESC);
+				}
+			}			
+		}		
 	}
 
 	private UserInfo getUserInfo(UtenteBulk utente, CNRUserContext userContext) throws ComponentException {
@@ -164,7 +190,8 @@ public class RESTServlet extends HttpServlet{
 		cnrUserInfo.setUtente(utente);
 		try {
 			cnrUserInfo.setUnita_organizzativa((Unita_organizzativaBulk) uoComponentSession().findUOByCodice(userContext, userContext.getCd_unita_organizzativa()));
-			cnrUserInfo.setCdr(loginComponentSession().cdrDaUo(userContext, cnrUserInfo.getUnita_organizzativa()));
+			if (cnrUserInfo.getUnita_organizzativa() != null)
+				cnrUserInfo.setCdr(loginComponentSession().cdrDaUo(userContext, cnrUserInfo.getUnita_organizzativa()));
 		} catch (RemoteException e) {
 			throw new ComponentException(e);
 		} catch (EJBException e) {
@@ -173,20 +200,22 @@ public class RESTServlet extends HttpServlet{
 		return cnrUserInfo;
 	}
 	private CNRUserContext getContextFromRequest(JSONRequest jsonRequest, String user, String sessionId, HttpServletRequest req) throws IOException, ApplicationException {
-		if (jsonRequest.getContext() == null)
-			throw new ApplicationException("Il contesto utente non puo essere vuoto");
-		if (jsonRequest.getContext().getEsercizio() == null)
-			throw new ApplicationException("Esercizio non puo essere vuoto");
-		if (jsonRequest.getContext().getCd_cds() == null)
-			throw new ApplicationException("Il codice del CdS non puo essere vuoto");
-		if (jsonRequest.getContext().getCd_unita_organizzativa() == null)
-			throw new ApplicationException("Il codice della UO non puo essere vuoto");
-		if (jsonRequest.getContext().getCd_cdr() == null)
-			throw new ApplicationException("Il codice del CdR non puo essere vuoto");
-		return new CNRUserContext(user, sessionId, jsonRequest.getContext().getEsercizio(), 
-				jsonRequest.getContext().getCd_unita_organizzativa(), 
-				jsonRequest.getContext().getCd_cds(), 
-				jsonRequest.getContext().getCd_cdr());
+		if (jsonRequest.getContext() != null) {
+			if (jsonRequest.getContext().getEsercizio() == null)
+				throw new ApplicationException("Esercizio non puo essere vuoto");
+			if (jsonRequest.getContext().getCd_cds() == null)
+				throw new ApplicationException("Il codice del CdS non puo essere vuoto");
+			if (jsonRequest.getContext().getCd_unita_organizzativa() == null)
+				throw new ApplicationException("Il codice della UO non puo essere vuoto");
+			if (jsonRequest.getContext().getCd_cdr() == null)
+				throw new ApplicationException("Il codice del CdR non puo essere vuoto");
+			return new CNRUserContext(user, sessionId, jsonRequest.getContext().getEsercizio(), 
+					jsonRequest.getContext().getCd_unita_organizzativa(), 
+					jsonRequest.getContext().getCd_cds(), 
+					jsonRequest.getContext().getCd_cdr());			
+		} else {
+			return new RESTUserContext();
+		}
 	}
     
 	@Override
@@ -209,7 +238,6 @@ public class RESTServlet extends HttpServlet{
             throw new ServletException("Action mappings configuration exception", actionmappingsconfigurationexception);
         }		
 	}
-
 	public UtenteBulk authenticate(HttpServletRequest req, HttpServletResponse res) throws ComponentException{
         boolean authorized = false;
 		UtenteBulk utente = new UtenteBulk();
@@ -253,11 +281,11 @@ public class RESTServlet extends HttpServlet{
             if (logger.isDebugEnabled())
             	logger.debug("Requesting authorization credentials");
             res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            res.setHeader("WWW-Authenticate", "Basic realm=\"SIGLA\"");               	
+            res.setHeader("WWW-Authenticate", "Basic realm=\"SIGLA\"");   
+            return null;
         }
         return utente;
 	}
-
 	public static GestioneLoginComponentSession loginComponentSession() throws javax.ejb.EJBException, java.rmi.RemoteException {
 		return (GestioneLoginComponentSession)it.cnr.jada.util.ejb.EJBCommonServices.createEJB("CNRUTENZE00_NAV_EJB_GestioneLoginComponentSession");
 	}
