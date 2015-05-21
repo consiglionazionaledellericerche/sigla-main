08390960373c49f783ca3826c5c1582ea4fe7600
gianfranco.gasparro <gianfranco.gasparro@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2015-05-21 07:10:14 +0000

    Corretta anomalia nel recupero del pdf della fattura elettronica nel caso di nota di credito. Aggiunto il controllo nel caso di generazione di file xml vuoto

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/ejb/FatturaAttivaSingolaComponentSession.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/ejb/FatturaAttivaSingolaComponentSession.java
@@ -14,6 +14,8 @@ import java.util.Date;
 
 import javax.ejb.Remote;
 
+import org.apache.chemistry.opencmis.client.api.Document;
+
 @Remote
 public interface FatturaAttivaSingolaComponentSession extends it.cnr.contab.docamm00.comp.DocumentoAmministrativoComponentSession, it.cnr.jada.ejb.CRUDComponentSession {
 it.cnr.contab.docamm00.docs.bulk.Nota_di_debito_attivaBulk addebitaDettagli(it.cnr.jada.UserContext param0,it.cnr.contab.docamm00.docs.bulk.Nota_di_debito_attivaBulk param1,java.util.List param2,java.util.Hashtable param3) throws it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
@@ -75,7 +77,7 @@ it.cnr.jada.bulk.OggettoBulk rebuildDocumento(it.cnr.jada.UserContext param0, it
 it.cnr.jada.util.RemoteIterator cercaObbligazioni(it.cnr.jada.UserContext param0,it.cnr.contab.docamm00.docs.bulk.Filtro_ricerca_obbligazioniVBulk param1) throws it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
 byte[] lanciaStampa(it.cnr.jada.UserContext userContext,java.lang.Long pg_stampa) throws PersistencyException, ComponentException,java.rmi.RemoteException;
 it.cnr.jada.bulk.OggettoBulk completaOggetto(it.cnr.jada.UserContext aUC,it.cnr.jada.bulk.OggettoBulk oggetto) throws PersistencyException,it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
-void gestioneAllegatiPerFatturazioneElettronica(it.cnr.jada.UserContext aUC,it.cnr.contab.docamm00.docs.bulk.Fattura_attivaBulk fatturaAttiva)throws PersistencyException,it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
+Document gestioneAllegatiPerFatturazioneElettronica(it.cnr.jada.UserContext aUC,it.cnr.contab.docamm00.docs.bulk.Fattura_attivaBulk fatturaAttiva)throws PersistencyException,it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
 boolean VerificaDuplicati(it.cnr.jada.UserContext aUC,it.cnr.contab.docamm00.docs.bulk.Fattura_attivaBulk fatturaAttiva)throws PersistencyException,it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
 java.util.List findListaModalitaPagamentoWS(it.cnr.jada.UserContext userContext,String terzo,String query,String dominio,String tipoRicerca)throws ComponentException,java.rmi.RemoteException;
 java.util.List findListaBancheWS(it.cnr.jada.UserContext userContext,String terzo,String modalita,String query,String dominio,String tipoRicerca)throws ComponentException,java.rmi.RemoteException;
