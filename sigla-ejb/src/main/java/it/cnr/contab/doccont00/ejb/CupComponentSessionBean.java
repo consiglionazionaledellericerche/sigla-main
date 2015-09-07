c999cfed71f488c790de4f8844a6516b93659f16
rpucciarelli <rpucciarelli@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2015-09-07 15:32:40 +0000

    Lancio nuova stampa dalla mappa di anagrafica dei Cup

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/CupComponentSessionBean.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/CupComponentSessionBean.java
@@ -1,8 +1,13 @@
 package it.cnr.contab.doccont00.ejb;
+import java.rmi.RemoteException;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-
 import it.cnr.contab.doccont00.comp.CupComponent;
+import it.cnr.jada.UserContext;
+import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.IntrospectionException;
+import it.cnr.jada.persistency.PersistencyException;
 @Stateless(name="CNRDOCCONT00_EJB_CupComponentSession")
 public class CupComponentSessionBean extends it.cnr.jada.ejb.CRUDComponentSessionBean implements CupComponentSession {
 @PostConstruct
@@ -12,5 +17,23 @@ public class CupComponentSessionBean extends it.cnr.jada.ejb.CRUDComponentSessio
 public static it.cnr.jada.ejb.CRUDComponentSessionBean newInstance() throws javax.ejb.EJBException {
 	return new CupComponentSessionBean();
 }
+public String recuperoCds(UserContext param0) throws ComponentException,
+		PersistencyException, IntrospectionException, RemoteException {
+	pre_component_invocation(param0,componentObj);
+	try {
+		String result = ((CupComponent)componentObj).recuperoCds(param0);
+		component_invocation_succes(param0,componentObj);
+		return result;
+	} catch(it.cnr.jada.comp.NoRollbackException e) {
+		component_invocation_succes(param0,componentObj);
+		throw e;
+	} catch(it.cnr.jada.comp.ComponentException e) {
+		component_invocation_failure(param0,componentObj);
+		throw e;
+	} catch(RuntimeException e) {
+		throw uncaughtRuntimeException(param0,componentObj,e);
+	} catch(Error e) {
+		throw uncaughtError(param0,componentObj,e);
+	}}
 
 }
