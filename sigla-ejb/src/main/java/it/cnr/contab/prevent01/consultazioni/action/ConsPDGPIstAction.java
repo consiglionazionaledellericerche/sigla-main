163bb22ecce3020aba18b3313f19e8696c2ead1e
mspasiano <mspasiano@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2016-01-16 16:23:33 +0000

    BUG memory leak sulle connessioni Chiudere sempre un iteratore remoto prima di aprirne uno nuovo

--- sigla-ejb/src/main/java/it/cnr/contab/prevent01/consultazioni/action/ConsPDGPIstAction.java
+++ sigla-ejb/src/main/java/it/cnr/contab/prevent01/consultazioni/action/ConsPDGPIstAction.java
@@ -6,24 +6,17 @@
  */
 package it.cnr.contab.prevent01.consultazioni.action;
 
-import java.rmi.RemoteException;
-import java.util.Iterator;
-
-import it.cnr.contab.prevent01.consultazioni.bp.*;
+import it.cnr.contab.prevent01.consultazioni.bp.ConsPDGPIstBP;
+import it.cnr.contab.prevent01.consultazioni.bp.ConsPDGPIstEtrBP;
+import it.cnr.contab.prevent01.consultazioni.bp.ConsPDGPIstSpeBP;
 import it.cnr.jada.action.ActionContext;
 import it.cnr.jada.action.BusinessProcessException;
 import it.cnr.jada.action.Forward;
-import it.cnr.jada.bulk.OggettoBulk;
-import it.cnr.jada.persistency.sql.CompoundFindClause;
-import it.cnr.jada.persistency.sql.SQLBuilder;
-import it.cnr.jada.util.RemoteIterator;
-import it.cnr.jada.util.action.BulkBP;
 import it.cnr.jada.util.action.ConsultazioniAction;
-import it.cnr.jada.util.action.ConsultazioniBP;
-import it.cnr.jada.util.ejb.EJBCommonServices;
 
 
 public class ConsPDGPIstAction extends ConsultazioniAction {
+	private static final long serialVersionUID = 1L;
 
 	public Forward doConsulta(ActionContext context, String livelloDestinazione) {
 		try {
@@ -39,6 +32,7 @@ public class ConsPDGPIstAction extends ConsultazioniAction {
 				return context.findDefaultForward();
 			}
 			ConsPDGPIstBP consultazioneBP = null;
+			context.closeBusinessProcess(bp);
 			if (bp instanceof ConsPDGPIstSpeBP) 
 				consultazioneBP = (ConsPDGPIstSpeBP)context.createBusinessProcess("ConsPDGPIstSpeBP");
 			else
