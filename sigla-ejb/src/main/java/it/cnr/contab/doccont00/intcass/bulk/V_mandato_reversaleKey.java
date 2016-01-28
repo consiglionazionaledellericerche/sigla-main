ce02bfe2d7e5c8d223561125f87bb44e67f65c75
mspasiano <mspasiano@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2016-01-28 05:19:26 +0000

    BUG sui mandati collegati

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/intcass/bulk/V_mandato_reversaleKey.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/intcass/bulk/V_mandato_reversaleKey.java
@@ -2,6 +2,7 @@ package it.cnr.contab.doccont00.intcass.bulk;
 
 import it.cnr.contab.cmis.annotation.CMISPolicy;
 import it.cnr.contab.cmis.annotation.CMISProperty;
+import it.cnr.contab.doccont00.core.bulk.MandatoKey;
 import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.persistency.KeyedPersistent;
 
@@ -22,77 +23,96 @@ public class V_mandato_reversaleKey extends OggettoBulk implements KeyedPersiste
 	@CMISProperty(name="doccont:numDoc")		
 	private java.lang.Long pg_documento_cont;
 
-public V_mandato_reversaleKey() {
-	super();
-}
-public V_mandato_reversaleKey( Integer esercizio, String cd_tipo_documento_cont, String cd_cds, Long pg_documento_cont) {
-	this.esercizio = esercizio;
-	this.cd_tipo_documento_cont = cd_tipo_documento_cont;
-	this.cd_cds = cd_cds ;
-	this.pg_documento_cont = pg_documento_cont;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @return java.lang.String
- */
-public java.lang.String getCd_cds() {
-	return cd_cds;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @return java.lang.String
- */
-public java.lang.String getCd_tipo_documento_cont() {
-	return cd_tipo_documento_cont;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @return java.lang.Integer
- */
-public java.lang.Integer getEsercizio() {
-	return esercizio;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @return java.lang.Long
- */
-public java.lang.Long getPg_documento_cont() {
-	return pg_documento_cont;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @param newCd_cds java.lang.String
- */
-public void setCd_cds(java.lang.String newCd_cds) {
-	cd_cds = newCd_cds;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @param newCd_tipo_documento_cont java.lang.String
- */
-public void setCd_tipo_documento_cont(java.lang.String newCd_tipo_documento_cont) {
-	cd_tipo_documento_cont = newCd_tipo_documento_cont;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @param newEsercizio java.lang.Integer
- */
-public void setEsercizio(java.lang.Integer newEsercizio) {
-	esercizio = newEsercizio;
-}
-/**
- * Insert the method's description here.
- * Creation date: (29/04/2002 15.51.30)
- * @param newPg_documento_cont java.lang.Long
- */
-public void setPg_documento_cont(java.lang.Long newPg_documento_cont) {
-	pg_documento_cont = newPg_documento_cont;
-}
+	public V_mandato_reversaleKey() {
+		super();
+	}
+	public V_mandato_reversaleKey( Integer esercizio, String cd_tipo_documento_cont, String cd_cds, Long pg_documento_cont) {
+		this.esercizio = esercizio;
+		this.cd_tipo_documento_cont = cd_tipo_documento_cont;
+		this.cd_cds = cd_cds ;
+		this.pg_documento_cont = pg_documento_cont;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @return java.lang.String
+	 */
+	public java.lang.String getCd_cds() {
+		return cd_cds;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @return java.lang.String
+	 */
+	public java.lang.String getCd_tipo_documento_cont() {
+		return cd_tipo_documento_cont;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @return java.lang.Integer
+	 */
+	public java.lang.Integer getEsercizio() {
+		return esercizio;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @return java.lang.Long
+	 */
+	public java.lang.Long getPg_documento_cont() {
+		return pg_documento_cont;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @param newCd_cds java.lang.String
+	 */
+	public void setCd_cds(java.lang.String newCd_cds) {
+		cd_cds = newCd_cds;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @param newCd_tipo_documento_cont java.lang.String
+	 */
+	public void setCd_tipo_documento_cont(java.lang.String newCd_tipo_documento_cont) {
+		cd_tipo_documento_cont = newCd_tipo_documento_cont;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @param newEsercizio java.lang.Integer
+	 */
+	public void setEsercizio(java.lang.Integer newEsercizio) {
+		esercizio = newEsercizio;
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (29/04/2002 15.51.30)
+	 * @param newPg_documento_cont java.lang.Long
+	 */
+	public void setPg_documento_cont(java.lang.Long newPg_documento_cont) {
+		pg_documento_cont = newPg_documento_cont;
+	}
+	
+	public boolean equalsByPrimaryKey(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof V_mandato_reversaleKey)) return false;
+		V_mandato_reversaleKey k = (V_mandato_reversaleKey)o;
+		if(!compareKey(getCd_cds(),k.getCd_cds())) return false;
+		if(!compareKey(getEsercizio(),k.getEsercizio())) return false;
+		if(!compareKey(getCd_tipo_documento_cont(),k.getCd_tipo_documento_cont())) return false;		
+		if(!compareKey(getPg_documento_cont(),k.getPg_documento_cont())) return false;
+		return true;
+	}
+	
+	public int primaryKeyHashCode() {
+		return
+			calculateKeyHashCode(getCd_cds())+
+			calculateKeyHashCode(getEsercizio())+
+			calculateKeyHashCode(getCd_tipo_documento_cont())+			
+			calculateKeyHashCode(getPg_documento_cont());
+	}	
 }
