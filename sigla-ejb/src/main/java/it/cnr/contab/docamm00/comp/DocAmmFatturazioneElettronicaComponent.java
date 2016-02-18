5f982de2ed20d2da5c71adbf685b4f6a5fe6ec26
gianfranco.gasparro <gianfranco.gasparro@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2016-02-18 10:17:55 +0000

    Corretto passaggio password

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/comp/DocAmmFatturazioneElettronicaComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/comp/DocAmmFatturazioneElettronicaComponent.java
@@ -18,7 +18,9 @@ import it.cnr.contab.docamm00.tabrif.bulk.TariffarioBulk;
 import it.cnr.contab.docamm00.tabrif.bulk.Voce_ivaBulk;
 import it.cnr.contab.doccont00.core.bulk.AccertamentoBulk;
 import it.cnr.contab.doccont00.core.bulk.Accertamento_scadenzarioBulk;
+import it.cnr.contab.util.StringEncrypter;
 import it.cnr.contab.util.Utility;
+import it.cnr.contab.util.StringEncrypter.EncryptionException;
 import it.cnr.jada.UserContext;
 import it.cnr.jada.comp.ApplicationException;
 import it.cnr.jada.comp.CRUDComponent;
@@ -70,6 +72,7 @@ import java.util.List;
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
 import javax.ejb.EJBException;
+import javax.mail.AuthenticationFailedException;
 import javax.mail.PasswordAuthentication;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -351,7 +354,12 @@ public class DocAmmFatturazioneElettronicaComponent extends CRUDComponent{
 		}
 
 		if (email != null)
-			return new PasswordAuthentication(email.getVal01(), email.getVal02());	
+			try {
+				String password = StringEncrypter.decrypt(email.getVal01(), email.getVal02());
+				return new PasswordAuthentication(email.getVal01(), password);	
+			} catch (EncryptionException e1) {
+				new AuthenticationFailedException("Cannot decrypt password");
+			}
 		throw new ApplicationException("Confiurazione PEC non trovata, contattare il servizio di HelpDesk!");
 	}
 
