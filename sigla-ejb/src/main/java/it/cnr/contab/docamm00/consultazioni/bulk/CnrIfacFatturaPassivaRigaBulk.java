19a1cc110795886f3744582302785746ccc21d5f
gianfranco.gasparro <gianfranco.gasparro@6da727d5-75f5-4a1e-88cd-5eff942b6d78>
2016-03-16 15:11:34 +0000

    BugID: 1386 Aggiunti una serie di servizi REST utili ad IFAC

---
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/consultazioni/bulk/CnrIfacFatturaPassivaRigaBulk.java
@@ -0,0 +1,21 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 14/03/2016
+ */
+package it.cnr.contab.docamm00.consultazioni.bulk;
+public class CnrIfacFatturaPassivaRigaBulk extends CnrIfacFatturaPassivaRigaBase {
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CNR_IFAC_FATTURA_PASSIVA_RIGA
+	 **/
+	public CnrIfacFatturaPassivaRigaBulk(java.lang.String cdCds,java.lang.String cdUnitaOrganizzativa,java.lang.Integer esercizio,java.lang.Long pgFatturaPassiva,java.lang.Long progressivoRiga) {
+		super(cdCds, cdUnitaOrganizzativa, esercizio, pgFatturaPassiva, progressivoRiga);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CNR_IFAC_FATTURA_PASSIVA_RIGA
+	 **/
+	public CnrIfacFatturaPassivaRigaBulk() {
+		super();
+	}
+}
---
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/CnrIfacDettObbligazioniBulk.java
@@ -0,0 +1,21 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 14/03/2016
+ */
+package it.cnr.contab.doccont00.consultazioni.bulk;
+public class CnrIfacDettObbligazioniBulk extends CnrIfacDettObbligazioniBase {
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CNR_IFAC_DETT_OBBLIGAZIONI
+	 **/
+	public CnrIfacDettObbligazioniBulk(java.lang.String cdCds,java.lang.String cdCentroResponsabilita,java.lang.String cdLineaAttivita,java.lang.String cdVoce,java.lang.Integer esercizio,java.lang.Integer esercizioOriginale,java.lang.Long pgObbligazione,java.lang.Long pgObbligazioneScadenzario,java.lang.String tiAppartenenza,java.lang.String tiGestione) {
+		super(cdCds, cdCentroResponsabilita, cdLineaAttivita, cdVoce, esercizio, esercizioOriginale, pgObbligazione, pgObbligazioneScadenzario, tiAppartenenza, tiGestione);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CNR_IFAC_DETT_OBBLIGAZIONI
+	 **/
+	public CnrIfacDettObbligazioniBulk() {
+		super();
+	}
+}
