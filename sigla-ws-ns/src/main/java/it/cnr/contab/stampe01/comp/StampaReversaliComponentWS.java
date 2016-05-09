a2698263adb8ba0f6996b85e8f6db0f19b0f245d
Marco Spasiano <marco.spasiano@cnr.it>
2016-05-09 11:31:00 +0100

    Risolto BUG sulla sicurezza dei webservices

--- sigla-ws-ns/src/main/java/it/cnr/contab/stampe01/comp/StampaReversaliComponentWS.java
+++ sigla-ws-ns/src/main/java/it/cnr/contab/stampe01/comp/StampaReversaliComponentWS.java
@@ -10,6 +10,7 @@ import it.cnr.jada.persistency.PersistencyException;
 
 import java.rmi.RemoteException;
 
+import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
@@ -23,44 +24,51 @@ import javax.xml.soap.SOAPFault;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.soap.SOAPFaultException;
 
-import org.jboss.wsf.spi.annotation.WebContext;
-
+import org.jboss.ws.api.annotation.WebContext;
 
 @Stateless
-@WebService(endpointInterface="it.cnr.contab.stampe01.comp.StampaReversaliComponentSessionWS")
-@WebContext(contextRoot="/SIGLA-SIGLAEJB_1")
-
+@WebService(endpointInterface = "it.cnr.contab.stampe01.comp.StampaReversaliComponentSessionWS")
+@WebContext(contextRoot = "/SIGLA-SIGLAEJB_1")
 public class StampaReversaliComponentWS {
-		
-public byte[] DownloadReversale(String user, String cds,String esercizio,String pgReversale) throws NumberFormatException, PersistencyException, ComponentException, RemoteException, EJBException, Exception {
-	try{
-    	UserContext userContext = new WSUserContext(user,null,new Integer(java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)),null,null,null);
-   return ((ReversaleComponentSession)it.cnr.jada.util.ejb.EJBCommonServices.createEJB("CNRDOCCONT00_EJB_ReversaleComponentSession",ReversaleComponentSession.class)).
-			lanciaStampa(userContext, cds, new Integer(esercizio),new Long(pgReversale));
-	}catch(FatturaNonTrovataException e){
-		throw new SOAPFaultException(faultFatturaNonTrovata());
-	}catch(GenerazioneReportException e){
-		throw new SOAPFaultException(faultGenerazioneStampa());
-	}
-	
-  }
+	@EJB ReversaleComponentSession reversaleComponentSession;
 
-	private SOAPFault faultFatturaNonTrovata() throws SOAPException{
+	public byte[] DownloadReversale(String user, String cds, String esercizio,
+			String pgReversale) throws NumberFormatException,
+			PersistencyException, ComponentException, RemoteException,
+			EJBException, Exception {
+		try {
+			UserContext userContext = new WSUserContext(user, null,
+					new Integer(java.util.Calendar.getInstance().get(
+							java.util.Calendar.YEAR)), null, null, null);
+			return reversaleComponentSession.lanciaStampa(
+					userContext, cds, new Integer(esercizio), new Long(
+							pgReversale));
+		} catch (FatturaNonTrovataException e) {
+			throw new SOAPFaultException(faultFatturaNonTrovata());
+		} catch (GenerazioneReportException e) {
+			throw new SOAPFaultException(faultGenerazioneStampa());
+		}
+
+	}
+
+	private SOAPFault faultFatturaNonTrovata() throws SOAPException {
 		return generaFault("Reversale non trovata");
 	}
-	private SOAPFault faultGenerazioneStampa() throws SOAPException{
+
+	private SOAPFault faultGenerazioneStampa() throws SOAPException {
 		return generaFault("Generazione stampa non riuscita");
 	}
-  private SOAPFault generaFault(String stringFault) throws SOAPException{
+
+	private SOAPFault generaFault(String stringFault) throws SOAPException {
 		MessageFactory factory = MessageFactory.newInstance();
-		SOAPMessage message = factory.createMessage(); 
+		SOAPMessage message = factory.createMessage();
 		SOAPFactory soapFactory = SOAPFactory.newInstance();
-		SOAPBody body = message.getSOAPBody(); 
+		SOAPBody body = message.getSOAPBody();
 		SOAPFault fault = body.addFault();
-		Name faultName = soapFactory.createName("","", SOAPConstants.URI_NS_SOAP_ENVELOPE);
+		Name faultName = soapFactory.createName("", "",
+				SOAPConstants.URI_NS_SOAP_ENVELOPE);
 		fault.setFaultCode(faultName);
 		fault.setFaultString(stringFault);
 		return fault;
-  }
+	}
 }
-
--- sigla-ws-ns/src/main/java/it/cnr/contab/stampe01/comp/StampaVariazioniComponentWS.java
+++ sigla-ws-ns/src/main/java/it/cnr/contab/stampe01/comp/StampaVariazioniComponentWS.java
@@ -10,6 +10,7 @@ import it.cnr.jada.persistency.PersistencyException;
 
 import java.rmi.RemoteException;
 
+import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
@@ -23,43 +24,51 @@ import javax.xml.soap.SOAPFault;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.soap.SOAPFaultException;
 
-import org.jboss.wsf.spi.annotation.WebContext;
-
+import org.jboss.ws.api.annotation.WebContext;
 
 @Stateless
-@WebService(endpointInterface="it.cnr.contab.stampe01.comp.StampaVariazioniComponentSessionWS")
-@WebContext(contextRoot="/SIGLA-SIGLAEJB_1")
+@WebService(endpointInterface = "it.cnr.contab.stampe01.comp.StampaVariazioniComponentSessionWS")
+@WebContext(contextRoot = "/SIGLA-SIGLAEJB_1")
 public class StampaVariazioniComponentWS {
+	@EJB PdGVariazioniComponentSession pdGVariazioniComponentSession;
 	
-public byte[] DownloadVariazione(String user,String esercizio,String pg,String tipoVar) throws NumberFormatException, PersistencyException, ComponentException, RemoteException, EJBException, Exception {
-    	UserContext userContext = new WSUserContext(user,null,new Integer(java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)),null,null,null);
-		try{	
-		   return  ((PdGVariazioniComponentSession)it.cnr.jada.util.ejb.EJBCommonServices.createEJB("CNRPDG00_EJB_PdGVariazioniComponentSession",PdGVariazioniComponentSession.class)).
-				   lanciaStampa(userContext, new Integer(esercizio), new Integer(pg), new String(tipoVar));
-		}catch(GenerazioneReportException e){
+	public byte[] DownloadVariazione(String user, String esercizio, String pg,
+			String tipoVar) throws NumberFormatException, PersistencyException,
+			ComponentException, RemoteException, EJBException, Exception {
+		UserContext userContext = new WSUserContext(user, null, new Integer(
+				java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)),
+				null, null, null);
+		try {
+			return pdGVariazioniComponentSession.lanciaStampa(
+					userContext, new Integer(esercizio), new Integer(pg),
+					new String(tipoVar));
+		} catch (GenerazioneReportException e) {
 			throw new SOAPFaultException(faultGenerazioneStampa());
-		}catch(FatturaNonTrovataException e){
+		} catch (FatturaNonTrovataException e) {
 			throw new SOAPFaultException(faultFatturaNonTrovata());
 		}
-  }
+	}
 
-	private SOAPFault faultFatturaNonTrovata() throws SOAPException{
-		return generaFault("002","Variazione non trovata");
+	private SOAPFault faultFatturaNonTrovata() throws SOAPException {
+		return generaFault("002", "Variazione non trovata");
 	}
-	private SOAPFault faultGenerazioneStampa() throws SOAPException{
-		return generaFault("004","Generazione stampa non riuscita");
+
+	private SOAPFault faultGenerazioneStampa() throws SOAPException {
+		return generaFault("004", "Generazione stampa non riuscita");
 	}
-  private SOAPFault generaFault(String localName,String stringFault) throws SOAPException{
+
+	private SOAPFault generaFault(String localName, String stringFault)
+			throws SOAPException {
 		MessageFactory factory = MessageFactory.newInstance();
-		SOAPMessage message = factory.createMessage(); 
+		SOAPMessage message = factory.createMessage();
 		SOAPFactory soapFactory = SOAPFactory.newInstance();
-		SOAPBody body = message.getSOAPBody(); 
+		SOAPBody body = message.getSOAPBody();
 		SOAPFault fault = body.addFault();
-		Name faultName = soapFactory.createName(localName,"", SOAPConstants.URI_NS_SOAP_ENVELOPE);
+		Name faultName = soapFactory.createName(localName, "",
+				SOAPConstants.URI_NS_SOAP_ENVELOPE);
 
 		fault.setFaultCode(faultName);
 		fault.setFaultString(stringFault);
 		return fault;
-  }
+	}
 }
-
