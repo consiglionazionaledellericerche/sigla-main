70f81f7a46ffe26aad274a626f258e2c21e0dea9
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-07-26 15:42:22 +0200

    Errore serializzazione Mandato

---
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/intcass/bulk/MandatoBanca.java
@@ -0,0 +1,9 @@
+package it.cnr.contab.doccont00.intcass.bulk;
+
+import it.cnr.contab.doccont00.intcass.xmlbnl.Mandato;
+import it.cnr.contab.doccont00.intcass.xmlbnl.Reversale;
+
+import java.io.Serializable;
+
+public class MandatoBanca extends Mandato implements Serializable{
+}
