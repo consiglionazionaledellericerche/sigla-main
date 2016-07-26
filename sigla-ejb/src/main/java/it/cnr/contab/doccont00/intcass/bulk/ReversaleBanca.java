285416b9aa6eeb97022637f970314a5f9d5bc445
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-07-26 15:10:47 +0200

    Errore di serializzazione

---
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/intcass/bulk/ReversaleBanca.java
@@ -0,0 +1,8 @@
+package it.cnr.contab.doccont00.intcass.bulk;
+
+import it.cnr.contab.doccont00.intcass.xmlbnl.Reversale;
+
+import java.io.Serializable;
+
+public class ReversaleBanca extends Reversale implements Serializable{
+}
