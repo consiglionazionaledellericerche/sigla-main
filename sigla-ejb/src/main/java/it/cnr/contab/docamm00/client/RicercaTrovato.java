c7a9b0a0fb7e19e6865d0657aea0e09591d21a81
Francesco Uliana <francesco.uliana@cnr.it>
2016-07-27 14:53:04 +0200

    fix webservice RicercaTrovato

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/client/RicercaTrovato.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/client/RicercaTrovato.java
@@ -1,35 +1,29 @@
 package it.cnr.contab.docamm00.client;
 
-import it.cnr.brevetti.FindTrovato;
-import it.cnr.brevetti.FindTrovatoE;
-import it.cnr.brevetti.FindTrovatoResponseE;
-import it.cnr.brevetti.FindTrovatoValido;
-import it.cnr.brevetti.FindTrovatoValidoE;
-import it.cnr.brevetti.FindTrovatoValidoResponseE;
-import it.cnr.brevetti.TrovatiWebServiceBeanServiceStub;
-import it.cnr.brevetti.TrovatoBean;
+import it.cnr.brevetti.*;
 import it.cnr.contab.docamm00.docs.bulk.TrovatoBulk;
 import it.cnr.contab.service.SpringUtil;
 import it.cnr.jada.comp.ApplicationException;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.lang.Exception;
+
 public class RicercaTrovato {
 
 	private static String targetEndpoint; //="http://siglaas4.cedrc.cnr.it:8480/";
-	private static String axis2Home; 
-	private static String axis2TrovatoConf; 
+	private static URL axis2Home;
+
+	private static URL axis2TrovatoConf;
+
 	private static String siglaWSClientPassword;
-	
 	public RicercaTrovato() throws FileNotFoundException, IOException {
 		super();
-//		recuperoTrovatoProperties();	
+//		recuperoTrovatoProperties();
 		// carica le proprietà solo alla prima occorrenza della classe
 //		if (getTargetEndpoint()==null)
 			loadProperties();
@@ -50,7 +44,7 @@ public class RicercaTrovato {
 //
 	private static ConfigurationContext axis2ConfContext() throws Exception   {
 
-		return ConfigurationContextFactory.createConfigurationContextFromFileSystem(getAxis2Home(), getAxis2TrovatoConf());
+		return ConfigurationContextFactory.createConfigurationContextFromURIs(getAxis2TrovatoConf(), getAxis2Home());
 	}
 
 	public TrovatoBulk ricercaDatiTrovato(it.cnr.jada.UserContext userContext,Long trovato, Boolean soloValidi) throws Exception {
@@ -61,24 +55,24 @@ public class RicercaTrovato {
 		} else {
 			trovatoBulk = cerca(ctx, trovato, soloValidi);
 		}
-		return trovatoBulk; 
+		return trovatoBulk;
 	}
 
 	public TrovatoBulk ricercaDatiTrovato(it.cnr.jada.UserContext userContext,Long trovato) throws Exception {
-		return ricercaDatiTrovato(userContext, trovato, false); 
+		return ricercaDatiTrovato(userContext, trovato, false);
 	}
 
 	private TrovatoBulk cerca(ConfigurationContext ctx, Long pgTrovato, Boolean soloValidi) throws Exception {
         TrovatiWebServiceBeanServiceStub stub = new TrovatiWebServiceBeanServiceStub(ctx,getTargetEndpoint()+"/brevetti/ws/TrovatiWebServiceBean");
 
         TrovatoBulk trovatoBulk = new TrovatoBulk();
-        
+
         if (soloValidi){
             FindTrovatoValidoE trovatoE = new FindTrovatoValidoE();
             FindTrovatoValido trovato = new FindTrovatoValido();
             trovato.setNsrif(pgTrovato.intValue());
             trovatoE.setFindTrovatoValido(trovato);
-            
+
     		FindTrovatoValidoResponseE respE = stub.findTrovatoValido(trovatoE);
 
     		if (respE.getFindTrovatoValidoResponse().getResult()!=null) {
@@ -92,7 +86,7 @@ public class RicercaTrovato {
             FindTrovato trovato = new FindTrovato();
             trovato.setNsrif(pgTrovato.intValue());
             trovatoE.setFindTrovato(trovato);
-            
+
     		FindTrovatoResponseE respE = stub.findTrovato(trovatoE);
 
     		if (respE.getFindTrovatoResponse().getResult()!=null) {
@@ -118,42 +112,27 @@ public class RicercaTrovato {
 		PWCBHandler.setSiglaClientWSPassword(trovatoProperties.getTrovatoSiglaWSClientPassword());
 
 		URL urlAxis2 = new ClassPathResource("axis2/").getURL();
-		
+
 		if (urlAxis2 != null){
-			setAxis2Home(urlAxis2.getPath());
+			setAxis2Home(urlAxis2);
 //			if (getAxis2Home().startsWith("file:")){
 //				String axis2Home = getAxis2Home().substring(5);
 //				if (axis2Home != null){
 //					setAxis2Home(axis2Home.replace("!", "-contents"));
 //				}
 //			}
-			setAxis2TrovatoConf(getAxis2Home()+"axis2-brevetti.xml");
+			setAxis2TrovatoConf(new URL(getAxis2Home()+"axis2-brevetti.xml"));
 		} else {
-			
+
 		}
 //		setAxis2TrovatoConf(getAxis2Home()+"axis2-brevetti.xml");
 
 	}
 
-	public static String getAxis2Home() {
-		return axis2Home;
-	}
-
-	public static void setAxis2Home(String axis2Home) {
-		RicercaTrovato.axis2Home = axis2Home;
-	}
-
-	public static String getAxis2TrovatoConf() {
-		return axis2TrovatoConf;
-	}
-
-	public static void setAxis2TrovatoConf(String axis2TrovatoConf) {
-		RicercaTrovato.axis2TrovatoConf = axis2TrovatoConf;
-	}
-
 	public static String getTargetEndpoint() {
 		return targetEndpoint;
 	}
+
 
 	public static void setTargetEndpoint(String targetEndpoint) {
 		RicercaTrovato.targetEndpoint = targetEndpoint;
@@ -165,5 +144,21 @@ public class RicercaTrovato {
 
 	public static void setSiglaWSClientPassword(String siglaWSClientPassword) {
 		RicercaTrovato.siglaWSClientPassword = siglaWSClientPassword;
+	}
+
+	public static URL getAxis2Home() {
+		return axis2Home;
+	}
+
+	public static void setAxis2Home(URL axis2Home) {
+		RicercaTrovato.axis2Home = axis2Home;
+	}
+
+	public static URL getAxis2TrovatoConf() {
+		return axis2TrovatoConf;
+	}
+
+	public static void setAxis2TrovatoConf(URL axis2TrovatoConf) {
+		RicercaTrovato.axis2TrovatoConf = axis2TrovatoConf;
 	}
 }
