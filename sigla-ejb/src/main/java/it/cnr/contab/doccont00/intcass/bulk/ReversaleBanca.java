306ba25dd08beb1bf79c1a9f9cbeaa19ec51e136
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-07-29 11:49:46 +0200

    Risolto problema flusso banca

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/intcass/bulk/ReversaleBanca.java
+++
@@ -1,8 +0,0 @@
-package it.cnr.contab.doccont00.intcass.bulk;
-
-import it.cnr.contab.doccont00.intcass.xmlbnl.Reversale;
-
-import java.io.Serializable;
-
-public class ReversaleBanca extends Reversale implements Serializable{
-}
