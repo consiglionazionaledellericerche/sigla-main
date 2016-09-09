a7e8b2c6f5e50507483e96f189d1722b3dba7f78
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-09-09 16:03:44 +0200

    Aggiunte classi per consultazione riepilogo compensi

---
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/consultazioni/bulk/VConsRiepCompensiBulk.java
@@ -0,0 +1,24 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 09/09/2016
+ */
+package it.cnr.contab.docamm00.consultazioni.bulk;
+import it.cnr.jada.action.ActionContext;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.util.action.CRUDBP;
+public class VConsRiepCompensiBulk extends VConsRiepCompensiBase {
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: V_CONS_RIEP_COMPENSI
+	 **/
+	public VConsRiepCompensiBulk() {
+		super();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: V_CONS_RIEP_COMPENSI
+	 **/
+	public VConsRiepCompensiBulk() {
+		super();
+	}
+}
