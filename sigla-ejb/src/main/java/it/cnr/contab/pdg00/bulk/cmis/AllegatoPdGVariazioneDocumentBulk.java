d44ee4394f3179c72540bcf9e36e54cf7c482232
marco.spasiano <marco.spasiano@cnr.it>
2016-09-13 12:22:10 +0200

    BUG su attiva file

--- sigla-ejb/src/main/java/it/cnr/contab/pdg00/bulk/cmis/AllegatoPdGVariazioneDocumentBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/pdg00/bulk/cmis/AllegatoPdGVariazioneDocumentBulk.java
@@ -51,7 +51,7 @@ public class AllegatoPdGVariazioneDocumentBulk extends OggettoBulk{
 	}
 	
 	public boolean isNodePresent(){
-		return getDocument() == null;
+		return getDocument() != null;
 	}
 
 	public boolean isNodeNotPresent(){
