4d3cc02b0691359cbee4d6a7f3d4cfee103a66cb
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-09-16 15:23:32 +0200

    Correzioni varie Rest Service Brevetti

---
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/consultazioni/bulk/VDocAmmBrevettiBulk.java
@@ -0,0 +1,21 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 12/09/2016
+ */
+package it.cnr.contab.docamm00.consultazioni.bulk;
+public class VDocAmmBrevettiBulk extends VDocAmmBrevettiBase {
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: V_DOC_AMM_BREVETTI
+	 **/
+	public VDocAmmBrevettiBulk() {
+		super();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: V_DOC_AMM_BREVETTI
+	 **/
+	public VDocAmmBrevettiBulk(String cd_cds,String cd_unita_organizzativa,Integer esercizio, Long pg_fattura_attiva,String tipoFatturaCompenso) {
+		super(cd_cds,cd_unita_organizzativa,esercizio,pg_fattura_attiva,tipoFatturaCompenso);
+	}
+}
