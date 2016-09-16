eb6678cd535ff45e0b31271ca261be442d359652
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-09-16 15:23:32 +0200

    Correzioni varie Rest Service Brevetti

---
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/consultazioni/bulk/VDocAmmBrevettiBase.java
@@ -0,0 +1,224 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 12/09/2016
+ */
+package it.cnr.contab.docamm00.consultazioni.bulk;
+import it.cnr.jada.persistency.Keyed;
+public class VDocAmmBrevettiBase extends VDocAmmBrevettiKey implements Keyed {
+//    DS_FATTURA_ATTIVA VARCHAR(1000)
+	private java.lang.String dsFatturaPassiva;
+ 
+//    CD_CDS_ORIGINE VARCHAR(30) NOT NULL
+	private java.lang.String cdCdsOrigine;
+ 
+//    CD_UO_ORIGINE VARCHAR(30) NOT NULL
+	private java.lang.String cdUoOrigine;
+ 
+//    DT_REGISTRAZIONE TIMESTAMP(7) NOT NULL
+	private java.sql.Timestamp dtRegistrazione;
+ 
+//    CD_TERZO DECIMAL(8,0) NOT NULL
+	private java.lang.Integer cdTerzo;
+ 
+//    COGNOME VARCHAR(50)
+	private java.lang.String cognome;
+ 
+//    NOME VARCHAR(50)
+	private java.lang.String nome;
+ 
+//    RAGIONE_SOCIALE VARCHAR(100)
+	private java.lang.String ragioneSociale;
+ 
+//    CAMBIO DECIMAL(15,4) NOT NULL
+	private java.math.BigDecimal cambio;
+ 
+//    CD_DIVISA VARCHAR(10) NOT NULL
+	private java.lang.String cdDivisa;
+ 
+	// NR_FATTURA_FORNITORE VARCHAR(20) NOT NULL
+	private java.lang.String nr_fattura_fornitore;
+
+	// DT_FATTURA_FORNITORE TIMESTAMP NOT NULL
+	private java.sql.Timestamp dt_fattura_fornitore;
+
+	// PARTITA_IVA VARCHAR(20)
+	private java.lang.String partita_iva;
+
+	// CODICE_FISCALE VARCHAR(20)
+	private java.lang.String codice_fiscale;
+
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: V_DOC_AMM_BREVETTI
+	 **/
+	public VDocAmmBrevettiBase() {
+		super();
+	}
+	public VDocAmmBrevettiBase(java.lang.String cd_cds,java.lang.String cd_unita_organizzativa,java.lang.Integer esercizio,java.lang.Long pg_fattura_passiva,java.lang.String tipoFatturaCompenso) {
+		super(cd_cds,cd_unita_organizzativa,esercizio,pg_fattura_passiva,tipoFatturaCompenso);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [dsFatturaAttiva]
+	 **/
+	public java.lang.String getDsFatturaPassiva() {
+		return dsFatturaPassiva;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [dsFatturaAttiva]
+	 **/
+	public void setDsFatturaPassiva(java.lang.String dsFatturaPassiva)  {
+		this.dsFatturaPassiva=dsFatturaPassiva;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdCdsOrigine]
+	 **/
+	public java.lang.String getCdCdsOrigine() {
+		return cdCdsOrigine;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdCdsOrigine]
+	 **/
+	public void setCdCdsOrigine(java.lang.String cdCdsOrigine)  {
+		this.cdCdsOrigine=cdCdsOrigine;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdUoOrigine]
+	 **/
+	public java.lang.String getCdUoOrigine() {
+		return cdUoOrigine;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdUoOrigine]
+	 **/
+	public void setCdUoOrigine(java.lang.String cdUoOrigine)  {
+		this.cdUoOrigine=cdUoOrigine;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [dtRegistrazione]
+	 **/
+	public java.sql.Timestamp getDtRegistrazione() {
+		return dtRegistrazione;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [dtRegistrazione]
+	 **/
+	public void setDtRegistrazione(java.sql.Timestamp dtRegistrazione)  {
+		this.dtRegistrazione=dtRegistrazione;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdTerzo]
+	 **/
+	public java.lang.Integer getCdTerzo() {
+		return cdTerzo;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdTerzo]
+	 **/
+	public void setCdTerzo(java.lang.Integer cdTerzo)  {
+		this.cdTerzo=cdTerzo;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cognome]
+	 **/
+	public java.lang.String getCognome() {
+		return cognome;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cognome]
+	 **/
+	public void setCognome(java.lang.String cognome)  {
+		this.cognome=cognome;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [nome]
+	 **/
+	public java.lang.String getNome() {
+		return nome;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [nome]
+	 **/
+	public void setNome(java.lang.String nome)  {
+		this.nome=nome;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [ragioneSociale]
+	 **/
+	public java.lang.String getRagioneSociale() {
+		return ragioneSociale;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [ragioneSociale]
+	 **/
+	public void setRagioneSociale(java.lang.String ragioneSociale)  {
+		this.ragioneSociale=ragioneSociale;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cambio]
+	 **/
+	public java.math.BigDecimal getCambio() {
+		return cambio;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cambio]
+	 **/
+	public void setCambio(java.math.BigDecimal cambio)  {
+		this.cambio=cambio;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdDivisa]
+	 **/
+	public java.lang.String getCdDivisa() {
+		return cdDivisa;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdDivisa]
+	 **/
+	public void setCdDivisa(java.lang.String cdDivisa)  {
+		this.cdDivisa=cdDivisa;
+	}
+	public java.lang.String getNr_fattura_fornitore() {
+		return nr_fattura_fornitore;
+	}
+	public void setNr_fattura_fornitore(java.lang.String nr_fattura_fornitore) {
+		this.nr_fattura_fornitore = nr_fattura_fornitore;
+	}
+	public java.sql.Timestamp getDt_fattura_fornitore() {
+		return dt_fattura_fornitore;
+	}
+	public void setDt_fattura_fornitore(java.sql.Timestamp dt_fattura_fornitore) {
+		this.dt_fattura_fornitore = dt_fattura_fornitore;
+	}
+	public java.lang.String getPartita_iva() {
+		return partita_iva;
+	}
+	public void setPartita_iva(java.lang.String partita_iva) {
+		this.partita_iva = partita_iva;
+	}
+	public java.lang.String getCodice_fiscale() {
+		return codice_fiscale;
+	}
+	public void setCodice_fiscale(java.lang.String codice_fiscale) {
+		this.codice_fiscale = codice_fiscale;
+	}
+}
