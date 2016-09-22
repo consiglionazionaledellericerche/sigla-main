1de84affe9b0a3bcca4d16b245f7e2cd7568c5b9
rosangela.pucciarelli <rosangela.pucciarelli@cnr.it>
2016-09-22 12:45:33 +0200

    -Blocco utilizzo caratteri speciali nell'inserimento codice Cup - Codice Cig -Blocco utilizzo caratteri speciali o numerici nella valuta della lettera 1210 -Visualizzazione delle lettere 1210 collegate al sospeso nella mappa sospesi -Impedita registrazione fatture elettroniche indicate come irregistrabili -Impedita indicazione nella lettera 1210 del sospeso se non nello stato trasmesso -Impedita associazione manuale Mandato a reversale su Mandati che non si trovano nello stato iniziale(Non inserito in distinta), inoltre è stato controllato che possono collegare solo reversali di incasso -Modificata consultazione Fondo economale -Impedito collegamento a mandati/reversali in tesoreria unica dei sospesi non ente (999) -Consentito modifica righe documento generico con 1210 in stato trasmesso -Consentita registrazione nota credito anche senza riferimenti alla fattura collegata - se presenti e riconosciuti sono stati considerati - altrimenti viene recuperata l'ultima fattura registrata per lo stesso fornitore/uo

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/docs/bulk/Lettera_pagam_esteroBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/docs/bulk/Lettera_pagam_esteroBulk.java
@@ -241,6 +241,9 @@ public class Lettera_pagam_esteroBulk extends Lettera_pagam_esteroBase implement
 		}
 		if (getDivisa() == null)
 			throw new ValidationException("Valorizzare la Divisa!");
+		for (int i = 0;i <getDivisa().length();i++)
+			if (!Character.isLetter(getDivisa().charAt(i)) && getDivisa().charAt(i)!=' ' )
+				throw new ValidationException( "La divisa può essere composta solo da lettere e non può contenere caratteri speciali." );
 		if (getBeneficiario() == null)
 			throw new ValidationException("Valorizzare il Beneficiario!");
 	}
