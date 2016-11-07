87b4853dab6d0b50acfba37b4ea3d8f8dff83a82
Pagano <raffaele.pagano@cnr.it>
2016-11-07 11:52:24 +0100

    Aggiornamento Stampe Bilancio

--- sigla-ejb/src/main/java/it/cnr/contab/prevent01/bp/StampaPdgpBilancioBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/prevent01/bp/StampaPdgpBilancioBP.java
@@ -1,13 +1,17 @@
 package it.cnr.contab.prevent01.bp;
 
+import java.util.Enumeration;
+
 import it.cnr.contab.config00.pdcfin.cla.bulk.Parametri_livelliBulk;
 import it.cnr.contab.prevent01.bulk.Stampa_pdgp_bilancioBulk;
+import it.cnr.contab.prevent01.ejb.PdgAggregatoModuloComponentSession;
 import it.cnr.contab.reports.bp.ParametricPrintBP;
 import it.cnr.contab.utenze00.bp.CNRUserContext;
 import it.cnr.contab.util.Utility;
 import it.cnr.jada.action.ActionContext;
 import it.cnr.jada.action.BusinessProcessException;
 import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.comp.ComponentException;
 
 
 public class StampaPdgpBilancioBP extends ParametricPrintBP {
@@ -37,7 +41,9 @@ public class StampaPdgpBilancioBP extends ParametricPrintBP {
 			OggettoBulk oggettoBulk = super.initializeBulkForPrint(context, bulk);
 			if (oggettoBulk instanceof Stampa_pdgp_bilancioBulk) {
 				((Stampa_pdgp_bilancioBulk) oggettoBulk).setEsercizio(CNRUserContext.getEsercizio(context.getUserContext()));
-				((Stampa_pdgp_bilancioBulk) oggettoBulk).setTi_fonte(Stampa_pdgp_bilancioBulk.TIPO_DECISIONALE_SCIENTIFICO);				
+				((Stampa_pdgp_bilancioBulk) oggettoBulk).setTi_stampa(Stampa_pdgp_bilancioBulk.TIPO_DECISIONALE);				
+				((Stampa_pdgp_bilancioBulk) oggettoBulk).setTi_aggregazione(Stampa_pdgp_bilancioBulk.TIPO_SCIENTIFICO);				
+				((Stampa_pdgp_bilancioBulk) oggettoBulk).setTi_origine(Stampa_pdgp_bilancioBulk.TIPO_PROVVISORIO);				
 				((Stampa_pdgp_bilancioBulk) oggettoBulk).setTi_gestione(Stampa_pdgp_bilancioBulk.TIPO_GESTIONE_ENTRATA);
 				
 				loadModelBulkOptions(context, (Stampa_pdgp_bilancioBulk) oggettoBulk);
@@ -60,17 +66,14 @@ public class StampaPdgpBilancioBP extends ParametricPrintBP {
 
 	private Stampa_pdgp_bilancioBulk loadModelBulkOptions(ActionContext context, Stampa_pdgp_bilancioBulk stampa) throws BusinessProcessException {
 		try {
-			stampa.setTi_livello(null);
-				
 			Parametri_livelliBulk parliv = Utility.createClassificazioneVociComponentSession().findParametriLivelli(context.getUserContext(), stampa.getEsercizio());
 	
 			it.cnr.jada.util.OrderedHashtable livelliOptions = new it.cnr.jada.util.OrderedHashtable();
 	
 			int index = 1;
 				
-			if (Stampa_pdgp_bilancioBulk.TIPO_DECISIONALE_SCIENTIFICO.equals(stampa.getTi_fonte())||
-				Stampa_pdgp_bilancioBulk.TIPO_GESTIONALE_SCIENTIFICO.equals(stampa.getTi_fonte())||
-				Stampa_pdgp_bilancioBulk.TIPO_STANZIAMENTO_SCIENTIFICO.equals(stampa.getTi_fonte())) {
+			if (Stampa_pdgp_bilancioBulk.TIPO_GESTIONE_SPESA.equals(stampa.getTi_gestione()) &&
+				Stampa_pdgp_bilancioBulk.TIPO_SCIENTIFICO.equals(stampa.getTi_aggregazione())) {
 				livelliOptions.put(index++, "Programma");
 				livelliOptions.put(index++, "Missione");
 			}
@@ -108,9 +111,22 @@ public class StampaPdgpBilancioBP extends ParametricPrintBP {
 			}
 
 			stampa.setLivelliOptions(livelliOptions);
+			if (stampa.getTi_livello()!=null) {
+				Enumeration a = livelliOptions.keys();
+				while (a.hasMoreElements()) {
+					Integer key = (Integer) a.nextElement();
+					if (livelliOptions.get(key).equals(stampa.getTi_livello()))
+						return stampa;
+				}
+			}
+			stampa.setTi_livello(livelliOptions.get(0).toString());
 		}catch(Throwable e) {
 			throw new BusinessProcessException(e);
 		}
 		return stampa;
 	}
+	
+	public void stampaBilancioCallAggiornaDati(ActionContext context, boolean aggPrevAC, boolean aggResiduiAC, boolean aggResiduiAP, boolean aggCassaAC) throws BusinessProcessException, ComponentException {
+		((PdgAggregatoModuloComponentSession)createComponentSession()).stampaBilancioCallAggiornaDati(context.getUserContext(), (Stampa_pdgp_bilancioBulk)this.getModel(), aggPrevAC, aggResiduiAC, aggResiduiAP, aggCassaAC);
+	}	
 }
