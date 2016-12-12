5c7058c87af3e58c269a981445dc7bd4f6e43669
marco.spasiano <marco.spasiano@cnr.it>
2016-12-12 18:01:41 +0100

    Servizi REST per missioni

--- sigla-web/src/main/java/it/cnr/contab/web/rest/config/RESTSecurityInterceptor.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/config/RESTSecurityInterceptor.java
@@ -28,8 +28,8 @@ import org.jboss.resteasy.core.ResourceMethodInvoker;
 
 @Provider
 public class RESTSecurityInterceptor implements
-		javax.ws.rs.container.ContainerRequestFilter,
-		ExceptionMapper<Exception> {
+javax.ws.rs.container.ContainerRequestFilter,
+ExceptionMapper<Exception> {
 
 	private Log LOGGER = LogFactory.getLog(RESTSecurityInterceptor.class);
 
@@ -39,48 +39,60 @@ public class RESTSecurityInterceptor implements
 	public void filter(ContainerRequestContext requestContext) {	
 		ResourceMethodInvoker methodInvoker = (ResourceMethodInvoker) requestContext
 				.getProperty(ResourceMethodInvoker.class.getName());
-		Method method = methodInvoker.getMethod();
-		// Access allowed for all
-		if (!method.isAnnotationPresent(PermitAll.class)) {
-			// Access denied for all
-			if (method.isAnnotationPresent(DenyAll.class)) {
+		final Method method = methodInvoker.getMethod();
+		final Class<?> declaring = method.getDeclaringClass();
+		
+		String[] rolesAllowed = null;
+		boolean denyAll;
+		boolean permitAll;
+		RolesAllowed allowed = (RolesAllowed) declaring.getAnnotation(RolesAllowed.class);
+		RolesAllowed methodAllowed = method.getAnnotation(RolesAllowed.class);
+		if (methodAllowed != null) allowed = methodAllowed;
+		if (allowed != null)
+		{
+			rolesAllowed = allowed.value();
+		}
+
+		denyAll = (declaring.isAnnotationPresent(DenyAll.class)
+				&& method.isAnnotationPresent(RolesAllowed.class) == false
+				&& method.isAnnotationPresent(PermitAll.class) == false) || method.isAnnotationPresent(DenyAll.class);
+
+		permitAll = (declaring.isAnnotationPresent(PermitAll.class) == true
+				&& method.isAnnotationPresent(RolesAllowed.class) == false
+				&& method.isAnnotationPresent(DenyAll.class) == false) || method.isAnnotationPresent(PermitAll.class);
+
+		if (rolesAllowed != null || denyAll || permitAll) {
+			if (denyAll) {
 				requestContext.abortWith(Response.status(Status.FORBIDDEN).entity(UNAUTHORIZED_MAP).build());
 				return;
 			}
-
-			// Get request headers
-			final MultivaluedMap<String, String> headers = requestContext
-					.getHeaders();
-
-			// Fetch authorization header
-			final List<String> authorization = headers
-					.get(AUTHORIZATION_PROPERTY);
-			UtenteBulk utenteBulk = null;
-			try {
-				utenteBulk = BasicAuthentication.authenticate(authorization);
-				if (utenteBulk == null){
-					requestContext.abortWith(Response.status(Status.UNAUTHORIZED).entity(UNAUTHORIZED_MAP).build());
-				}
-			} catch (Exception e) {
-				LOGGER.error("ERROR for REST SERVICE", e);
-				requestContext.abortWith(Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build());
-			}
-
-			// Verify user access
-			if (method.isAnnotationPresent(RolesAllowed.class)) {
-				RolesAllowed rolesAnnotation = method
-						.getAnnotation(RolesAllowed.class);
-				Set<String> rolesSet = new HashSet<String>(
-						Arrays.asList(rolesAnnotation.value()));
+			if (permitAll) return;
+		    if (rolesAllowed != null) {
+				final MultivaluedMap<String, String> headers = requestContext.getHeaders();
+				final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
+				UtenteBulk utenteBulk = null;
 				try {
-					if (!isUserAllowed(utenteBulk, rolesSet)) {
+					utenteBulk = BasicAuthentication.authenticate(authorization);
+					if (utenteBulk == null){
 						requestContext.abortWith(Response.status(Status.UNAUTHORIZED).entity(UNAUTHORIZED_MAP).build());
 						return;
 					}
 				} catch (Exception e) {
+					LOGGER.error("ERROR for REST SERVICE", e);
 					requestContext.abortWith(Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build());
+					return;
 				}
-			}
+				Set<String> rolesSet = new HashSet<String>(
+						Arrays.asList(rolesAllowed));
+				try {
+					if (!isUserAllowed(utenteBulk, rolesSet)) {
+						requestContext.abortWith(Response.status(Status.UNAUTHORIZED).entity(Collections.singletonMap("ERROR", "User doesn't have the following roles: " + rolesSet)).build());
+						return;
+					}
+				} catch (Exception e) {
+					requestContext.abortWith(Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build());
+				}		    	
+		    }			
 		}
 	}
 
@@ -88,9 +100,9 @@ public class RESTSecurityInterceptor implements
 			final Set<String> rolesSet) throws Exception{
 		try {
 			return Optional.ofNullable(BasicAuthentication.getRuoli(new RESTUserContext(), utente))
-				.map(x -> x.stream())
-				.get()
-				.filter(x -> rolesSet.contains(x.getCd_ruolo())).count() > 0;
+					.map(x -> x.stream())
+					.get()
+					.filter(x -> rolesSet.contains(x.getCd_ruolo())).count() > 0;
 		} catch (Exception e) {
 			throw e;
 		}
