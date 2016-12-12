2ff0707c18d9fb0585759761ceae136c14c80bc3
marco.spasiano <marco.spasiano@cnr.it>
2016-12-12 15:24:12 +0100

    Servizi REST per missioni

--- sigla-web/src/main/java/it/cnr/contab/web/rest/config/JaxRsActivator.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/config/JaxRsActivator.java
@@ -4,6 +4,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/restapi")
-public class JaxRsActivator extends Application {
+public class JaxRsActivator extends Application{
 
 }
