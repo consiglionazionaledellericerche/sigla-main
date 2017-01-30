fd76bda545e5d8ad5e56a9b61703295f8b344ef8
rosangela.pucciarelli <rosangela.pucciarelli@cnr.it>
2017-01-30 18:25:47 +0100

    Blocco utilizzo modalità F24EP da utenti NON supervisori Ripristinato invio PEC Firma variazioni pdg Gestita apertura file con carattere "'" nel nome file

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/TransactionalMandatoAutomaticoComponentSession.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/TransactionalMandatoAutomaticoComponentSession.java
@@ -528,4 +528,24 @@ public Boolean isMandatoCollegatoAnnullodaRiemettere(UserContext param0,
 		}
 	}
 }
+@Override
+public Boolean isVerificataModPagMandato(UserContext param0,
+		V_mandato_reversaleBulk param1)
+		throws ComponentException, RemoteException {
+	try {
+		return (java.lang.Boolean)invoke("isVerificataModPagMandato",new Object[] {
+			param0,
+			param1 });
+	} catch(java.rmi.RemoteException e) {
+		throw e;
+	} catch(java.lang.reflect.InvocationTargetException e) {
+		try {
+			throw e.getTargetException();
+		} catch(it.cnr.jada.comp.ComponentException ex) {
+			throw ex;
+		} catch(Throwable ex) {
+			throw new java.rmi.RemoteException("Uncaugth exception",ex);
+		}
+	}
+}
 }
