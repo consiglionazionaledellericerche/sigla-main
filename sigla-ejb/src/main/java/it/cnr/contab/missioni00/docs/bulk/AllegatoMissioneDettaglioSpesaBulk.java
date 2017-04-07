a490c6695946e2ba770072299d92954716132879
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-04-07 12:20:45 +0200

    Corretta gestione allegati dettaglio missione

--- sigla-ejb/src/main/java/it/cnr/contab/missioni00/docs/bulk/AllegatoMissioneDettaglioSpesaBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/missioni00/docs/bulk/AllegatoMissioneDettaglioSpesaBulk.java
@@ -7,23 +7,31 @@ import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.opencmis.commons.PropertyIds;
 
 import it.cnr.contab.cmis.annotation.CMISProperty;
+import it.cnr.contab.missioni00.service.MissioniCMISService;
 import it.cnr.contab.util00.bulk.cmis.AllegatoGenericoBulk;
 
 public class AllegatoMissioneDettaglioSpesaBulk extends AllegatoGenericoBulk {
 	private static final long serialVersionUID = 1L;
 
+	private String aspectName;
+	
 	public AllegatoMissioneDettaglioSpesaBulk() {
 		super();
+		setAspectName(MissioniCMISService.ASPECT_MISSIONE_SIGLA_DETTAGLIO);
 	}
 
 	public AllegatoMissioneDettaglioSpesaBulk(Document node) {
 		super(node);
+		setAspectName(MissioniCMISService.ASPECT_MISSIONE_SIGLA_DETTAGLIO);
 	}
 	
 	@CMISProperty(name=PropertyIds.SECONDARY_OBJECT_TYPE_IDS)
 	public List<String> getAspect() {
 		 List<String> results = new ArrayList<String>();
 		 results.add("P:cm:titled");
+		 if (getAspectName() != null){
+			 results.add(getAspectName());
+		 }
 		 return results;
 	}
 
@@ -33,5 +41,13 @@ public class AllegatoMissioneDettaglioSpesaBulk extends AllegatoGenericoBulk {
 			return false;
 
 		return true;
+	}
+
+	public String getAspectName() {
+		return aspectName;
+	}
+
+	public void setAspectName(String aspectName) {
+		this.aspectName = aspectName;
 	}
 }
