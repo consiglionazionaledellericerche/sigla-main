4c45f34b0711d84ce0dd7cbc3038ca7f53b8b8c1
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-04-11 15:16:06 +0200

    Rilascio gestione allegati su Missioni

--- sigla-ejb/src/main/java/it/cnr/contab/missioni00/docs/bulk/Missione_dettaglioHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/missioni00/docs/bulk/Missione_dettaglioHome.java
@@ -1,11 +1,6 @@
 package it.cnr.contab.missioni00.docs.bulk;
 
-import org.springframework.util.StringUtils;
-
-import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
-import it.cnr.jada.persistency.PersistencyException;
-import it.cnr.jada.persistency.Persistent;
 import it.cnr.jada.persistency.PersistentCache;
 
 public class Missione_dettaglioHome extends BulkHome {
