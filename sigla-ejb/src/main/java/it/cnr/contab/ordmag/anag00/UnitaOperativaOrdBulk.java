7343faf72f4ff941a8f586152e6b4df211b071ba
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-05-03 17:14:26 +0200

    Rilascio associazione tra unità operative

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/UnitaOperativaOrdBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/UnitaOperativaOrdBulk.java
@@ -4,12 +4,16 @@
  */
 package it.cnr.contab.ordmag.anag00;
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
+import it.cnr.jada.bulk.BulkCollection;
+import it.cnr.jada.bulk.BulkList;
 import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.util.action.CRUDBP;
 public class UnitaOperativaOrdBulk extends UnitaOperativaOrdBase {
 	/**
 	 * [UNITA_ORGANIZZATIVA Rappresentazione dei Centri di Spesa e delle Unità Organizzative in una struttura ad albero organizzata su più livelli]
 	 **/
 	private Unita_organizzativaBulk unitaOrganizzativa =  new Unita_organizzativaBulk();
+	private BulkList unitaOperativaColl = new BulkList();
 	/**
 	 * Created by BulkGenerator 2.0 [07/12/2009]
 	 * Table name: UNITA_OPERATIVA_ORD
@@ -56,7 +60,39 @@ public class UnitaOperativaOrdBulk extends UnitaOperativaOrdBase {
 		this.getUnitaOrganizzativa().setCd_unita_organizzativa(cdUnitaOrganizzativa);
 	}
 	protected OggettoBulk initialize(it.cnr.jada.util.action.CRUDBP bp,it.cnr.jada.action.ActionContext context) {
-		setCdUnitaOrganizzativa(it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getCd_unita_organizzativa());
+		impostaUnitaOrganizzativa(context);
 		return super.initialize(bp,context);
+	}
+	private void impostaUnitaOrganizzativa(it.cnr.jada.action.ActionContext context) {
+		setCdUnitaOrganizzativa(it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getCd_unita_organizzativa());
+	}
+	public OggettoBulk initializeForSearch(CRUDBP bp,it.cnr.jada.action.ActionContext context) {
+
+		super.initializeForSearch(bp,context);
+
+		impostaUnitaOrganizzativa(context);
+
+		return this;
+	}
+	public BulkList getUnitaOperativaColl() {
+		return unitaOperativaColl;
+	}
+	public void setUnitaOperativaColl(BulkList unitaOperativaColl) {
+		this.unitaOperativaColl = unitaOperativaColl;
+	}
+	public BulkCollection[] getBulkLists() {
+		 return new it.cnr.jada.bulk.BulkCollection[] { 
+				 unitaOperativaColl};
+	}
+	public int addToUnitaOperativaColl( AssUnitaOperativaOrdBulk ass) 
+	{
+		unitaOperativaColl.add(ass);
+		ass.setUnitaOperativaOrd(this);
+		return unitaOperativaColl.size()-1;
+	}
+	public AssUnitaOperativaOrdBulk removeFromUnitaOperativaColl(int index) 
+	{
+		// Gestisce la selezione del bottone cancella repertorio
+		return (AssUnitaOperativaOrdBulk)unitaOperativaColl.remove(index);
 	}
 }
