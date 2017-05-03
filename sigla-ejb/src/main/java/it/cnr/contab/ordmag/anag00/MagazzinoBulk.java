8523190f39a8d6e2bb8b2d51bc63b43b185e3909
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-05-03 09:01:20 +0200

    Varie

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/MagazzinoBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/MagazzinoBulk.java
@@ -4,6 +4,7 @@
  */
 package it.cnr.contab.ordmag.anag00;
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
+import it.cnr.jada.bulk.OggettoBulk;
 public class MagazzinoBulk extends MagazzinoBase {
 	/**
 	 * [UNITA_ORGANIZZATIVA Rappresentazione dei Centri di Spesa e delle Unità Organizzative in una struttura ad albero organizzata su più livelli]
@@ -611,5 +612,9 @@ public class MagazzinoBulk extends MagazzinoBase {
 	}
 	public void setTipoMovimentoMagChi(TipoMovimentoMagBulk tipoMovimentoMagChi) {
 		this.tipoMovimentoMagChi = tipoMovimentoMagChi;
+	}
+	protected OggettoBulk initialize(it.cnr.jada.util.action.CRUDBP bp,it.cnr.jada.action.ActionContext context) {
+		setCdCds(it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getCd_cds());
+		return super.initialize(bp,context);
 	}
 }
