dc41f5dc040911b87f208ebf963b0dcfd7328956
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-05-18 09:20:02 +0200

    Varie

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/TipoOperazioneOrdBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/TipoOperazioneOrdBulk.java
@@ -7,6 +7,8 @@ import it.cnr.jada.action.ActionContext;
 import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.util.action.CRUDBP;
 public class TipoOperazioneOrdBulk extends TipoOperazioneOrdBase {
+    public final static String OPERAZIONE_RICHIESTA= "RIC";
+    public final static String OPERAZIONE_ORDINE= "ORD";
 	/**
 	 * Created by BulkGenerator 2.0 [07/12/2009]
 	 * Table name: TIPO_OPERAZIONE_ORD
