6bbea4955d6eaf606ab81c69c6de23a43cf5100d
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-05-21 23:18:19 +0200

    Varie

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/richieste/bulk/RichiestaUopRigaHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/richieste/bulk/RichiestaUopRigaHome.java
@@ -5,13 +5,23 @@
 package it.cnr.contab.ordmag.richieste.bulk;
 import java.sql.Connection;
 
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
 public class RichiestaUopRigaHome extends BulkHome {
 	public RichiestaUopRigaHome(Connection conn) {
 		super(RichiestaUopRigaBulk.class, conn);
 	}
 	public RichiestaUopRigaHome(Connection conn, PersistentCache persistentCache) {
 		super(RichiestaUopRigaBulk.class, conn, persistentCache);
+	}
+	@Override
+	public SQLBuilder selectByClause(UserContext usercontext, CompoundFindClause compoundfindclause)
+			throws PersistencyException {
+		// TODO Auto-generated method stub
+		return super.selectByClause(usercontext, compoundfindclause);
 	}
 }
