ea30077324ebebb90e30f0f401a1832d56c954e3
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-06-07 11:44:29 +0200

    Varie

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/richieste/ejb/RichiestaUopComponentSessionBean.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/richieste/ejb/RichiestaUopComponentSessionBean.java
@@ -39,6 +39,27 @@ public Boolean isUtenteAbilitatoRichiesta(UserContext usercontext, RichiestaUopB
 		throw uncaughtError(usercontext,componentObj,e);
 	}
 }
+public Boolean isUtenteAbilitatoValidazioneRichiesta(UserContext usercontext, RichiestaUopBulk richiesta) throws ComponentException, PersistencyException,javax.ejb.EJBException {
+	pre_component_invocation(usercontext,componentObj);
+	try {
+		Boolean result = ((RichiestaUopComponent)componentObj).isUtenteAbilitatoValidazioneRichiesta(usercontext, richiesta);
+		component_invocation_succes(usercontext,componentObj);
+		return result;
+	} catch(it.cnr.jada.comp.NoRollbackException e) {
+		component_invocation_succes(usercontext,componentObj);
+		throw e;
+	} catch(it.cnr.jada.comp.ComponentException e) {
+		component_invocation_failure(usercontext,componentObj);
+		throw e;
+	} catch(it.cnr.jada.persistency.PersistencyException e) {
+		component_invocation_failure(usercontext,componentObj);
+		throw e;
+	} catch(RuntimeException e) {
+		throw uncaughtRuntimeException(usercontext,componentObj,e);
+	} catch(Error e) {
+		throw uncaughtError(usercontext,componentObj,e);
+	}
+}
 public void gestioneStampaRichiesta(UserContext userContext, RichiestaUopBulk richiesta) throws it.cnr.jada.comp.ComponentException,javax.ejb.EJBException, RemoteException {
 	pre_component_invocation(userContext,componentObj);
 	try {
@@ -49,6 +70,20 @@ public void gestioneStampaRichiesta(UserContext userContext, RichiestaUopBulk ri
 		throw e;
 	} catch(it.cnr.jada.comp.ComponentException e) {
 		component_invocation_failure(userContext,componentObj);
+		throw e;
+	} catch(RuntimeException e) {
+		throw uncaughtRuntimeException(userContext,componentObj,e);
+	} catch(Error e) {
+		throw uncaughtError(userContext,componentObj,e);
+	}
+}
+public void completaRichiesta(UserContext userContext, RichiestaUopBulk richiesta) throws PersistencyException,javax.ejb.EJBException, RemoteException {
+	pre_component_invocation(userContext,componentObj);
+	try {
+		((RichiestaUopComponent)componentObj).completaRichiesta(userContext, richiesta);
+		component_invocation_succes(userContext,componentObj);
+	} catch(it.cnr.jada.comp.NoRollbackException e) {
+		component_invocation_succes(userContext,componentObj);
 		throw e;
 	} catch(RuntimeException e) {
 		throw uncaughtRuntimeException(userContext,componentObj,e);
