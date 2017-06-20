5ad8a02d2a0f9331062dc5d857590e9b60ea4489
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-06-20 09:39:04 +0200

    Gestione associazione Obbligazione Scadenzario Voce per le Missioni

---
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/core/DatiFinanziariScadenzeDTO.java
@@ -0,0 +1,50 @@
+package it.cnr.contab.doccont00.core;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class DatiFinanziariScadenzeDTO {
+	BigDecimal nuovoImportoScadenzaVecchia;
+	String nuovaDescrizione;
+	Timestamp nuovaScadenza;
+	String cdLineaAttivita;
+	String cdVoce;
+	String cdCentroResponsabilita;
+	
+	public BigDecimal getNuovoImportoScadenzaVecchia() {
+		return nuovoImportoScadenzaVecchia;
+	}
+	public void setNuovoImportoScadenzaVecchia(BigDecimal nuovoImportoScadenzaVecchia) {
+		this.nuovoImportoScadenzaVecchia = nuovoImportoScadenzaVecchia;
+	}
+	public String getNuovaDescrizione() {
+		return nuovaDescrizione;
+	}
+	public void setNuovaDescrizione(String nuovaDescrizione) {
+		this.nuovaDescrizione = nuovaDescrizione;
+	}
+	public Timestamp getNuovaScadenza() {
+		return nuovaScadenza;
+	}
+	public void setNuovaScadenza(Timestamp nuovaScadenza) {
+		this.nuovaScadenza = nuovaScadenza;
+	}
+	public String getCdLineaAttivita() {
+		return cdLineaAttivita;
+	}
+	public void setCdLineaAttivita(String cdLineaAttivita) {
+		this.cdLineaAttivita = cdLineaAttivita;
+	}
+	public String getCdVoce() {
+		return cdVoce;
+	}
+	public void setCdVoce(String cdVoce) {
+		this.cdVoce = cdVoce;
+	}
+	public String getCdCentroResponsabilita() {
+		return cdCentroResponsabilita;
+	}
+	public void setCdCentroResponsabilita(String cdCentroResponsabilita) {
+		this.cdCentroResponsabilita = cdCentroResponsabilita;
+	}
+}
