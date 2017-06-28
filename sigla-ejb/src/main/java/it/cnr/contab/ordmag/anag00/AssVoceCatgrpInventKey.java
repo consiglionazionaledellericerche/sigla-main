7c6093f378687a2c350783fb9380843d61b82eda
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-06-28 09:33:20 +0200

    Varie

---
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/AssVoceCatgrpInventKey.java
@@ -0,0 +1,75 @@
+/*
+ * Created by Aurelio's BulkGenerator 1.0
+ * Date 07/05/2007
+ */
+package it.cnr.contab.ordmag.anag00;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.persistency.KeyedPersistent;
+public class AssVoceCatgrpInventKey extends OggettoBulk implements KeyedPersistent {
+	private java.lang.Integer esercizio;
+	private java.lang.String ti_appartenenza;
+	private java.lang.String ti_gestione;
+	private java.lang.String cd_elemento_voce;
+	private java.lang.String cd_categoria_gruppo;
+	public AssVoceCatgrpInventKey() {
+		super();
+	}
+	public AssVoceCatgrpInventKey(java.lang.Integer esercizio, java.lang.String ti_appartenenza, java.lang.String ti_gestione, java.lang.String cd_elemento_voce, java.lang.String cd_categoria_gruppo) {
+		super();
+		this.esercizio=esercizio;
+		this.ti_appartenenza=ti_appartenenza;
+		this.ti_gestione=ti_gestione;
+		this.cd_elemento_voce=cd_elemento_voce;
+		this.cd_categoria_gruppo=cd_categoria_gruppo;
+	}
+	public boolean equalsByPrimaryKey(Object o) {
+		if (this== o) return true;
+		if (!(o instanceof AssVoceCatgrpInventKey)) return false;
+		AssVoceCatgrpInventKey k = (AssVoceCatgrpInventKey) o;
+		if (!compareKey(getEsercizio(), k.getEsercizio())) return false;
+		if (!compareKey(getTi_appartenenza(), k.getTi_appartenenza())) return false;
+		if (!compareKey(getTi_gestione(), k.getTi_gestione())) return false;
+		if (!compareKey(getCd_elemento_voce(), k.getCd_elemento_voce())) return false;
+		if (!compareKey(getCd_categoria_gruppo(), k.getCd_categoria_gruppo())) return false;
+		return true;
+	}
+	public int primaryKeyHashCode() {
+		int i = 0;
+		i = i + calculateKeyHashCode(getEsercizio());
+		i = i + calculateKeyHashCode(getTi_appartenenza());
+		i = i + calculateKeyHashCode(getTi_gestione());
+		i = i + calculateKeyHashCode(getCd_elemento_voce());
+		i = i + calculateKeyHashCode(getCd_categoria_gruppo());
+		return i;
+	}
+	public void setEsercizio(java.lang.Integer esercizio)  {
+		this.esercizio=esercizio;
+	}
+	public java.lang.Integer getEsercizio() {
+		return esercizio;
+	}
+	public void setTi_appartenenza(java.lang.String ti_appartenenza)  {
+		this.ti_appartenenza=ti_appartenenza;
+	}
+	public java.lang.String getTi_appartenenza() {
+		return ti_appartenenza;
+	}
+	public void setTi_gestione(java.lang.String ti_gestione)  {
+		this.ti_gestione=ti_gestione;
+	}
+	public java.lang.String getTi_gestione() {
+		return ti_gestione;
+	}
+	public void setCd_elemento_voce(java.lang.String cd_elemento_voce)  {
+		this.cd_elemento_voce=cd_elemento_voce;
+	}
+	public java.lang.String getCd_elemento_voce() {
+		return cd_elemento_voce;
+	}
+	public void setCd_categoria_gruppo(java.lang.String cd_categoria_gruppo)  {
+		this.cd_categoria_gruppo=cd_categoria_gruppo;
+	}
+	public java.lang.String getCd_categoria_gruppo() {
+		return cd_categoria_gruppo;
+	}
+}
