93b15b9ba0cf0d14b797c26644f7f83838cc94a2
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-07-03 17:00:55 +0200

    Varie per gestione ordini

---
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/ordini/bulk/OrdineAcqRigaKey.java
@@ -0,0 +1,137 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 28/06/2017
+ */
+package it.cnr.contab.ordmag.ordini.bulk;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.persistency.KeyedPersistent;
+public class OrdineAcqRigaKey extends OggettoBulk implements KeyedPersistent {
+	private java.lang.String cdCds;
+	private java.lang.String cdUnitaOperativa;
+	private java.lang.Integer esercizio;
+	private java.lang.String cdNumeratore;
+	private java.lang.Integer numero;
+	private java.lang.Integer riga;
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: ORDINE_ACQ_RIGA
+	 **/
+	public OrdineAcqRigaKey() {
+		super();
+	}
+	public OrdineAcqRigaKey(java.lang.String cdCds, java.lang.String cdUnitaOperativa, java.lang.Integer esercizio, java.lang.String cdNumeratore, java.lang.Integer numero, java.lang.Integer riga) {
+		super();
+		this.cdCds=cdCds;
+		this.cdUnitaOperativa=cdUnitaOperativa;
+		this.esercizio=esercizio;
+		this.cdNumeratore=cdNumeratore;
+		this.numero=numero;
+		this.riga=riga;
+	}
+	public boolean equalsByPrimaryKey(Object o) {
+		if (this== o) return true;
+		if (!(o instanceof OrdineAcqRigaKey)) return false;
+		OrdineAcqRigaKey k = (OrdineAcqRigaKey) o;
+		if (!compareKey(getCdCds(), k.getCdCds())) return false;
+		if (!compareKey(getCdUnitaOperativa(), k.getCdUnitaOperativa())) return false;
+		if (!compareKey(getEsercizio(), k.getEsercizio())) return false;
+		if (!compareKey(getCdNumeratore(), k.getCdNumeratore())) return false;
+		if (!compareKey(getNumero(), k.getNumero())) return false;
+		if (!compareKey(getRiga(), k.getRiga())) return false;
+		return true;
+	}
+	public int primaryKeyHashCode() {
+		int i = 0;
+		i = i + calculateKeyHashCode(getCdCds());
+		i = i + calculateKeyHashCode(getCdUnitaOperativa());
+		i = i + calculateKeyHashCode(getEsercizio());
+		i = i + calculateKeyHashCode(getCdNumeratore());
+		i = i + calculateKeyHashCode(getNumero());
+		i = i + calculateKeyHashCode(getRiga());
+		return i;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdCds]
+	 **/
+	public void setCdCds(java.lang.String cdCds)  {
+		this.cdCds=cdCds;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdCds]
+	 **/
+	public java.lang.String getCdCds() {
+		return cdCds;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdUnitaOperativa]
+	 **/
+	public void setCdUnitaOperativa(java.lang.String cdUnitaOperativa)  {
+		this.cdUnitaOperativa=cdUnitaOperativa;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdUnitaOperativa]
+	 **/
+	public java.lang.String getCdUnitaOperativa() {
+		return cdUnitaOperativa;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [esercizio]
+	 **/
+	public void setEsercizio(java.lang.Integer esercizio)  {
+		this.esercizio=esercizio;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [esercizio]
+	 **/
+	public java.lang.Integer getEsercizio() {
+		return esercizio;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [cdNumeratore]
+	 **/
+	public void setCdNumeratore(java.lang.String cdNumeratore)  {
+		this.cdNumeratore=cdNumeratore;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [cdNumeratore]
+	 **/
+	public java.lang.String getCdNumeratore() {
+		return cdNumeratore;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [numero]
+	 **/
+	public void setNumero(java.lang.Integer numero)  {
+		this.numero=numero;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [numero]
+	 **/
+	public java.lang.Integer getNumero() {
+		return numero;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [riga]
+	 **/
+	public void setRiga(java.lang.Integer riga)  {
+		this.riga=riga;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [riga]
+	 **/
+	public java.lang.Integer getRiga() {
+		return riga;
+	}
+}
