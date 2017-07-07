30536f0447e21b07d93f77961af87e94523767e2
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-07-07 08:39:36 +0200

    Varie

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/AbilUtenteUopOperMagHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/AbilUtenteUopOperMagHome.java
@@ -4,8 +4,13 @@
  */
 package it.cnr.contab.ordmag.anag00;
 import java.sql.Connection;
+
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.persistency.IntrospectionException;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.SQLBuilder;
 public class AbilUtenteUopOperMagHome extends BulkHome {
 	public AbilUtenteUopOperMagHome(Connection conn) {
 		super(AbilUtenteUopOperMagBulk.class, conn);
@@ -13,4 +18,13 @@ public class AbilUtenteUopOperMagHome extends BulkHome {
 	public AbilUtenteUopOperMagHome(Connection conn, PersistentCache persistentCache) {
 		super(AbilUtenteUopOperMagBulk.class, conn, persistentCache);
 	}
+	public java.util.Collection findMagazziniAbilitati(UserContext userContext, String tipoOperazione, 
+			UnitaOperativaOrdBulk unitaOperativaBulk) throws IntrospectionException, PersistencyException {	
+		SQLBuilder sql = createSQLBuilder();
+		sql.addSQLClause("AND","TIPO_OPERAZIONE",sql.EQUALS,tipoOperazione);
+		sql.addSQLClause("AND","CD_UTENTE",sql.EQUALS,userContext.getUser());
+		sql.addSQLClause("AND","CD_UNITA_OPERATIVA",sql.EQUALS,unitaOperativaBulk.getCdUnitaOperativa());
+		return fetchAll(sql);
+	}
+	
 }
