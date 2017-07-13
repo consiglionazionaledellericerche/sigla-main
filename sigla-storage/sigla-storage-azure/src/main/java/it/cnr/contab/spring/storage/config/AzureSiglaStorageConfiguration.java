3118a445ffba8ed0e332988895eb0655ace6008f
Francesco Uliana <francesco.uliana@cnr.it>
2017-07-13 15:41:33 +0200

    azure

---
+++ sigla-storage/sigla-storage-azure/src/main/java/it/cnr/contab/spring/storage/config/AzureSiglaStorageConfiguration.java
@@ -0,0 +1,28 @@
+package it.cnr.contab.spring.storage.config;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by mspasiano on 6/5/17.
+ */
+@Configuration
+@Profile("Azure")
+public class AzureSiglaStorageConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureSiglaStorageConfiguration.class);
+
+
+}
---
+++ sigla-storage/sigla-storage-azure/src/main/java/it/cnr/contab/spring/storage/config/AzureSiglaStorageConfigurationProperties.java
@@ -0,0 +1,15 @@
+package it.cnr.contab.spring.storage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by mspasiano on 6/5/17.
+ */
+@Configuration
+@Profile("Azure")
+public class AzureSiglaStorageConfigurationProperties {
+
+
+
+}
