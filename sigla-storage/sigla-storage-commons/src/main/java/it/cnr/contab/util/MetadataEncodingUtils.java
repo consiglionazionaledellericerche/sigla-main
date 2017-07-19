ca413d49f087bb6b906f600e47a11cff076dbb72
Francesco Uliana <francesco.uliana@cnr.it>
2017-07-19 16:33:50 +0200

    refactoring MetadataEncodingUtils

--- sigla-storage/sigla-storage-commons/src/main/java/it/cnr/contab/util/MetadataEncodingUtils.java
+++ sigla-storage/sigla-storage-commons/src/main/java/it/cnr/contab/util/MetadataEncodingUtils.java
@@ -18,73 +18,7 @@ public class MetadataEncodingUtils {
     private static final String UTF_8 = "UTF-8";
     private static final String PREFIX = "CNR_";
 
-
     /**
-     *
-     * Decode metadata value
-     *
-     * @param input encoded string (${md5(value)}|${b64(value)}) to decode
-     * @return decoded value
-     */
-    public static String decodeValue(String input) {
-        String [] a = input.split("\\|");
-        Assert.isTrue(2 == a.length, "invalid metadata: " + input);
-        byte[] decoded = Base64.getDecoder().decode(a[1]);
-        try {
-            String decodedValue = new String(decoded, UTF_8);
-            LOGGER.info("{} decoded to {}", input, decodedValue);
-            Assert.isTrue(a[0].equals(DigestUtils.md5Hex(decodedValue).toUpperCase()), "integrity issue with input "+ input);
-            return decodedValue;
-        } catch (UnsupportedEncodingException e) {
-            throw new StorageException(StorageException.Type.GENERIC, "cannot decode " + input, e);
-        }
-    }
-
-    /**
-     *
-     * Decode a multi-valued metadata field
-     *
-     * @param input encoded string (made by a concatenation of encoded strings)
-     * @return a list of decoded values
-     */
-    public static List<String> decodeValues(String input) {
-
-        return Optional.ofNullable(input)
-                .map(s -> Arrays
-                        .stream(s.split(","))
-                        .map(MetadataEncodingUtils::decodeValue)
-                        .collect(Collectors.toList())
-                )
-                .orElse(Collections.emptyList());
-    }
-
-
-    /**
-     *
-     * Encode metadata key
-     *
-     * @param input metadata key
-     * @return encoded metadata key (CNR_${base64(key)})
-     */
-    public static String encodeKey(String input) {
-        byte[] bytes;
-        try {
-            bytes = input.getBytes(UTF_8);
-        } catch (UnsupportedEncodingException e) {
-            throw new StorageException(StorageException.Type.GENERIC, "cannot encode key " + input, e);
-        }
-        String suffix = Base64
-                .getEncoder()
-                .encodeToString(bytes)
-                .replaceAll("=+$", "");
-        String b64encodedKey = String.format("%s%s", PREFIX, suffix);
-        LOGGER.info("key {} encoded to {}", input, b64encodedKey);
-        return b64encodedKey;
-
-    }
-
-    /**
-     *
      * Encode metadata value
      *
      * @param value metadata to encode
@@ -111,9 +45,28 @@ public class MetadataEncodingUtils {
         return output;
     }
 
+    /**
+     * Decode metadata value
+     *
+     * @param input encoded string (${md5(value)}|${b64(value)}) to decode
+     * @return decoded value
+     */
+    public static String decodeValue(String input) {
+        String[] a = input.split("\\|");
+        Assert.isTrue(2 == a.length, "invalid metadata: " + input);
+        byte[] decoded = Base64.getDecoder().decode(a[1]);
+        try {
+            String decodedValue = new String(decoded, UTF_8);
+            LOGGER.info("{} decoded to {}", input, decodedValue);
+            Assert.isTrue(a[0].equals(DigestUtils.md5Hex(decodedValue).toUpperCase()), "integrity issue with input " + input);
+            return decodedValue;
+        } catch (UnsupportedEncodingException e) {
+            throw new StorageException(StorageException.Type.GENERIC, "cannot decode " + input, e);
+        }
+    }
+
 
     /**
-     *
      * Encode a list of metadata values
      *
      * @param entryValue list of metadata values
@@ -132,12 +85,52 @@ public class MetadataEncodingUtils {
 
     }
 
+    /**
+     * Decode a multi-valued metadata field
+     *
+     * @param input encoded string (made by a concatenation of encoded strings)
+     * @return a list of decoded values
+     */
+    public static List<String> decodeValues(String input) {
+
+        return Optional.ofNullable(input)
+                .map(s -> Arrays
+                        .stream(s.split(","))
+                        .map(MetadataEncodingUtils::decodeValue)
+                        .collect(Collectors.toList())
+                )
+                .orElse(Collections.emptyList());
+    }
+
 
     /**
+     * Encode metadata key
      *
+     * @param input metadata key
+     * @return encoded metadata key (i.e. ${PREFIX}${base64(key)})
+     */
+    public static String encodeKey(String input) {
+        byte[] bytes;
+        try {
+            bytes = input.getBytes(UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            throw new StorageException(StorageException.Type.GENERIC, "cannot encode key " + input, e);
+        }
+        String suffix = Base64
+                .getEncoder()
+                .encodeToString(bytes)
+                .replaceAll("=+$", "");
+        String b64encodedKey = String.format("%s%s", PREFIX, suffix);
+        LOGGER.info("key {} encoded to {}", input, b64encodedKey);
+        return b64encodedKey;
+
+    }
+
+
+    /**
      * Decode a metadata key
      *
-     * @param input metadata key such as CNR_${base64(key)}
+     * @param input metadata key such as ${PREFIX}${base64(key)}
      * @return decoded metadata key
      */
     public static String decodeKey(String input) {
@@ -150,8 +143,6 @@ public class MetadataEncodingUtils {
         } catch (UnsupportedEncodingException e) {
             throw new StorageException(StorageException.Type.GENERIC, "cannot decode " + input, e);
         }
-
-
     }
 
 }
