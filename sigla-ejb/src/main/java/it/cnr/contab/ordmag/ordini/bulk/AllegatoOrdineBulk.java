50700842584d29f4607f71a8f045aa9a99d21122
marco.spasiano <marco.spasiano@cnr.it>
2017-09-20 16:29:22 +0200

    Merge master to iss

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/ordini/bulk/AllegatoOrdineBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/ordini/bulk/AllegatoOrdineBulk.java
@@ -3,13 +3,9 @@ package it.cnr.contab.ordmag.ordini.bulk;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.chemistry.opencmis.client.api.Document;
-import org.apache.chemistry.opencmis.commons.PropertyIds;
-
-import it.cnr.contab.cmis.annotation.CMISProperty;
+import it.cnr.contab.spring.storage.annotation.StorageProperty;
+import it.cnr.contab.util00.bulk.storage.AllegatoGenericoBulk;
 import it.cnr.contab.ordmag.ordini.service.OrdineAcqCMISService;
-import it.cnr.contab.ordmag.richieste.service.RichiesteCMISService;
-import it.cnr.contab.util00.bulk.cmis.AllegatoGenericoBulk;
 import it.cnr.jada.bulk.ValidationException;
 import it.cnr.jada.util.OrderedHashtable;
 
@@ -28,8 +24,8 @@ public class AllegatoOrdineBulk extends AllegatoGenericoBulk {
 		setAspectName(OrdineAcqCMISService.ASPECT_ALLEGATI_ORDINI);
 	}
 
-	public AllegatoOrdineBulk(Document node) {
-		super(node);
+	public AllegatoOrdineBulk(String storageKey) {
+		super(storageKey);
 		setAspectName(OrdineAcqCMISService.ASPECT_ALLEGATI_ORDINI);
 	}
 	
@@ -39,7 +35,8 @@ public class AllegatoOrdineBulk extends AllegatoGenericoBulk {
 	public void setAspectName(String aspectName) {
 		this.aspectName = aspectName;
 	}
-	@CMISProperty(name=PropertyIds.SECONDARY_OBJECT_TYPE_IDS)
+
+	@StorageProperty(name="cmis:secondaryObjectTypeIds")
 	public List<String> getAspect() {
 		 List<String> results = new ArrayList<String>();
 		 results.add("P:cm:titled");
