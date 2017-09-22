34ed11c330ecf491c0c45c7d7c161900baee2211
SPASIANO MARCO <marco.spasiano@cnr.it>
2017-09-22 14:45:44 +0200

    Gestione errore di file già presente

--- sigla-ejb/src/main/java/it/cnr/contab/util00/bp/AllegatiCRUDBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/util00/bp/AllegatiCRUDBP.java
@@ -1,6 +1,7 @@
 package it.cnr.contab.util00.bp;
 
 import it.cnr.contab.service.SpringUtil;
+import it.cnr.contab.spring.storage.StorageException;
 import it.cnr.contab.spring.storage.StorageObject;
 import it.cnr.contab.spring.storage.StoreService;
 import it.cnr.contab.spring.storage.config.StoragePropertyNames;
@@ -238,6 +239,10 @@ public abstract class AllegatiCRUDBP<T extends AllegatoGenericoBulk, K extends A
                                     true));
                     allegato.setCrudStatus(OggettoBulk.NORMAL);
                 } catch (FileNotFoundException e) {
+                    throw handleException(e);
+                } catch (StorageException e) {
+                    if (e.getType().equals(StorageException.Type.CONSTRAINT_VIOLATED))
+                        throw new ApplicationException("File ["+allegato.getNome()+"] gia' presente. Inserimento non possibile!");
                     throw handleException(e);
                 }
             }else if (allegato.isToBeUpdated()) {
