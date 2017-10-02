81e0c6e700a117a609b9c54c2850a24005bdb537
rosangela.pucciarelli <rosangela.pucciarelli@cnr.it>
2017-10-02 09:14:17 +0200

    - Spesometro 2017 - Risoluzione anomalie

---
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/docs/bulk/VSpesometroNewHome.java
@@ -0,0 +1,23 @@
+package it.cnr.contab.docamm00.docs.bulk;
+import java.sql.Connection;
+
+import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.jada.UserContext;
+import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.persistency.PersistencyException;
+import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+public class VSpesometroNewHome extends BulkHome {
+	public VSpesometroNewHome(Connection conn) {
+		super(VSpesometroNewBulk.class, conn);
+	}
+	public VSpesometroNewHome(Connection conn, PersistentCache persistentCache) {
+		super(VSpesometroNewBulk.class, conn, persistentCache);
+	}
+	public SQLBuilder selectByClause(UserContext usercontext, CompoundFindClause compoundfindclause) throws PersistencyException {
+		SQLBuilder sql = super.selectByClause(usercontext, compoundfindclause);
+	    sql.addClause("AND","esercizio",SQLBuilder.EQUALS, CNRUserContext.getEsercizio(usercontext));
+		return sql;
+	}
+}
