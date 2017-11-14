733112aaa5feaaff8f9f38eafe3736a1a7c8e7cb
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-11-14 16:44:39 +0100

    Corretto recupero progetti per servizio REST

--- sigla-ejb/src/main/java/it/cnr/contab/progettiric00/core/bulk/ProgettoGestHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/progettiric00/core/bulk/ProgettoGestHome.java
@@ -347,16 +347,34 @@ public class ProgettoGestHome extends BulkHome implements ConsultazioniRestHome 
 					if (clause.getPropertyName() != null && clause.getPropertyName().equals("cd_unita_organizzativa") &&
 							operator == SQLBuilder.EQUALS){
 						trovataCondizioneUo = true;
+						
+						Unita_organizzativaBulk uo = (Unita_organizzativaBulk)getHomeCache().getHome(Unita_organizzativaBulk.class).findByPrimaryKey(new Unita_organizzativaBulk((String)clause.getValue()));
+						
 						ProgettoHome progettoHome = (ProgettoHome) getHomeCache().getHome(ProgettoBulk.class);
 						sqlExists = progettoHome.createSQLBuilder();
 						sqlExists.addTableToHeader("V_ABIL_PROGETTI");
+
+						sqlExists.addTableToHeader("UNITA_ORGANIZZATIVA");
+						sqlExists.addSQLJoin("UNITA_ORGANIZZATIVA.CD_UNITA_ORGANIZZATIVA", "V_ABIL_PROGETTI.CD_UNITA_ORGANIZZATIVA");
+						sqlExists.openParenthesis("AND");		  
+						sqlExists.addSQLClause("AND","UNITA_ORGANIZZATIVA.CD_UNITA_PADRE",SQLBuilder.EQUALS,uo.getUnita_padre().getCd_unita_organizzativa());
+						if (uo.getCd_tipo_unita().compareTo(it.cnr.contab.config00.sto.bulk.Tipo_unita_organizzativaHome.TIPO_UO_AREA)==0){
+							SQLBuilder sqlArea = getHomeCache().getHome(Ass_uo_areaBulk.class).createSQLBuilder();
+							sqlArea.addTableToHeader("UNITA_ORGANIZZATIVA UO");
+							sqlArea.addSQLJoin("UNITA_ORGANIZZATIVA.CD_UNITA_PADRE", "UO.CD_UNITA_PADRE");
+							sqlArea.addSQLJoin("ASS_UO_AREA.CD_UNITA_ORGANIZZATIVA", "UO.CD_UNITA_ORGANIZZATIVA");
+							sqlArea.addSQLClause("AND","ASS_UO_AREA.CD_AREA_RICERCA",SQLBuilder.EQUALS,uo.getUnita_padre().getCd_unita_organizzativa());
+							sqlArea.addSQLClause("AND","ASS_UO_AREA.ESERCIZIO",SQLBuilder.EQUALS,CNRUserContext.getEsercizio(userContext));
+							sqlExists.addSQLExistsClause("OR",sqlArea);
+						}
+						sqlExists.closeParenthesis();
+
 						sqlExists.addSQLJoin("PROGETTO.ESERCIZIO", "PROGETTO_GEST.ESERCIZIO");
 						sqlExists.addSQLJoin("PROGETTO.TIPO_FASE", "PROGETTO_GEST.TIPO_FASE");
 						sqlExists.addSQLJoin("PROGETTO.PG_PROGETTO", "PROGETTO_GEST.PG_PROGETTO");
 						sqlExists.addSQLJoin("PROGETTO_GEST.ESERCIZIO", "V_ABIL_PROGETTI.ESERCIZIO_COMMESSA");
 						sqlExists.addSQLJoin("PROGETTO_GEST.TIPO_FASE", "V_ABIL_PROGETTI.TIPO_FASE_COMMESSA");
 						sqlExists.addSQLJoin("PROGETTO_GEST.PG_PROGETTO", "V_ABIL_PROGETTI.PG_COMMESSA");
-						sqlExists.addSQLClause("AND","V_ABIL_PROGETTI.CD_UNITA_ORGANIZZATIVA", SQLBuilder.EQUALS, clause.getValue());
 					} else {
 						newClauses.addClause(clause.getLogicalOperator(), clause.getPropertyName(), clause.getOperator(), clause.getValue());
 					}
