41067115efef86f8ccdf0e83fc549939bf70f5c0
marco.spasiano <marco.spasiano@cnr.it>
2017-12-13 11:08:57 +0100

    Aggiunta tab dei documenti amministrativi collegati al mandato con i relativi allegati gestiti Missioni e Fatture

--- sigla-ejb/src/main/java/it/cnr/contab/util00/bulk/storage/AllegatoParentBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/util00/bulk/storage/AllegatoParentBulk.java
@@ -1,6 +1,5 @@
 package it.cnr.contab.util00.bulk.storage;
 
-import it.cnr.contab.util00.bulk.storage.AllegatoGenericoBulk;
 import it.cnr.jada.bulk.BulkList;
 
 public interface AllegatoParentBulk {
---
+++ sigla-ejb/src/main/java/it/cnr/contab/util00/bulk/storage/AllegatoStorePath.java
@@ -0,0 +1,7 @@
+package it.cnr.contab.util00.bulk.storage;
+
+import java.util.List;
+
+public interface AllegatoStorePath {
+    List<String> getStorePath();
+}
