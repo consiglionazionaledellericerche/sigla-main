cc4bfb6dcbe3a289f26d5f0bcfbbb3beafebb26b
rosangela.pucciarelli <rosangela.pucciarelli@cnr.it>
2018-01-02 11:30:45 +0100

    Modifiche gestione partite di giro senza voce di contropartita definita

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/core/bulk/Ass_partita_giroHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/core/bulk/Ass_partita_giroHome.java
@@ -1,5 +1,7 @@
 package it.cnr.contab.doccont00.core.bulk;
 
+import it.cnr.contab.config00.bulk.Parametri_cnrBulk;
+import it.cnr.contab.config00.bulk.Parametri_cnrHome;
 import it.cnr.jada.bulk.*;
 import it.cnr.jada.persistency.*;
 import it.cnr.jada.persistency.beans.*;
@@ -28,6 +30,9 @@ public Ass_partita_giroHome(java.sql.Connection conn,PersistentCache persistentC
 public Ass_partita_giroBulk getAssociazionePGiroFor(AccertamentoPGiroBulk accert_pgiro) 
 	throws PersistencyException, it.cnr.jada.comp.ApplicationException {
 
+	PersistentHome parCNRHome = getHomeCache().getHome(Parametri_cnrBulk.class);
+	Parametri_cnrBulk parCNR = (Parametri_cnrBulk)parCNRHome.findByPrimaryKey(new Parametri_cnrBulk(accert_pgiro.getEsercizio()));
+	
 	SQLBuilder sql = createSQLBuilder();
 	sql.addClause("AND", "ti_appartenenza", sql.EQUALS, accert_pgiro.getTi_appartenenza());
 	sql.addClause("AND", "ti_gestione", sql.EQUALS, accert_pgiro.getTi_gestione());
@@ -36,14 +41,20 @@ public Ass_partita_giroBulk getAssociazionePGiroFor(AccertamentoPGiroBulk accert
  
 	java.util.List result = fetchAll(sql);
 	if (result.size() == 0)
-		throw new it.cnr.jada.comp.ApplicationException("Non esiste associazione fra capitoli di entrata e capitoli di spesa su Partite di giro");
+		if(parCNR.getFl_nuova_gestione_pg().booleanValue())
+			throw new it.cnr.jada.comp.ApplicationException("Non esiste associazione fra capitoli di entrata e capitoli di spesa su Partite di giro, indicare la voce del Piano Contr.");
+		else
+			throw new it.cnr.jada.comp.ApplicationException("Non esiste associazione fra capitoli di entrata e capitoli di spesa su Partite di giro");
+			
 	if (result.size() > 1)
 		throw new it.cnr.jada.comp.ApplicationException("Esiste più di un'associazione fra il capitolo di entrata e i capitoli di spesa su Partite di giro");
 	return (Ass_partita_giroBulk)result.get(0);
 }
 public Ass_partita_giroBulk getAssociazionePGiroFor(ImpegnoPGiroBulk imp_pgiro) 
 	throws PersistencyException, it.cnr.jada.comp.ApplicationException {
-
+	PersistentHome parCNRHome = getHomeCache().getHome(Parametri_cnrBulk.class);
+	Parametri_cnrBulk parCNR = (Parametri_cnrBulk)parCNRHome.findByPrimaryKey(new Parametri_cnrBulk(imp_pgiro.getEsercizio()));
+	
 	SQLBuilder sql = createSQLBuilder();
 	sql.addClause("AND","ti_appartenenza_clg",sql.EQUALS, imp_pgiro.getTi_appartenenza() );
 	sql.addClause("AND","ti_gestione_clg",sql.EQUALS, imp_pgiro.getTi_gestione() );
@@ -51,7 +62,10 @@ public Ass_partita_giroBulk getAssociazionePGiroFor(ImpegnoPGiroBulk imp_pgiro)
 	sql.addClause("AND","esercizio",sql.EQUALS, imp_pgiro.getEsercizio() );		
 	java.util.List result = fetchAll( sql );
 	if ( result.size() == 0 )
-		throw new it.cnr.jada.comp.ApplicationException("Non esiste associazione fra capitoli di entrata e capitoli di spesa su Partite di giro");
+		if(parCNR.getFl_nuova_gestione_pg().booleanValue())
+			throw new it.cnr.jada.comp.ApplicationException("Non esiste associazione fra capitoli di entrata e capitoli di spesa su Partite di giro, indicare la voce del Piano Contr.");
+		else
+			throw new it.cnr.jada.comp.ApplicationException("Non esiste associazione fra capitoli di entrata e capitoli di spesa su Partite di giro");
 	if ( result.size() > 1 )
 		throw new it.cnr.jada.comp.ApplicationException("Esiste più di un'associazione fra il capitolo di spesa e i capitoli di entrata su Partite di giro");			
 	return (Ass_partita_giroBulk) result.get(0);
