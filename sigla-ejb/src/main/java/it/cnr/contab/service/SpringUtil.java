a53a8864e1d55518e05bd1ae92c0b25020c92858
marco.spasiano <marco.spasiano@cnr.it>
2018-01-31 11:17:42 +0100

    Change init of spring applicationContext

--- sigla-ejb/src/main/java/it/cnr/contab/service/SpringUtil.java
+++ sigla-ejb/src/main/java/it/cnr/contab/service/SpringUtil.java
@@ -1,26 +1,29 @@
 package it.cnr.contab.service;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
-public final class SpringUtil {
-	private static BeanFactory beanFactory;
-
-	static {
-		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(
-				new String[] {"it/cnr/contab/service/sigla-service-context.xml"});
-		beanFactory =  (BeanFactory) appContext;
-	}
+@Component
+public class SpringUtil implements ApplicationContextAware{
+	private static ApplicationContext CONTEXT;
 
 	public static <T extends Object> T getBean(String beanName, Class<T> clazz){
-		return (T) beanFactory.getBean(beanName, clazz);
+		return (T) CONTEXT.getBean(beanName, clazz);
 	}
 
 	public static <T extends Object> T getBean(Class<T> clazz){
-		return (T) beanFactory.getBean(clazz);
+		return (T) CONTEXT.getBean(clazz);
 	}
 	public static boolean containsBean(String name) {
-		return beanFactory.containsBean(name);
+		return CONTEXT.containsBean(name);
 	}
 
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		CONTEXT = applicationContext;
+	}
 }
