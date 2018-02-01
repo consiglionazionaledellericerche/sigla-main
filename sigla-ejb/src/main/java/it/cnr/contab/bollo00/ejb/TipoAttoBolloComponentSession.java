af0124acfadf6715fdca2413bca8df43e48d8cc4
Pagano <raffaele.pagano@cnr.it>
2018-02-01 13:17:24 +0100

    Gestione Bollo Virtuale

---
+++ sigla-ejb/src/main/java/it/cnr/contab/bollo00/ejb/TipoAttoBolloComponentSession.java
@@ -0,0 +1,10 @@
+package it.cnr.contab.bollo00.ejb;
+
+import javax.ejb.Remote;
+
+/**
+ * Remote interface for Enterprise Bean: CNRBOLLO00_EJB_TipoAttoBolloComponentSession
+ */
+@Remote
+public interface TipoAttoBolloComponentSession extends it.cnr.jada.ejb.CRUDComponentSession {
+}
