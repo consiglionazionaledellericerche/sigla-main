3a0046b9c40e2b6b09d103109bfa715bbdfbbb24
Pagano <raffaele.pagano@cnr.it>
2018-02-09 15:43:06 +0100

    Gestione Bollo Virtuale

--- sigla-ejb/src/main/java/it/cnr/contab/bollo00/bulk/Atto_bolloBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/bollo00/bulk/Atto_bolloBulk.java
@@ -4,13 +4,18 @@ import java.util.Optional;
 
 import it.cnr.contab.bollo00.tabrif.bulk.Tipo_atto_bolloBulk;
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
+import it.cnr.contab.util00.bulk.storage.AllegatoGenericoBulk;
+import it.cnr.contab.util00.bulk.storage.AllegatoParentBulk;
+import it.cnr.jada.bulk.BulkCollection;
+import it.cnr.jada.bulk.BulkList;
 
-public class Atto_bolloBulk extends Atto_bolloBase {
+public class Atto_bolloBulk extends Atto_bolloBase implements AllegatoParentBulk {
 	private static final long serialVersionUID = 1L;
 
 	private Unita_organizzativaBulk unitaOrganizzativa;
 	private Tipo_atto_bolloBulk tipoAttoBollo;
-
+	private BulkList<AllegatoGenericoBulk> archivioAllegati = new BulkList<AllegatoGenericoBulk>();
+	
 	public Atto_bolloBulk() {
 		super();
 	}
@@ -61,5 +66,38 @@ public class Atto_bolloBulk extends Atto_bolloBase {
 	
 	public boolean isRONumFogli() {
 		return Optional.ofNullable(getNumRighe()).filter(el->el>0).isPresent();
+	}
+
+	public BulkCollection[] getBulkLists() {
+		 return new it.cnr.jada.bulk.BulkCollection[] {
+				 archivioAllegati };
+	}
+
+	public AllegatoGenericoBulk removeFromArchivioAllegati(int index) {
+		return getArchivioAllegati().remove(index);
+	}
+	
+	public int addToArchivioAllegati(AllegatoGenericoBulk allegato) {
+		archivioAllegati.add(allegato);
+		return archivioAllegati.size()-1;		
+	}
+	
+	public BulkList<AllegatoGenericoBulk> getArchivioAllegati() {
+		return archivioAllegati;
+	}
+
+	public void setArchivioAllegati(
+			BulkList<AllegatoGenericoBulk> archivioAllegati) {
+		this.archivioAllegati = archivioAllegati;
+	}
+	
+	public String getCdTipoAttoBollo(){
+		return Optional.ofNullable(getTipoAttoBollo())
+				.map(Tipo_atto_bolloBulk::getCodice)
+				.orElse(null);		
+	}
+	
+	public void setCdTipoAttoBollo(String cdTipoAttoBollo) {
+		Optional.ofNullable(getTipoAttoBollo()).ifPresent(el->el.setCodice(cdTipoAttoBollo));
 	}
 }
