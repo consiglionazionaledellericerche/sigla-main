735efd2337d98e42374df8a24047024676c6dcd5
marco.spasiano <marco.spasiano@cnr.it>
2018-02-16 09:10:19 +0100

    update Readme.md

---
+++ sigla-backend/init-user-postgres-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER sigla;
+    alter user sigla with encrypted password 'siglapw';
+    CREATE DATABASE sigladb;
+    GRANT ALL PRIVILEGES ON DATABASE sigladb TO sigla;
+EOSQL
