2d47a77ec236f36b1064c5bd517388a3bfaadd65
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-02-23 10:26:06 +0100

    Aggiunti bottoni per il download del file xml e del file firmato della fattura attiva

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/bp/CRUDFatturaAttivaIBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/bp/CRUDFatturaAttivaIBP.java
@@ -105,7 +105,7 @@ public class CRUDFatturaAttivaIBP extends CRUDFatturaAttivaBP {
 		return toolbar;
 	}
 	protected it.cnr.jada.util.jsp.Button[] createToolbar() {
-		it.cnr.jada.util.jsp.Button[] toolbar = new it.cnr.jada.util.jsp.Button[12];
+		it.cnr.jada.util.jsp.Button[] toolbar = new it.cnr.jada.util.jsp.Button[14];
 		int i = 0;
 		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(it.cnr.jada.util.action.CRUDBP.class),"CRUDToolbar.search");
 		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(it.cnr.jada.util.action.CRUDBP.class),"CRUDToolbar.startSearch");
@@ -119,6 +119,8 @@ public class CRUDFatturaAttivaIBP extends CRUDFatturaAttivaBP {
 		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(getClass()),"CRUDToolbar.riportaIndietro");
 		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(getClass()),"CRUDToolbar.riportaAvanti");
 		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(getClass()),"CRUDToolbar.documento");
+		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(getClass()),"CRUDToolbar.downloadXml");
+		toolbar[i++] = new it.cnr.jada.util.jsp.Button(it.cnr.jada.util.Config.getHandler().getProperties(getClass()),"CRUDToolbar.downloadFatturaFirmata");
 
 		return toolbar;
 	}
