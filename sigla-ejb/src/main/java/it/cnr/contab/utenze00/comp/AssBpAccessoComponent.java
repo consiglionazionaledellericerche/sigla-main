97dcc91583a91fde15f08841efc0efefe19367fa
marco.spasiano <marco.spasiano@cnr.it>
2018-03-14 10:01:17 +0100

    Gestione dei Preferiti

--- sigla-ejb/src/main/java/it/cnr/contab/utenze00/comp/AssBpAccessoComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/utenze00/comp/AssBpAccessoComponent.java
@@ -1,46 +1,64 @@
 package it.cnr.contab.utenze00.comp;
 
-import java.util.Iterator;
-import java.util.List;
-
-import it.cnr.contab.config00.sto.bulk.CdsBulk;
-import it.cnr.contab.config00.sto.bulk.CdsHome;
 import it.cnr.contab.utenze00.bulk.AccessoBulk;
 import it.cnr.contab.utenze00.bulk.AccessoHome;
-import it.cnr.contab.utenze00.bulk.AccessoKey;
 import it.cnr.contab.utenze00.bulk.AssBpAccessoBulk;
 import it.cnr.contab.utenze00.bulk.AssBpAccessoHome;
 import it.cnr.jada.UserContext;
 import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.sql.FindClause;
 import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Classe che ridefinisce alcune operazioni di CRUD su CdsBulk e Unita_organizzativaBulk
  */
 
 public class AssBpAccessoComponent extends it.cnr.jada.comp.CRUDComponent {
-	public  AssBpAccessoComponent()
-	{
-		
-	}
-public java.util.List findAccessoByBP(UserContext userContext, String bp) throws ComponentException {
-	try {
-		AssBpAccessoHome home = (AssBpAccessoHome)getHome(userContext,AssBpAccessoBulk.class);
-		SQLBuilder sql = (SQLBuilder)super.select( userContext,null,new AssBpAccessoBulk());
-		sql.addSQLClause( "AND", "business_process", sql.EQUALS, bp);
-		List lista = home.fetchAll(sql);
-		if (lista != null && !lista.isEmpty()){
-			for (Iterator<Object> i= lista.iterator(); i.hasNext();) {
-				AssBpAccessoBulk accesso= (AssBpAccessoBulk) i.next();
-				AccessoHome accessoHome = (AccessoHome)getHome(userContext, AccessoBulk.class);
-				AccessoBulk accessoBulk = (AccessoBulk)accessoHome.findByPrimaryKey(new AccessoBulk(accesso.getCdAccesso()));
-				accesso.setAccesso(accessoBulk);
-			}
-		}
+    public AssBpAccessoComponent() {
 
-		return lista;
-	} catch (it.cnr.jada.persistency.PersistencyException e) {
-		throw new ComponentException(e);
-	}
-}
+    }
+
+    public java.util.List findAccessoByBP(UserContext userContext, String bp) throws ComponentException {
+        try {
+            AssBpAccessoHome home = (AssBpAccessoHome) getHome(userContext, AssBpAccessoBulk.class);
+            SQLBuilder sql = (SQLBuilder) super.select(userContext, null, new AssBpAccessoBulk());
+            sql.addSQLClause("AND", "business_process", sql.EQUALS, bp);
+            List lista = home.fetchAll(sql);
+            if (lista != null && !lista.isEmpty()) {
+                for (Iterator<Object> i = lista.iterator(); i.hasNext(); ) {
+                    AssBpAccessoBulk accesso = (AssBpAccessoBulk) i.next();
+                    AccessoHome accessoHome = (AccessoHome) getHome(userContext, AccessoBulk.class);
+                    AccessoBulk accessoBulk = (AccessoBulk) accessoHome.findByPrimaryKey(new AccessoBulk(accesso.getCdAccesso()));
+                    accesso.setAccesso(accessoBulk);
+                }
+            }
+            return lista;
+        } catch (it.cnr.jada.persistency.PersistencyException e) {
+            throw new ComponentException(e);
+        }
+    }
+
+    public AssBpAccessoBulk finAssBpAccesso(UserContext userContext, String businessProcess, String tiFunzione) throws ComponentException {
+        try {
+            AssBpAccessoHome home = (AssBpAccessoHome) getHome(userContext, AssBpAccessoBulk.class, "default");
+            SQLBuilder sql = home.createSQLBuilder();
+            sql.addClause(FindClause.AND, "businessProcess", SQLBuilder.EQUALS, businessProcess);
+            sql.addClause(FindClause.AND, "tiFunzione", Optional.ofNullable(tiFunzione)
+                    .filter(s -> s.equals("C"))
+                    .map(s -> SQLBuilder.ISNULL)
+                    .orElse(SQLBuilder.EQUALS), tiFunzione);
+            List<AssBpAccessoBulk> result = home.fetchAll(sql);
+            getHomeCache(userContext).fetchAll(userContext);
+            return Optional.ofNullable(result)
+                    .map(s -> s.stream())
+                    .map(assBpAccessoBulkStream -> assBpAccessoBulkStream.findFirst())
+                    .orElse(Optional.empty()).orElse(null);
+        } catch (it.cnr.jada.persistency.PersistencyException e) {
+            throw new ComponentException(e);
+        }
+    }
 }
--- sigla-ejb/src/main/java/it/cnr/contab/utenze00/ejb/TransactionalAssBpAccessoComponentSession.java
+++ sigla-ejb/src/main/java/it/cnr/contab/utenze00/ejb/TransactionalAssBpAccessoComponentSession.java
@@ -1,23 +1,44 @@
 package it.cnr.contab.utenze00.ejb;
+
 import java.rmi.RemoteException;
 import java.util.List;
 
 public class TransactionalAssBpAccessoComponentSession extends it.cnr.jada.ejb.TransactionalCRUDComponentSession implements AssBpAccessoComponentSession {
-	public java.util.List findAccessoByBP(it.cnr.jada.UserContext param0,String param1) throws RemoteException,it.cnr.jada.comp.ComponentException {
-	try {
-		return (List)invoke("findAccessoByBP",new Object[] {
-			param0,
-			param1 });
-	} catch(java.rmi.RemoteException e) {
-		throw e;
-	} catch(java.lang.reflect.InvocationTargetException e) {
-		try {
-			throw e.getTargetException();
-		} catch(it.cnr.jada.comp.ComponentException ex) {
-			throw ex;
-		} catch(Throwable ex) {
-			throw new java.rmi.RemoteException("Uncaugth exception",ex);
-		}
-	}
-}
+    public java.util.List findAccessoByBP(it.cnr.jada.UserContext param0, String param1) throws RemoteException, it.cnr.jada.comp.ComponentException {
+        try {
+            return (List) invoke("findAccessoByBP", new Object[]{
+                    param0,
+                    param1});
+        } catch (java.rmi.RemoteException e) {
+            throw e;
+        } catch (java.lang.reflect.InvocationTargetException e) {
+            try {
+                throw e.getTargetException();
+            } catch (it.cnr.jada.comp.ComponentException ex) {
+                throw ex;
+            } catch (Throwable ex) {
+                throw new java.rmi.RemoteException("Uncaugth exception", ex);
+            }
+        }
+    }
+
+    public it.cnr.contab.utenze00.bulk.AssBpAccessoBulk finAssBpAccesso(it.cnr.jada.UserContext param0, String param1, String param2) throws RemoteException, it.cnr.jada.comp.ComponentException {
+        try {
+            return (it.cnr.contab.utenze00.bulk.AssBpAccessoBulk) invoke("finAssBpAccesso", new Object[]{
+                    param0,
+                    param1,
+                    param2
+            });
+        } catch (java.rmi.RemoteException e) {
+            throw e;
+        } catch (java.lang.reflect.InvocationTargetException e) {
+            try {
+                throw e.getTargetException();
+            } catch (it.cnr.jada.comp.ComponentException ex) {
+                throw ex;
+            } catch (Throwable ex) {
+                throw new java.rmi.RemoteException("Uncaugth exception", ex);
+            }
+        }
+    }
 }
