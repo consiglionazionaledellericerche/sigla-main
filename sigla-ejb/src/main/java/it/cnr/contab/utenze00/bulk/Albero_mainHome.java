d89d678d1fd273d0c0b1d9b0e1bd4b535ac75e43
marco.spasiano <marco.spasiano@cnr.it>
2018-04-11 12:15:52 +0200

    Add servizio rest todo

--- sigla-ejb/src/main/java/it/cnr/contab/utenze00/bulk/Albero_mainHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/utenze00/bulk/Albero_mainHome.java
@@ -1,28 +1,41 @@
 package it.cnr.contab.utenze00.bulk;
 
-import it.cnr.jada.bulk.*;
-import it.cnr.jada.persistency.*;
-import it.cnr.jada.persistency.beans.*;
-import it.cnr.jada.persistency.sql.*;
+import it.cnr.jada.UserContext;
+import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.persistency.PersistencyException;
+import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.FindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.util.List;
 
 public class Albero_mainHome extends BulkHome {
-/**
- * <!-- @TODO: da completare -->
- * Costruisce un Albero_mainHome
- *
- * @param conn	La java.sql.Connection su cui vengono effettuate le operazione di persistenza
- */
-public Albero_mainHome(java.sql.Connection conn) {
-	super(Albero_mainBulk.class,conn);
-}
-/**
- * <!-- @TODO: da completare -->
- * Costruisce un Albero_mainHome
- *
- * @param conn	La java.sql.Connection su cui vengono effettuate le operazione di persistenza
- * @param persistentCache	La PersistentCache in cui vengono cachati gli oggetti persistenti caricati da questo Home
- */
-public Albero_mainHome(java.sql.Connection conn,PersistentCache persistentCache) {
-	super(Albero_mainBulk.class,conn,persistentCache);
-}
+    /**
+     * <!-- @TODO: da completare -->
+     * Costruisce un Albero_mainHome
+     *
+     * @param conn La java.sql.Connection su cui vengono effettuate le operazione di persistenza
+     */
+    public Albero_mainHome(java.sql.Connection conn) {
+        super(Albero_mainBulk.class, conn);
+    }
+
+    /**
+     * <!-- @TODO: da completare -->
+     * Costruisce un Albero_mainHome
+     *
+     * @param conn            La java.sql.Connection su cui vengono effettuate le operazione di persistenza
+     * @param persistentCache La PersistentCache in cui vengono cachati gli oggetti persistenti caricati da questo Home
+     */
+    public Albero_mainHome(java.sql.Connection conn, PersistentCache persistentCache) {
+        super(Albero_mainBulk.class, conn, persistentCache);
+    }
+
+    public List<Albero_mainBulk> findNodo(UserContext userContext, String businessProcess, String cdAccesso) throws PersistencyException {
+        SQLBuilder sqlBuilder = this.createSQLBuilder();
+        sqlBuilder.addClause(FindClause.AND, "business_process", SQLBuilder.EQUALS, businessProcess);
+        sqlBuilder.addClause(FindClause.AND, "cd_accesso", SQLBuilder.EQUALS, cdAccesso);
+        sqlBuilder.addClause(FindClause.AND, "fl_terminale", SQLBuilder.EQUALS, Boolean.TRUE);
+        return this.fetchAll(sqlBuilder);
+    }
 }
