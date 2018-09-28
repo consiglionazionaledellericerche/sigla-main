f0af30ddffc07941d94c5a2ed24855ef79c5d42c
Pagano <raffaele.pagano@cnr.it>
2018-09-28 15:59:59 +0200

    PDGP: modifiche

--- sigla-ejb/src/main/java/it/cnr/contab/progettiric00/core/bulk/Progetto_piano_economicoBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/progettiric00/core/bulk/Progetto_piano_economicoBulk.java
@@ -1,10 +1,16 @@
 package it.cnr.contab.progettiric00.core.bulk;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import it.cnr.contab.config00.pdcfin.bulk.Elemento_voceBulk;
 import it.cnr.contab.progettiric00.tabrif.bulk.Voce_piano_economico_prgBulk;
+import it.cnr.jada.bulk.BulkList;
 
 public class Progetto_piano_economicoBulk extends Progetto_piano_economicoBase {
 	private Voce_piano_economico_prgBulk voce_piano_economico;
 	private ProgettoBulk progetto;
+	private BulkList vociBilancioAssociate = new BulkList();
 	
 	public Progetto_piano_economicoBulk() {
 		super();
@@ -73,4 +79,33 @@ public class Progetto_piano_economicoBulk extends Progetto_piano_economicoBase {
 	public void setSaldoSpesa(V_saldi_piano_econom_progettoBulk saldoSpesa) {
 		this.saldoSpesa = saldoSpesa;
 	}
+	
+	public java.math.BigDecimal getImTotaleSpesa() {
+		return Optional.ofNullable(getIm_spesa_finanziato()).orElse(BigDecimal.ZERO)
+				.add(Optional.ofNullable(getIm_spesa_cofinanziato()).orElse(BigDecimal.ZERO));
+	}
+	
+	public BulkList getVociBilancioAssociate() {
+		return vociBilancioAssociate;
+	}
+	
+	public void setVociBilancioAssociate(BulkList vociBilancioAssociate) {
+		this.vociBilancioAssociate = vociBilancioAssociate;
+	}
+	
+	public int addToVociBilancioAssociate(Ass_progetto_piaeco_voceBulk dett) {
+		dett.setProgetto_piano_economico( this );
+		vociBilancioAssociate.add(dett);
+		return vociBilancioAssociate.size()-1;
+	}
+
+	public it.cnr.jada.bulk.BulkCollection[] getBulkLists() {
+		return new it.cnr.jada.bulk.BulkCollection[] {vociBilancioAssociate};
+	}
+
+	public Ass_progetto_piaeco_voceBulk removeFromVociBilancioAssociate(int index) {
+		Ass_progetto_piaeco_voceBulk dett = (Ass_progetto_piaeco_voceBulk)vociBilancioAssociate.remove(index);
+		return dett;
+	}
+
 }
