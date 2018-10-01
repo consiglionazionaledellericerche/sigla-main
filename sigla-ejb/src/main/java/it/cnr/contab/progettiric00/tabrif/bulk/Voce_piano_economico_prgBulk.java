b185b4faa15b89f5da9534fe767b7b293f3d2b4d
Pagano <raffaele.pagano@cnr.it>
2018-10-01 11:15:43 +0200

    Aggiornamenti Progetti

--- sigla-ejb/src/main/java/it/cnr/contab/progettiric00/tabrif/bulk/Voce_piano_economico_prgBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/progettiric00/tabrif/bulk/Voce_piano_economico_prgBulk.java
@@ -1,5 +1,7 @@
 package it.cnr.contab.progettiric00.tabrif.bulk;
 
+import java.util.Dictionary;
+
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
 import it.cnr.contab.utenze00.bp.CNRUserContext;
 import it.cnr.jada.UserContext;
@@ -11,6 +13,19 @@ import it.cnr.jada.util.action.CRUDBP;
 
 public class Voce_piano_economico_prgBulk extends Voce_piano_economico_prgBase {
 	private Unita_organizzativaBulk unita_organizzativa;
+
+	public final static Dictionary tipoVoceEconomicaKeys;
+
+	public final static String PERSONALE_DETER = "PDT";
+	public final static String PERSONALE_INDET = "PIN";
+	public final static String PERSONALE_OTHER = "POT";
+	
+	static {
+		tipoVoceEconomicaKeys = new it.cnr.jada.util.OrderedHashtable();
+		tipoVoceEconomicaKeys.put(PERSONALE_DETER,"Personale a tempo determinato");
+		tipoVoceEconomicaKeys.put(PERSONALE_INDET,"Personale a tempo indeterminato");
+		tipoVoceEconomicaKeys.put(PERSONALE_OTHER,"Altro tipo di personale");
+	}
 
 	public Voce_piano_economico_prgBulk() {
 		super();
@@ -37,5 +52,9 @@ public class Voce_piano_economico_prgBulk extends Voce_piano_economico_prgBase {
 
 	public void setCd_unita_organizzativa(java.lang.String cd_unita_organizzativa) {
 		this.getUnita_organizzativa().setCd_unita_organizzativa(cd_unita_organizzativa);
+	}
+	
+	public static Dictionary getTipovoceeconomicakeys() {
+		return tipoVoceEconomicaKeys;
 	}
 }
