6b4da0f8528dfea2d48d1cebd87eca793d0c3122
Pagano <raffaele.pagano@cnr.it>
2018-10-02 16:31:32 +0200

    Aggiornamento progetti

--- sigla-ejb/src/main/java/it/cnr/contab/progettiric00/core/bulk/ProgettoBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/progettiric00/core/bulk/ProgettoBulk.java
@@ -1,11 +1,13 @@
 package it.cnr.contab.progettiric00.core.bulk;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Dictionary;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import it.cnr.contab.anagraf00.core.bulk.TerzoBulk;
 import it.cnr.contab.config00.bulk.Parametri_cdsBulk;
@@ -18,6 +20,7 @@ import it.cnr.contab.prevent01.bulk.Pdg_programmaBulk;
 import it.cnr.contab.progettiric00.bp.TestataProgettiRicercaBP;
 import it.cnr.contab.progettiric00.bp.TestataProgettiRicercaNuovoBP;
 import it.cnr.contab.progettiric00.tabrif.bulk.Tipo_progettoBulk;
+import it.cnr.contab.progettiric00.tabrif.bulk.Voce_piano_economico_prgBulk;
 import it.cnr.jada.bulk.BulkList;
 import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.util.DateUtils;
@@ -1009,6 +1012,36 @@ public void setUnita_organizzativa(it.cnr.contab.config00.sto.bulk.Unita_organiz
 		return !getDettagliPianoEconomicoAnnoCorrente().isEmpty() ||
 				!getDettagliPianoEconomicoAltriAnni().isEmpty() ||
 				!getDettagliPianoEconomicoTotale().isEmpty();
+	}
+
+	public BulkList<Progetto_piano_economicoBulk> getPianoEconomicoSummary() {
+		BulkList<Progetto_piano_economicoBulk> items = new BulkList<Progetto_piano_economicoBulk>();
+		items.addAll(getDettagliPianoEconomicoTotale());
+		items.addAll(getDettagliPianoEconomicoAnnoCorrente());
+		items.addAll(getDettagliPianoEconomicoAltriAnni());
+
+		Map<String, List<Progetto_piano_economicoBulk>> resultByVoce = 
+				items.stream()
+					 .filter(el->el.getVoce_piano_economico()!=null)
+					 .filter(el->el.getVoce_piano_economico().getCd_unita_organizzativa()!=null)
+					 .filter(el->el.getVoce_piano_economico().getCd_voce_piano()!=null)					 
+					 .collect(Collectors.groupingBy(x->{
+						 return x.getCd_unita_organizzativa().concat(x.getCd_voce_piano());
+					 }));
+		
+		return new BulkList<Progetto_piano_economicoBulk>(resultByVoce.keySet().stream()
+				.map(vocePiano->{
+						Progetto_piano_economicoBulk bulk = new Progetto_piano_economicoBulk();
+						bulk.setVoce_piano_economico(resultByVoce.get(vocePiano).stream().findFirst().get().getVoce_piano_economico());
+						bulk.setIm_spesa_finanziato(resultByVoce.get(vocePiano).stream().map(el->Optional.ofNullable(el.getIm_spesa_finanziato()).orElse(BigDecimal.ZERO)).reduce((x, y)->x.add(y)).orElse(BigDecimal.ZERO));
+						bulk.setIm_spesa_cofinanziato(resultByVoce.get(vocePiano).stream().map(el->Optional.ofNullable(el.getIm_spesa_cofinanziato()).orElse(BigDecimal.ZERO)).reduce((x, y)->x.add(y)).orElse(BigDecimal.ZERO));
+						return bulk;
+					})
+				.collect(Collectors.toList()));
+	}
+
+	public boolean isROdatiOtherField() {
+		return this.isDettagliPianoEconomicoPresenti();
 	}
 
 }
