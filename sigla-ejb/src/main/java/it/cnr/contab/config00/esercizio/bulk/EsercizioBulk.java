16e851df87558149c8e773f9bd6f528e34fc360a
marco.spasiano <marco.spasiano@cnr.it>
2018-10-05 17:08:44 +0200

    Gestione esercizio

--- sigla-ejb/src/main/java/it/cnr/contab/config00/esercizio/bulk/EsercizioBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/esercizio/bulk/EsercizioBulk.java
@@ -1,173 +1,184 @@
 package it.cnr.contab.config00.esercizio.bulk;
 
-import java.util.*;
+import it.cnr.contab.config00.sto.bulk.CdsBulk;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.bulk.ValidationException;
+import it.cnr.jada.util.Config;
+import it.cnr.jada.util.OrderedHashtable;
 
-import it.cnr.jada.bulk.*;
-import it.cnr.jada.persistency.*;
-import it.cnr.jada.persistency.beans.*;
-import it.cnr.jada.persistency.sql.*;
-import it.cnr.jada.util.*;
+import java.util.Hashtable;
+import java.util.Optional;
 
 /**
  * Gestione dei dati relativi alla tabella Esercizio
  */
 public class EsercizioBulk extends EsercizioBase {
-		private String ds_cds;
-		private static OrderedHashtable statoKeys;
-		private static Hashtable prossimoStato;
-		final public static String STATO_INIZIALE 	= "I";
-		final public static String STATO_PDG_APERTO 	= "G";
-		final public static String STATO_APERTO 		= "A";
-//		final public static String STATO_CHIUSO_PROV 	= "P";
-		final public static String STATO_CHIUSO_DEF 	= "C";
+    final public static String STATO_INIZIALE = "I";
+    final public static String STATO_PDG_APERTO = "G";
+    final public static String STATO_APERTO = "A";
+    //		final public static String STATO_CHIUSO_PROV 	= "P";
+    final public static String STATO_CHIUSO_DEF = "C";
+    final public static Integer ESERCIZIO_INIZIO;
+    final public static Integer ESERCIZIO_FINE;
+    private static OrderedHashtable statoKeys;
+    private static Hashtable prossimoStato;
 
-		final public static Integer ESERCIZIO_INIZIO;
-		final public static Integer ESERCIZIO_FINE;
-		
-		static {
+    static {
 
-			Integer ESERCIZIO_INIZIO_DA_CONFIG;
-			try {
-				ESERCIZIO_INIZIO_DA_CONFIG = Integer.valueOf(Config.getHandler().getProperty(EsercizioBulk.class, "Esercizio.inizio"));
-			} catch (Throwable t) {
-				ESERCIZIO_INIZIO_DA_CONFIG = new Integer(2003);
-			}
-			Integer ESERCIZIO_FINE_DA_CONFIG;
-			try {
-				ESERCIZIO_FINE_DA_CONFIG = Integer.valueOf(Config.getHandler().getProperty(EsercizioBulk.class, "Esercizio.fine"));
-			} catch (Throwable t) {
-				ESERCIZIO_FINE_DA_CONFIG = new Integer(2100);
-			}
-			ESERCIZIO_INIZIO = ESERCIZIO_INIZIO_DA_CONFIG;
-			ESERCIZIO_FINE = ESERCIZIO_FINE_DA_CONFIG;
-		}
-/**
- * Primo costruttore della classe <code>EsercizioBulk</code>.
- */
-public EsercizioBulk() {
-	super();
-}
-/**
- * Secondo costruttore della classe <code>EsercizioBulk</code>.
- * @param esercizio Esercizio contabile corrente.
- */
-public EsercizioBulk(String cd_cds, java.lang.Integer esercizio ) {
-	super(cd_cds, esercizio );
-}
-/**
- * Restituisce il valore della proprietà 'ds_cds'
- *
- * @return Il valore della proprietà 'ds_cds'
- */
-public java.lang.String getDs_cds() {
-	return ds_cds;
-}
-/**
- * Metodo statico che permette di recuperare il valore dei possibili stati 
- * che può assumere un esercizio contabile.
- * @return prossimoStato Variabile di tipo <code>Hashtable</code> che 
- * 						 contiene codici e relative descrizioni degli stati.
- */
-public static java.util.Hashtable getProssimoStato() 
-{
-	if (prossimoStato == null)
-	{
-		prossimoStato = new Hashtable();
-		// prossimoStato.put( STATO_INIZIALE, STATO_APERTO );
-		prossimoStato.put( STATO_INIZIALE, STATO_PDG_APERTO );
-		prossimoStato.put( STATO_PDG_APERTO, STATO_APERTO );
-	//	prossimoStato.put( STATO_APERTO, STATO_CHIUSO_PROV );
-	//	prossimoStato.put( STATO_CHIUSO_PROV, STATO_CHIUSO_DEF );
-		prossimoStato.put( STATO_APERTO, STATO_CHIUSO_DEF );
-		prossimoStato.put( STATO_CHIUSO_DEF, STATO_APERTO );			
-	}	
-	return prossimoStato;
-}
-/**
- * Metodo con cui si ottiene il valore dello stato attuale dell'esercizio in
- * questione.
- * @return statoKeys Valore dello stato dell' esercizio contabile corrente,
- *					 di tipo <code>OrderedHashtable</code>.
- */
-public OrderedHashtable getStatoKeys() {
-	if (statoKeys == null)
-	{
-		statoKeys = new OrderedHashtable();
-		statoKeys.put("I", "Iniziale");	
-		statoKeys.put("G", "Piano di Gestione aperto");	
-		statoKeys.put("A", "Aperto");
+        Integer ESERCIZIO_INIZIO_DA_CONFIG;
+        try {
+            ESERCIZIO_INIZIO_DA_CONFIG = Integer.valueOf(Config.getHandler().getProperty(EsercizioBulk.class, "Esercizio.inizio"));
+        } catch (Throwable t) {
+            ESERCIZIO_INIZIO_DA_CONFIG = new Integer(2003);
+        }
+        Integer ESERCIZIO_FINE_DA_CONFIG;
+        try {
+            ESERCIZIO_FINE_DA_CONFIG = Integer.valueOf(Config.getHandler().getProperty(EsercizioBulk.class, "Esercizio.fine"));
+        } catch (Throwable t) {
+            ESERCIZIO_FINE_DA_CONFIG = new Integer(2100);
+        }
+        ESERCIZIO_INIZIO = ESERCIZIO_INIZIO_DA_CONFIG;
+        ESERCIZIO_FINE = ESERCIZIO_FINE_DA_CONFIG;
+    }
+
+    private CdsBulk cds;
+    /**
+     * Primo costruttore della classe <code>EsercizioBulk</code>.
+     */
+    public EsercizioBulk() {
+        super();
+    }
+
+    /**
+     * Secondo costruttore della classe <code>EsercizioBulk</code>.
+     *
+     * @param esercizio Esercizio contabile corrente.
+     */
+    public EsercizioBulk(String cd_cds, java.lang.Integer esercizio) {
+        super(cd_cds, esercizio);
+        setCds(new CdsBulk(cd_cds));
+    }
+
+    /**
+     * Metodo statico che permette di recuperare il valore dei possibili stati
+     * che può assumere un esercizio contabile.
+     *
+     * @return prossimoStato Variabile di tipo <code>Hashtable</code> che
+     * contiene codici e relative descrizioni degli stati.
+     */
+    public static java.util.Hashtable getProssimoStato() {
+        if (prossimoStato == null) {
+            prossimoStato = new Hashtable();
+            // prossimoStato.put( STATO_INIZIALE, STATO_APERTO );
+            prossimoStato.put(STATO_INIZIALE, STATO_PDG_APERTO);
+            prossimoStato.put(STATO_PDG_APERTO, STATO_APERTO);
+            //	prossimoStato.put( STATO_APERTO, STATO_CHIUSO_PROV );
+            //	prossimoStato.put( STATO_CHIUSO_PROV, STATO_CHIUSO_DEF );
+            prossimoStato.put(STATO_APERTO, STATO_CHIUSO_DEF);
+            prossimoStato.put(STATO_CHIUSO_DEF, STATO_APERTO);
+        }
+        return prossimoStato;
+    }
+
+    /**
+     * Metodo utilizzato per fare il reset delle variabili statiche definite
+     * nella classe <code>EsercizioBulk</code>.
+     */
+    public static void reset() {
+        statoKeys = null;
+    }
+
+    /**
+     * Metodo con cui si ottiene il valore dello stato attuale dell'esercizio in
+     * questione.
+     *
+     * @return statoKeys Valore dello stato dell' esercizio contabile corrente,
+     * di tipo <code>OrderedHashtable</code>.
+     */
+    public OrderedHashtable getStatoKeys() {
+        if (statoKeys == null) {
+            statoKeys = new OrderedHashtable();
+            statoKeys.put("I", "Iniziale");
+            statoKeys.put("G", "Piano di Gestione aperto");
+            statoKeys.put("A", "Aperto");
 //		statoKeys.put("P", "Chiuso provvisoriamente");
-		statoKeys.put("C", "Chiuso");
-	}
-	return statoKeys;
-}
-/**
- * Metodo per inizializzare l'oggetto bulk in fase di inserimento.
- * @param bp  Business Process <code>CRUDBP</code> in uso.
- * @param context  <code>ActionContext</code> in uso.
- * @return OggettoBulk this Oggetto bulk <code>EsercizioBulk</code>.
- */
-public OggettoBulk initializeForInsert(it.cnr.jada.util.action.CRUDBP bp,it.cnr.jada.action.ActionContext context) {
-	super.initializeForInsert( bp, context);
-	setIm_cassa_iniziale( new java.math.BigDecimal(0));
-	setSt_apertura_chiusura( STATO_INIZIALE );
-	setCd_cds( it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getUnita_padre().getCd_unita_organizzativa());
-	return this;
-}
-/**
- * Metodo per inizializzare l'oggetto bulk in fase di inserimento.
- * @param bp  Business Process <code>CRUDBP</code> in uso.
- * @param context  <code>ActionContext</code> in uso.
- * @return OggettoBulk this Oggetto bulk <code>EsercizioBulk</code>.
- */
-public OggettoBulk initializeForSearch(it.cnr.jada.util.action.CRUDBP bp,it.cnr.jada.action.ActionContext context) {
-	super.initializeForSearch( bp, context);
-	setCd_cds( it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getUnita_padre().getCd_unita_organizzativa());
-	return this;
-}
-/**
- * Metodo che stabilisce se l'esercizio contabile corrente è stato chiuso.
- * @return boolean True se l'esercizio contabile in questione è stato chiuso
- *				   in modo provvisorio o definitivo.
- */
-public boolean isChiuso() {
-//	return (getSt_apertura_chiusura().equals( STATO_CHIUSO_DEF) || getSt_apertura_chiusura().equals( STATO_CHIUSO_PROV ));
-	return getSt_apertura_chiusura().equals( STATO_CHIUSO_DEF);
+            statoKeys.put("C", "Chiuso");
+        }
+        return statoKeys;
+    }
 
-}
-/**
- * Determina quando abilitare o meno nell'interfaccia utente il campo <code>st_apertura_chiusura</code>.
- * @return boolean true quando il campo deve essere disabilitato.
- */
-public boolean isROSt_apertura_chiusura()
-{
-	return true;
-}
-/**
- * Metodo utilizzato per fare il reset delle variabili statiche definite 
- * nella classe <code>EsercizioBulk</code>.
- */
-public static void reset() {
-	statoKeys = null;
-}
-/**
- * Imposta il valore della proprietà 'ds_cds'
- *
- * @param newDs_cds	Il valore da assegnare a 'ds_cds'
- */
-public void setDs_cds(java.lang.String newDs_cds) {
-	ds_cds = newDs_cds;
-}
-/**
- * Metodo con cui si verifica la validità di alcuni campi, mediante un 
- * controllo sintattico o contestuale.
- */
-public void validate() throws ValidationException {
-		if ( getEsercizio() == null )
-			throw new ValidationException( "Il campo ESERCIZIO è obbligatorio." );
-		if ( getEsercizio().toString().length() != 4 )
-			throw new ValidationException( "Il campo ESERCIZIO deve essere di quattro cifre." );
-		if (getSt_apertura_chiusura() == null)
-			throw new ValidationException( "E' necessario indicare lo STATO dell'esercizio contabile." );
-}
+    /**
+     * Metodo per inizializzare l'oggetto bulk in fase di inserimento.
+     *
+     * @param bp      Business Process <code>CRUDBP</code> in uso.
+     * @param context <code>ActionContext</code> in uso.
+     * @return OggettoBulk this Oggetto bulk <code>EsercizioBulk</code>.
+     */
+    public OggettoBulk initializeForInsert(it.cnr.jada.util.action.CRUDBP bp, it.cnr.jada.action.ActionContext context) {
+        super.initializeForInsert(bp, context);
+        setIm_cassa_iniziale(new java.math.BigDecimal(0));
+        setSt_apertura_chiusura(STATO_INIZIALE);
+        setCd_cds(it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getUnita_padre().getCd_unita_organizzativa());
+        return this;
+    }
+
+    /**
+     * Metodo per inizializzare l'oggetto bulk in fase di inserimento.
+     *
+     * @param bp      Business Process <code>CRUDBP</code> in uso.
+     * @param context <code>ActionContext</code> in uso.
+     * @return OggettoBulk this Oggetto bulk <code>EsercizioBulk</code>.
+     */
+    public OggettoBulk initializeForSearch(it.cnr.jada.util.action.CRUDBP bp, it.cnr.jada.action.ActionContext context) {
+        super.initializeForSearch(bp, context);
+        setCd_cds(it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context).getUnita_padre().getCd_unita_organizzativa());
+        return this;
+    }
+
+    /**
+     * Metodo che stabilisce se l'esercizio contabile corrente è stato chiuso.
+     *
+     * @return boolean True se l'esercizio contabile in questione è stato chiuso
+     * in modo provvisorio o definitivo.
+     */
+    public boolean isChiuso() {
+//	return (getSt_apertura_chiusura().equals( STATO_CHIUSO_DEF) || getSt_apertura_chiusura().equals( STATO_CHIUSO_PROV ));
+        return getSt_apertura_chiusura().equals(STATO_CHIUSO_DEF);
+
+    }
+
+    /**
+     * Determina quando abilitare o meno nell'interfaccia utente il campo <code>st_apertura_chiusura</code>.
+     *
+     * @return boolean true quando il campo deve essere disabilitato.
+     */
+    public boolean isROSt_apertura_chiusura() {
+        return true;
+    }
+
+    /**
+     * Metodo con cui si verifica la validità di alcuni campi, mediante un
+     * controllo sintattico o contestuale.
+     */
+    public void validate() throws ValidationException {
+        if (getEsercizio() == null)
+            throw new ValidationException("Il campo ESERCIZIO è obbligatorio.");
+        if (getEsercizio().toString().length() != 4)
+            throw new ValidationException("Il campo ESERCIZIO deve essere di quattro cifre.");
+        if (getSt_apertura_chiusura() == null)
+            throw new ValidationException("E' necessario indicare lo STATO dell'esercizio contabile.");
+    }
+
+    public CdsBulk getCds() {
+        return cds;
+    }
+
+    public void setCds(CdsBulk cds) {
+        this.cds = cds;
+    }
+
+    @Override
+    public String getCd_cds() {
+        return Optional.ofNullable(cds).map(CdsBulk::getCd_unita_organizzativa).orElse(super.getCd_cds());
+    }
 }
