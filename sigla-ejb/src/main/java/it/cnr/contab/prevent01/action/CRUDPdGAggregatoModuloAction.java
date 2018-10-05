5ff1363047aee9ecde8460b44309eb0eb2470549
marco.spasiano <marco.spasiano@cnr.it>
2018-10-05 12:17:37 +0200

    Blocco selezione dei progetti

--- sigla-ejb/src/main/java/it/cnr/contab/prevent01/action/CRUDPdGAggregatoModuloAction.java
+++ sigla-ejb/src/main/java/it/cnr/contab/prevent01/action/CRUDPdGAggregatoModuloAction.java
@@ -398,14 +398,14 @@ public class CRUDPdGAggregatoModuloAction extends CRUDAction  {
                             .flatMap(progetto_other_fieldBulk -> Optional.ofNullable(progetto_other_fieldBulk.getStato()))
                             .filter(stato -> Arrays.asList(Progetto_other_fieldBulk.STATO_NEGOZIAZIONE, Progetto_other_fieldBulk.STATO_APPROVATO).indexOf(stato) != -1).isPresent()) {
                             errorMessage +="Attenzione: il progetto " + progetto.getCd_progetto() +" non ha uno stato utile alla previsione!";
-                            errorMessage += bp.getParentRoot().isBootstrap() ? "<br>" : "/n";
+                            errorMessage += bp.getParentRoot().isBootstrap() ? "<br>" : "\n";
                             continue;
                     }
                     if (!Optional.ofNullable(progetto.getOtherField())
                             .flatMap(progetto_other_fieldBulk -> Optional.ofNullable(progetto_other_fieldBulk.getTipoFinanziamento()))
                             .filter(tipoFinanziamentoBulk -> tipoFinanziamentoBulk.getFlPrevEntSpesa() || tipoFinanziamentoBulk.getFlRipCostiPers()).isPresent()) {
                         errorMessage +="Attenzione: il progetto " + progetto.getCd_progetto() +" non è consentita la previsione!<br>";
-                        errorMessage += bp.getParentRoot().isBootstrap() ? "<br>" : "/n";
+                        errorMessage += bp.getParentRoot().isBootstrap() ? "<br>" : "\n";
                         continue;
                     }
 					if (!cdr.getDettagli().containsByPrimaryKey(mod))
