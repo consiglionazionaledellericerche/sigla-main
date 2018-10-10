6741cccd6c16305a6346e5185047494922cd109e
marco.spasiano <marco.spasiano@cnr.it>
2018-10-10 10:07:42 +0200

    Add ApplicationMessageFormatException

--- sigla-ejb/src/main/java/it/cnr/contab/config00/pdcfin/bulk/Elemento_voceKey.java
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/pdcfin/bulk/Elemento_voceKey.java
@@ -1,100 +1,115 @@
 package it.cnr.contab.config00.pdcfin.bulk;
 
-import it.cnr.jada.bulk.*;
-import it.cnr.jada.persistency.*;
-import it.cnr.jada.persistency.beans.*;
-import it.cnr.jada.persistency.sql.*;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.persistency.KeyedPersistent;
 
 public class Elemento_voceKey extends OggettoBulk implements KeyedPersistent {
-	// TI_APPARTENENZA CHAR(1) NOT NULL (PK)
-	private java.lang.String ti_appartenenza;
+    // TI_APPARTENENZA CHAR(1) NOT NULL (PK)
+    private java.lang.String ti_appartenenza;
 
-	// CD_ELEMENTO_VOCE VARCHAR(20) NOT NULL (PK)
-	private java.lang.String cd_elemento_voce;
+    // CD_ELEMENTO_VOCE VARCHAR(20) NOT NULL (PK)
+    private java.lang.String cd_elemento_voce;
 
-	// ESERCIZIO DECIMAL(4,0) NOT NULL (PK)
-	private java.lang.Integer esercizio;
+    // ESERCIZIO DECIMAL(4,0) NOT NULL (PK)
+    private java.lang.Integer esercizio;
 
-	// TI_GESTIONE CHAR(1) NOT NULL (PK)
-	private java.lang.String ti_gestione;
+    // TI_GESTIONE CHAR(1) NOT NULL (PK)
+    private java.lang.String ti_gestione;
 
-public Elemento_voceKey() {
-	super();
-}
-public Elemento_voceKey(java.lang.String cd_elemento_voce,java.lang.Integer esercizio,java.lang.String ti_appartenenza,java.lang.String ti_gestione) {
-	super();
-	this.cd_elemento_voce = cd_elemento_voce;
-	this.esercizio = esercizio;
-	this.ti_appartenenza = ti_appartenenza;
-	this.ti_gestione = ti_gestione;
-}
-public boolean equalsByPrimaryKey(Object o) {
-	if (this == o) return true;
-	if (!(o instanceof Elemento_voceKey)) return false;
-	Elemento_voceKey k = (Elemento_voceKey)o;
-	if(!compareKey(getCd_elemento_voce(),k.getCd_elemento_voce())) return false;
-	if(!compareKey(getEsercizio(),k.getEsercizio())) return false;
-	if(!compareKey(getTi_appartenenza(),k.getTi_appartenenza())) return false;
-	if(!compareKey(getTi_gestione(),k.getTi_gestione())) return false;
-	return true;
-}
-@Override
-public boolean equals(Object obj) {
-	return this.equalsByPrimaryKey(obj);
-}
-/* 
- * Getter dell'attributo cd_elemento_voce
- */
-public java.lang.String getCd_elemento_voce() {
-	return cd_elemento_voce;
-}
-/* 
- * Getter dell'attributo esercizio
- */
-public java.lang.Integer getEsercizio() {
-	return esercizio;
-}
-/* 
- * Getter dell'attributo ti_appartenenza
- */
-public java.lang.String getTi_appartenenza() {
-	return ti_appartenenza;
-}
-/* 
- * Getter dell'attributo ti_gestione
- */
-public java.lang.String getTi_gestione() {
-	return ti_gestione;
-}
-public int primaryKeyHashCode() {
-	return
-		calculateKeyHashCode(getCd_elemento_voce())+
-		calculateKeyHashCode(getEsercizio())+
-		calculateKeyHashCode(getTi_appartenenza())+
-		calculateKeyHashCode(getTi_gestione());
-}
-/* 
- * Setter dell'attributo cd_elemento_voce
- */
-public void setCd_elemento_voce(java.lang.String cd_elemento_voce) {
-	this.cd_elemento_voce = cd_elemento_voce;
-}
-/* 
- * Setter dell'attributo esercizio
- */
-public void setEsercizio(java.lang.Integer esercizio) {
-	this.esercizio = esercizio;
-}
-/* 
- * Setter dell'attributo ti_appartenenza
- */
-public void setTi_appartenenza(java.lang.String ti_appartenenza) {
-	this.ti_appartenenza = ti_appartenenza;
-}
-/* 
- * Setter dell'attributo ti_gestione
- */
-public void setTi_gestione(java.lang.String ti_gestione) {
-	this.ti_gestione = ti_gestione;
-}
+    public Elemento_voceKey() {
+        super();
+    }
+
+    public Elemento_voceKey(java.lang.String cd_elemento_voce, java.lang.Integer esercizio, java.lang.String ti_appartenenza, java.lang.String ti_gestione) {
+        super();
+        this.cd_elemento_voce = cd_elemento_voce;
+        this.esercizio = esercizio;
+        this.ti_appartenenza = ti_appartenenza;
+        this.ti_gestione = ti_gestione;
+    }
+
+    public boolean equalsByPrimaryKey(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Elemento_voceKey)) return false;
+        Elemento_voceKey k = (Elemento_voceKey) o;
+        if (!compareKey(getCd_elemento_voce(), k.getCd_elemento_voce())) return false;
+        if (!compareKey(getEsercizio(), k.getEsercizio())) return false;
+        if (!compareKey(getTi_appartenenza(), k.getTi_appartenenza())) return false;
+        if (!compareKey(getTi_gestione(), k.getTi_gestione())) return false;
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.equalsByPrimaryKey(obj);
+    }
+
+    /*
+     * Getter dell'attributo cd_elemento_voce
+     */
+    public java.lang.String getCd_elemento_voce() {
+        return cd_elemento_voce;
+    }
+
+    /*
+     * Setter dell'attributo cd_elemento_voce
+     */
+    public void setCd_elemento_voce(java.lang.String cd_elemento_voce) {
+        this.cd_elemento_voce = cd_elemento_voce;
+    }
+
+    /*
+     * Getter dell'attributo esercizio
+     */
+    public java.lang.Integer getEsercizio() {
+        return esercizio;
+    }
+
+    /*
+     * Setter dell'attributo esercizio
+     */
+    public void setEsercizio(java.lang.Integer esercizio) {
+        this.esercizio = esercizio;
+    }
+
+    /*
+     * Getter dell'attributo ti_appartenenza
+     */
+    public java.lang.String getTi_appartenenza() {
+        return ti_appartenenza;
+    }
+
+    /*
+     * Setter dell'attributo ti_appartenenza
+     */
+    public void setTi_appartenenza(java.lang.String ti_appartenenza) {
+        this.ti_appartenenza = ti_appartenenza;
+    }
+
+    /*
+     * Getter dell'attributo ti_gestione
+     */
+    public java.lang.String getTi_gestione() {
+        return ti_gestione;
+    }
+
+    /*
+     * Setter dell'attributo ti_gestione
+     */
+    public void setTi_gestione(java.lang.String ti_gestione) {
+        this.ti_gestione = ti_gestione;
+    }
+
+    @Override
+    public int hashCode() {
+        return primaryKeyHashCode();
+    }
+
+    public int primaryKeyHashCode() {
+        return
+                calculateKeyHashCode(getCd_elemento_voce()) +
+                        calculateKeyHashCode(getEsercizio()) +
+                        calculateKeyHashCode(getTi_appartenenza()) +
+                        calculateKeyHashCode(getTi_gestione());
+    }
 }
