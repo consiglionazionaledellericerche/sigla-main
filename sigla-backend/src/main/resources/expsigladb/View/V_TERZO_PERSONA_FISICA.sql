18211d15f5e0f1476347a62270ceac5fa8b688a7
rosangela.pucciarelli <rosangela.pucciarelli@cnr.it>
2018-10-29 16:15:56 +0100

    Modifiche per fatturazione elettronica

--- sigla-backend/src/main/resources/expsigladb/View/V_TERZO_PERSONA_FISICA.sql
+++ sigla-backend/src/main/resources/expsigladb/View/V_TERZO_PERSONA_FISICA.sql
@@ -2,7 +2,7 @@
 --  DDL for View V_TERZO_PERSONA_FISICA
 --------------------------------------------------------
 
-  CREATE OR REPLACE FORCE VIEW "V_TERZO_PERSONA_FISICA" ("CD_TERZO", "FRAZIONE_SEDE", "CD_ANAG", "DT_FINE_RAPPORTO", "TI_TERZO", "CD_PRECEDENTE", "DENOMINAZIONE_SEDE", "VIA_SEDE", "NUMERO_CIVICO_SEDE", "PG_COMUNE_SEDE", "CAP_COMUNE_SEDE", "PG_RAPP_LEGALE", "CD_UNITA_ORGANIZZATIVA", "NOME_UNITA_ORGANIZZATIVA", "NOTE", "DT_CANC", "DACR", "UTCR", "DUVA", "UTUV", "PG_VER_REC", "CODICE_FISCALE", "NOME", "COGNOME", "TI_SESSO", "DT_NASCITA", "CODICE_UNIVOCO_UFFICIO_IPA", "CODICE_UNIVOCO_PCC", "DENOMINAZIONE_PCC") AS 
+  CREATE OR REPLACE FORCE VIEW "V_TERZO_PERSONA_FISICA" ("CD_TERZO", "FRAZIONE_SEDE", "CD_ANAG", "DT_FINE_RAPPORTO", "TI_TERZO", "CD_PRECEDENTE", "DENOMINAZIONE_SEDE", "VIA_SEDE", "NUMERO_CIVICO_SEDE", "PG_COMUNE_SEDE", "CAP_COMUNE_SEDE", "PG_RAPP_LEGALE", "CD_UNITA_ORGANIZZATIVA", "NOME_UNITA_ORGANIZZATIVA", "NOTE", "DT_CANC", "DACR", "UTCR", "DUVA", "UTUV", "PG_VER_REC", "CODICE_FISCALE", "NOME", "COGNOME", "TI_SESSO", "DT_NASCITA", "CODICE_UNIVOCO_UFFICIO_IPA", "CODICE_UNIVOCO_PCC", "DENOMINAZIONE_PCC","CODICE_DESTINATARIO_FATT","FL_SBLOCCO_FATTURA_ELETTRONICA") AS 
   SELECT
 --
 -- Date: 31/05/2001
@@ -26,7 +26,7 @@
           terzo.note, terzo.dt_canc, terzo.dacr, terzo.utcr, terzo.duva,
           terzo.utuv, terzo.pg_ver_rec, anagrafico.codice_fiscale,
           anagrafico.nome, anagrafico.cognome, anagrafico.ti_sesso,
-          anagrafico.dt_nascita, terzo.codice_univoco_ufficio_ipa,terzo.codice_univoco_pcc,terzo.denominazione_pcc
+          anagrafico.dt_nascita, terzo.codice_univoco_ufficio_ipa,terzo.codice_univoco_pcc,terzo.denominazione_pcc,CODICE_DESTINATARIO_FATT,FL_SBLOCCO_FATTURA_ELETTRONICA
      FROM terzo, anagrafico
     WHERE terzo.cd_anag = anagrafico.cd_anag AND anagrafico.ti_entita = 'F';
 
