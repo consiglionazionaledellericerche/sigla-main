1cadaa624b8615309f8aaeccf736ed58ccf1b365
Pagano <raffaele.pagano@cnr.it>
2018-12-12 18:47:47 +0100

    Ribaltamento Residui: controllo progetto approvato

--- sigla-ejb/src/main/java/it/cnr/contab/chiusura00/ejb/RicercaDocContComponentSessionBean.java
+++ sigla-ejb/src/main/java/it/cnr/contab/chiusura00/ejb/RicercaDocContComponentSessionBean.java
@@ -715,4 +715,40 @@ public class RicercaDocContComponentSessionBean extends it.cnr.jada.ejb.RicercaC
 			throw uncaughtError(param0,componentObj,e);
 		}
 	}
+	public boolean isProgettiCollegatiGaeApprovati(it.cnr.jada.UserContext param0) throws it.cnr.jada.comp.ComponentException,javax.ejb.EJBException {
+		pre_component_invocation(param0,componentObj);
+		try {
+			boolean result = ((RicercaDocContComponent)componentObj).isProgettiCollegatiGaeApprovati(param0);
+			component_invocation_succes(param0,componentObj);
+			return result;
+		} catch(it.cnr.jada.comp.NoRollbackException e) {
+			component_invocation_succes(param0,componentObj);
+			throw e;
+		} catch(it.cnr.jada.comp.ComponentException e) {
+			component_invocation_failure(param0,componentObj);
+			throw e;
+		} catch(RuntimeException e) {
+			throw uncaughtRuntimeException(param0,componentObj,e);
+		} catch(Error e) {
+			throw uncaughtError(param0,componentObj,e);
+		}
+	}
+	public it.cnr.jada.util.RemoteIterator cercaProgettiCollegatiGaeNonApprovatiForRibaltamento(it.cnr.jada.UserContext param0) throws it.cnr.jada.comp.ComponentException,javax.ejb.EJBException {
+		pre_component_invocation(param0,componentObj);
+		try {
+			it.cnr.jada.util.RemoteIterator result = ((RicercaDocContComponent)componentObj).cercaProgettiCollegatiGaeNonApprovatiForRibaltamento(param0);
+			component_invocation_succes(param0,componentObj);
+			return result;
+		} catch(it.cnr.jada.comp.NoRollbackException e) {
+			component_invocation_succes(param0,componentObj);
+			throw e;
+		} catch(it.cnr.jada.comp.ComponentException e) {
+			component_invocation_failure(param0,componentObj);
+			throw e;
+		} catch(RuntimeException e) {
+			throw uncaughtRuntimeException(param0,componentObj,e);
+		} catch(Error e) {
+			throw uncaughtError(param0,componentObj,e);
+		}
+	}
 }
