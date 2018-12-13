f5d8c1f22bc9f8f180496de3885661cf50d308d8
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-12-13 14:41:11 +0100

    Rilascio servizio REST per aggiornamento dati terzo relativi alla fatturazione elettronica

--- sigla-web/src/main/java/it/cnr/contab/web/rest/config/SIGLARoles.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/config/SIGLARoles.java
@@ -2,5 +2,6 @@ package it.cnr.contab.web.rest.config;
 
 public final class SIGLARoles {
 	public static final String MISSIONI = "MISSIONI",
-			FATTURE_ATTIVE = "FATTURE_AT";
+			FATTURE_ATTIVE = "FATTURE_AT",
+			TERZO = "TERZO_REST";
 }
