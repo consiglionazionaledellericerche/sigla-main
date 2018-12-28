5b8da6f803ac8753c5ed235ba6f433086ff83781
marco.spasiano <marco.spasiano@cnr.it>
2018-12-28 18:05:17 +0100

    SIOPE+

--- sigla-ejb/src/main/java/it/cnr/contab/siope/plus/bulk/SIOPEPlusRisultatoBase.java
+++ sigla-ejb/src/main/java/it/cnr/contab/siope/plus/bulk/SIOPEPlusRisultatoBase.java
@@ -4,13 +4,24 @@
  */
 package it.cnr.contab.siope.plus.bulk;
 import it.cnr.jada.persistency.Keyed;
+
+import java.sql.Timestamp;
+
 public class SIOPEPlusRisultatoBase extends SIOPEPlusRisultatoKey implements Keyed {
 //    ESITO VARCHAR(50) NOT NULL
 	private String esito;
  
 //    LOCATION VARCHAR(1000) NOT NULL
 	private String location;
- 
+
+	private Integer prog_flusso;
+
+	private Integer prog_esito_applicativo;
+
+	private Timestamp data_produzione;
+
+	private Timestamp data_upload;
+
 	/**
 	 * Created by BulkGenerator 2.0 [07/12/2009]
 	 * Table name: SIOPE_PLUS_RISULTATO
@@ -50,4 +61,35 @@ public class SIOPEPlusRisultatoBase extends SIOPEPlusRisultatoKey implements Key
 		this.location=location;
 	}
 
+	public Integer getProg_flusso() {
+		return prog_flusso;
+	}
+
+	public void setProg_flusso(Integer prog_flusso) {
+		this.prog_flusso = prog_flusso;
+	}
+
+	public Integer getProg_esito_applicativo() {
+		return prog_esito_applicativo;
+	}
+
+	public void setProg_esito_applicativo(Integer prog_esito_applicativo) {
+		this.prog_esito_applicativo = prog_esito_applicativo;
+	}
+
+	public Timestamp getData_produzione() {
+		return data_produzione;
+	}
+
+	public void setData_produzione(Timestamp data_produzione) {
+		this.data_produzione = data_produzione;
+	}
+
+	public Timestamp getData_upload() {
+		return data_upload;
+	}
+
+	public void setData_upload(Timestamp data_upload) {
+		this.data_upload = data_upload;
+	}
 }
