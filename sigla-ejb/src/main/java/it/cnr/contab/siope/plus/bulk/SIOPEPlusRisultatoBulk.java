9b7e50cedab7da5c61d25ce7b097d5d0656e79ed
marco.spasiano <marco.spasiano@cnr.it>
2018-12-28 18:05:17 +0100

    SIOPE+

--- sigla-ejb/src/main/java/it/cnr/contab/siope/plus/bulk/SIOPEPlusRisultatoBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/siope/plus/bulk/SIOPEPlusRisultatoBulk.java
@@ -5,7 +5,12 @@
 package it.cnr.contab.siope.plus.bulk;
 
 import it.cnr.contab.model.Esito;
+import it.cnr.contab.model.Risultato;
 import it.cnr.contab.service.OrdinativiSiopePlusService;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Optional;
 
 public class SIOPEPlusRisultatoBulk extends SIOPEPlusRisultatoBase {
     /**
@@ -24,10 +29,23 @@ public class SIOPEPlusRisultatoBulk extends SIOPEPlusRisultatoBase {
         super(id);
     }
 
-    public SIOPEPlusRisultatoBulk(String esito, String location) {
+    public SIOPEPlusRisultatoBulk(String esito, Risultato risultato) {
         super();
         setEsito(esito);
-        setLocation(location);
+        setProg_flusso(Optional.ofNullable(risultato.getProgFlusso()).orElse(null));
+        setProg_esito_applicativo(Optional.ofNullable(risultato.getProgEsitoApplicativo()).orElse(null));
+        setData_produzione(
+                Optional.ofNullable(risultato.getDataProduzione())
+                    .map(Date::getTime)
+                    .map(aLong -> new Timestamp(aLong))
+                    .orElse(null)
+        );
+        setData_upload(
+                Optional.ofNullable(risultato.getDataUpload())
+                .map(Date::getTime)
+                .map(aLong -> new Timestamp(aLong))
+                .orElse(null));
+        setLocation(risultato.getLocation());
         setToBeCreated();
     }
 
