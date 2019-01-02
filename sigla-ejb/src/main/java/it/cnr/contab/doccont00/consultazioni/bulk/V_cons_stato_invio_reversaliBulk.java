ba4e23267d4624b09effea356f1d062cf9256107
marco.spasiano <marco.spasiano@cnr.it>
2019-01-02 16:05:04 +0100

    SIOPE+ aggiunti esiti sulle consultazioni di mandati e reversali

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/V_cons_stato_invio_reversaliBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/V_cons_stato_invio_reversaliBulk.java
@@ -1,25 +1,45 @@
 /*
-* Created by Generator 1.0
-* Date 27/04/2006
-*/
+ * Created by Generator 1.0
+ * Date 27/04/2006
+ */
 package it.cnr.contab.doccont00.consultazioni.bulk;
-import it.cnr.contab.doccont00.core.bulk.Numerazione_doc_contBulk;
-public class V_cons_stato_invio_reversaliBulk extends V_cons_stato_invio_reversaliBase implements V_cons_stato_invio{
-	private String contabile;
-	public V_cons_stato_invio_reversaliBulk() {
-		super();
-	}
 
-	public Long getProgressivo() {
-		return getPg_reversale();
-	}
-	public String getTipo() {
-		return Numerazione_doc_contBulk.TIPO_REV;
-	}
-	public String getContabile() {
-		return contabile;
-	}
-	public void setContabile(String contabile) {
-		this.contabile = contabile;
-	}	
+import it.cnr.contab.doccont00.core.bulk.Numerazione_doc_contBulk;
+import it.cnr.contab.doccont00.core.bulk.ReversaleBulk;
+import it.cnr.jada.util.OrderedHashtable;
+
+import java.util.Dictionary;
+
+public class V_cons_stato_invio_reversaliBulk extends V_cons_stato_invio_reversaliBase implements V_cons_stato_invio {
+    public final static Dictionary esito_OperazioneKeys = new OrderedHashtable();
+
+    static {
+        for (ReversaleBulk.EsitoOperazione esito : ReversaleBulk.EsitoOperazione.values()) {
+            esito_OperazioneKeys.put(esito.value(), esito.label());
+        }
+    }
+
+    private String contabile;
+    // ERRORE_SIOPE_PLUS VARCHAR2(2000)
+    private java.lang.String erroreSiopePlus;
+
+    public V_cons_stato_invio_reversaliBulk() {
+        super();
+    }
+
+    public Long getProgressivo() {
+        return getPg_reversale();
+    }
+
+    public String getTipo() {
+        return Numerazione_doc_contBulk.TIPO_REV;
+    }
+
+    public String getContabile() {
+        return contabile;
+    }
+
+    public void setContabile(String contabile) {
+        this.contabile = contabile;
+    }
 }
