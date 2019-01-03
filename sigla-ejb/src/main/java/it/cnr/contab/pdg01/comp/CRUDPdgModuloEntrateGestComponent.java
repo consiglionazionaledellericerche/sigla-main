a18ce1a34245888269fd57141cc3e22a9e2e27b4
raffaele.pagano <raffaele.pagano@cnr.it>
2019-01-03 16:55:10 +0100

    Piano Economico: bug gestionale

--- sigla-ejb/src/main/java/it/cnr/contab/pdg01/comp/CRUDPdgModuloEntrateGestComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/pdg01/comp/CRUDPdgModuloEntrateGestComponent.java
@@ -169,20 +169,15 @@ public class CRUDPdgModuloEntrateGestComponent extends it.cnr.jada.comp.CRUDComp
 													CompoundFindClause clause) throws ComponentException, PersistencyException {	
 		SQLBuilder sql = getHome(userContext, latt, "V_LINEA_ATTIVITA_VALIDA").createSQLBuilder();
 
-		sql.addSQLClause("AND","V_LINEA_ATTIVITA_VALIDA.ESERCIZIO",sql.EQUALS,CNRUserContext.getEsercizio(userContext));
-		sql.addClause("AND","cd_centro_responsabilita",sql.EQUALS,dett.getCd_cdr_assegnatario());
-		sql.addClause("AND","pg_progetto",sql.EQUALS,dett.getPg_progetto());
-		sql.addClause("AND","cd_natura",sql.EQUALS,dett.getCd_natura());
+		sql.addSQLClause("AND","V_LINEA_ATTIVITA_VALIDA.ESERCIZIO",SQLBuilder.EQUALS,CNRUserContext.getEsercizio(userContext));
+		sql.addClause("AND","cd_centro_responsabilita",SQLBuilder.EQUALS,dett.getCd_cdr_assegnatario());
+		sql.addClause("AND","pg_progetto",SQLBuilder.EQUALS,dett.getPg_progetto());
+		sql.addClause("AND","cd_natura",SQLBuilder.EQUALS,dett.getCd_natura());
 
 		sql.openParenthesis(FindClause.AND);
 		sql.addClause(FindClause.OR,"ti_gestione",SQLBuilder.EQUALS,WorkpackageBulk.TI_GESTIONE_ENTRATE);
 		sql.addClause(FindClause.OR,"ti_gestione",SQLBuilder.EQUALS,WorkpackageBulk.TI_GESTIONE_ENTRAMBE);
 		sql.closeParenthesis();
-
-		if (dett.getPdg_modulo_entrate().getVoce_piano_economico()!=null && dett.getPdg_modulo_entrate().getVoce_piano_economico().getCd_voce_piano()!=null ) {
-			sql.addSQLClause(FindClause.AND,"V_LINEA_ATTIVITA_VALIDA.CD_UNITA_PIANO",SQLBuilder.EQUALS,dett.getPdg_modulo_entrate().getVoce_piano_economico().getCd_unita_organizzativa());
-			sql.addSQLClause(FindClause.AND,"V_LINEA_ATTIVITA_VALIDA.CD_VOCE_PIANO",SQLBuilder.EQUALS,dett.getPdg_modulo_entrate().getVoce_piano_economico().getCd_voce_piano());
-		}
 
 		Parametri_cnrHome parCnrhome = (Parametri_cnrHome)getHome(userContext, Parametri_cnrBulk.class);
 		Parametri_cnrBulk parCnrBulk = (Parametri_cnrBulk)parCnrhome.findByPrimaryKey(new Parametri_cnrBulk(it.cnr.contab.utenze00.bp.CNRUserContext.getEsercizio( userContext )));
