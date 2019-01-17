78bab06b7a95ea475ecf239b95fe578b7c1e031a
marco.spasiano <marco.spasiano@cnr.it>
2019-01-17 17:48:06 +0100

    SIOPE+

--- sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/ToDoResource.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/ToDoResource.java
@@ -6,10 +6,13 @@ import it.cnr.contab.docamm00.docs.bulk.Lettera_pagam_esteroBulk;
 import it.cnr.contab.docamm00.ejb.FatturaElettronicaPassivaComponentSession;
 import it.cnr.contab.docamm00.fatturapa.bulk.DocumentoEleTestataBulk;
 import it.cnr.contab.docamm00.fatturapa.bulk.StatoDocumentoEleEnum;
+import it.cnr.contab.doccont00.core.bulk.MandatoBulk;
+import it.cnr.contab.doccont00.core.bulk.Numerazione_doc_contBulk;
 import it.cnr.contab.doccont00.intcass.bulk.StatoTrasmissione;
 import it.cnr.contab.doccont00.intcass.bulk.V_mandato_reversaleBulk;
 import it.cnr.contab.missioni00.docs.bulk.MissioneBulk;
 import it.cnr.contab.missioni00.ejb.MissioneComponentSession;
+import it.cnr.contab.model.Esito;
 import it.cnr.contab.pdg00.bulk.ArchiviaStampaPdgVariazioneBulk;
 import it.cnr.contab.pdg00.bulk.Pdg_variazioneBulk;
 import it.cnr.contab.pdg00.ejb.PdGVariazioniComponentSession;
@@ -20,6 +23,7 @@ import it.cnr.contab.utenze00.bulk.AbilitatoFirma;
 import it.cnr.contab.utenze00.bulk.Albero_mainBulk;
 import it.cnr.contab.utenze00.bulk.Albero_mainKey;
 import it.cnr.contab.utenze00.bulk.UtenteBulk;
+import it.cnr.contab.util.enumeration.EsitoOperazione;
 import it.cnr.contab.web.rest.exception.RestException;
 import it.cnr.contab.web.rest.local.util.ToDoLocal;
 import it.cnr.jada.UserContext;
@@ -400,6 +404,73 @@ public class ToDoResource implements ToDoLocal {
                                     }
                                 });
                     }
+                    break;
+                }
+                case CRUDDistintaCassiereBP: {
+                    V_mandato_reversaleBulk mandatoReversaleBulk = new V_mandato_reversaleBulk();
+                    mandatoReversaleBulk.setCd_tipo_documento_cont(Numerazione_doc_contBulk.TIPO_MAN);
+                    mandatoReversaleBulk.setEsercizio(CNRUserContext.getEsercizio(userContext));
+                    mandatoReversaleBulk.setEsitoOperazione(EsitoOperazione.NON_ACQUISITO.value());
+                    mandatoReversaleBulk.setStato(MandatoBulk.STATO_MANDATO_EMESSO);
+                    mandatoReversaleBulk.setStato_trasmissione(MandatoBulk.STATO_TRASMISSIONE_TRASMESSO);
+
+                    BulkLoaderIterator remoteIteratorMandato =
+                            Optional.ofNullable(crudComponentSession.cerca(
+                                    userContext,
+                                    null,
+                                    mandatoReversaleBulk))
+                                    .filter(BulkLoaderIterator.class::isInstance)
+                                    .map(BulkLoaderIterator.class::cast)
+                                    .orElseThrow(() -> new RestException(Response.Status.INTERNAL_SERVER_ERROR, "Cannot create remote iterator"));
+                    Optional.ofNullable(remoteIteratorMandato)
+                            .ifPresent(iterator -> {
+                                try {
+                                    iterator.open(userContext);
+                                    final int i = iterator.countElements();
+                                    if (i > 0) {
+                                        result.add(new ToDoDetail(
+                                                "0.DOC.CON.MAN.INIOMAN",
+                                                "fa fa-fw fa-university text-danger",
+                                                "SIOPE+",
+                                                firstLabel(i),
+                                                detailLabel(i, "Mandato non acquisito", "Mandati non acquisiti", "")
+                                        ));
+                                    }
+                                } catch (ComponentException | RemoteException e) {
+                                    throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+                                } finally {
+                                    iterator.ejbRemove();
+                                }
+                            });
+                    mandatoReversaleBulk.setCd_tipo_documento_cont(Numerazione_doc_contBulk.TIPO_REV);
+                    BulkLoaderIterator remoteIteratorReversale =
+                            Optional.ofNullable(crudComponentSession.cerca(
+                                    userContext,
+                                    null,
+                                    mandatoReversaleBulk))
+                                    .filter(BulkLoaderIterator.class::isInstance)
+                                    .map(BulkLoaderIterator.class::cast)
+                                    .orElseThrow(() -> new RestException(Response.Status.INTERNAL_SERVER_ERROR, "Cannot create remote iterator"));
+                    Optional.ofNullable(remoteIteratorReversale)
+                            .ifPresent(iterator -> {
+                                try {
+                                    iterator.open(userContext);
+                                    final int i = iterator.countElements();
+                                    if (i > 0) {
+                                        result.add(new ToDoDetail(
+                                                "0.DOC.CON.REV.INIOREV",
+                                                "fa fa-fw fa-university text-danger",
+                                                "SIOPE+",
+                                                firstLabel(i),
+                                                detailLabel(i, "Reversale non acquisita", "Reversali non acquisite", "")
+                                        ));
+                                    }
+                                } catch (ComponentException | RemoteException e) {
+                                    throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+                                } finally {
+                                    iterator.ejbRemove();
+                                }
+                            });
                     break;
                 }
             }
