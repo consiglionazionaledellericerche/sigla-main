19243b28e29a3bbc5f7b6980247e8bb225fbeb26
marco.spasiano <marco.spasiano@cnr.it>
2019-01-21 16:02:33 +0100

    add test

--- sigla-ejb/src/test/java/it/cnr/contab/util/CompensoCigModificabile.java
+++ sigla-ejb/src/test/java/it/cnr/contab/util/CompensoCigModificabile.java
@@ -5,7 +5,6 @@ import it.cnr.contab.compensi00.docs.bulk.CompensoBulk;
 import it.cnr.contab.compensi00.tabrif.bulk.Tipo_trattamentoBulk;
 import it.cnr.contab.config00.bulk.CigBulk;
 import it.cnr.contab.doccont00.core.bulk.ObbligazioneBulk;
-import it.cnr.contab.doccont00.core.bulk.ObbligazioneHome;
 import it.cnr.contab.doccont00.core.bulk.Obbligazione_scadenzarioBulk;
 import it.cnr.jada.action.BusinessProcessException;
 import org.junit.Test;
@@ -19,7 +18,7 @@ public class CompensoCigModificabile {
         final CompensoBulk compensoBulk = new CompensoBulk();
         try {
             crudCompensoBP.setModel(null, compensoBulk);
-            //Assert.isTrue(!crudCompensoBP.isCigModificabile());
+            Assert.isTrue(!crudCompensoBP.isCigModificabile());
         } catch (BusinessProcessException e) {
         }
     }
@@ -34,7 +33,7 @@ public class CompensoCigModificabile {
         compensoBulk.setTipoTrattamento(tipo_trattamentoBulk);
         try {
             crudCompensoBP.setModel(null, compensoBulk);
-            //Assert.isTrue(!crudCompensoBP.isCigModificabile());
+            Assert.isTrue(!crudCompensoBP.isCigModificabile());
         } catch (BusinessProcessException e) {
         }
     }
@@ -49,7 +48,7 @@ public class CompensoCigModificabile {
         compensoBulk.setTipoTrattamento(tipo_trattamentoBulk);
         try {
             crudCompensoBP.setModel(null, compensoBulk);
-            //Assert.isTrue(!crudCompensoBP.isCigModificabile());
+            Assert.isTrue(!crudCompensoBP.isCigModificabile());
         } catch (BusinessProcessException e) {
         }
     }
@@ -65,6 +64,7 @@ public class CompensoCigModificabile {
 
         Obbligazione_scadenzarioBulk obbligazione_scadenzarioBulk = new Obbligazione_scadenzarioBulk();
         ObbligazioneBulk obbligazioneBulk = new ObbligazioneBulk();
+        obbligazioneBulk.setPg_obbligazione(Long.valueOf(1));
         obbligazioneBulk.getContratto().setPg_contratto(Long.valueOf(1));
 
         obbligazione_scadenzarioBulk.setObbligazione(obbligazioneBulk);
@@ -72,7 +72,83 @@ public class CompensoCigModificabile {
 
         try {
             crudCompensoBP.setModel(null, compensoBulk);
-            //Assert.isTrue(crudCompensoBP.isCigModificabile());
+            Assert.isTrue(crudCompensoBP.isCigModificabile());
+        } catch (BusinessProcessException e) {
+        }
+    }
+
+
+    @Test
+    public void compensoFive() {
+        CRUDCompensoBP crudCompensoBP = new CRUDCompensoBP();
+        final CompensoBulk compensoBulk = new CompensoBulk();
+        final Tipo_trattamentoBulk tipo_trattamentoBulk = new Tipo_trattamentoBulk();
+        tipo_trattamentoBulk.setCd_trattamento("TEST");
+        tipo_trattamentoBulk.setTipoDebitoSiope(Tipo_trattamentoBulk.TIPO_DEBITO_COMMERCIALE);
+        compensoBulk.setTipoTrattamento(tipo_trattamentoBulk);
+
+        Obbligazione_scadenzarioBulk obbligazione_scadenzarioBulk = new Obbligazione_scadenzarioBulk();
+        ObbligazioneBulk obbligazioneBulk = new ObbligazioneBulk();
+        obbligazioneBulk.setPg_obbligazione(Long.valueOf(1));
+        obbligazioneBulk.getContratto().setPg_contratto(Long.valueOf(1));
+
+        obbligazione_scadenzarioBulk.setObbligazione(obbligazioneBulk);
+        compensoBulk.setObbligazioneScadenzario(obbligazione_scadenzarioBulk);
+
+        try {
+            crudCompensoBP.setModel(null, compensoBulk);
+            Assert.isTrue(crudCompensoBP.isCigModificabile());
+        } catch (BusinessProcessException e) {
+        }
+    }
+
+
+    @Test
+    public void compensoSix() {
+        CRUDCompensoBP crudCompensoBP = new CRUDCompensoBP();
+        final CompensoBulk compensoBulk = new CompensoBulk();
+        final Tipo_trattamentoBulk tipo_trattamentoBulk = new Tipo_trattamentoBulk();
+        tipo_trattamentoBulk.setCd_trattamento("TEST");
+        tipo_trattamentoBulk.setTipoDebitoSiope(Tipo_trattamentoBulk.TIPO_DEBITO_COMMERCIALE);
+        compensoBulk.setTipoTrattamento(tipo_trattamentoBulk);
+
+        Obbligazione_scadenzarioBulk obbligazione_scadenzarioBulk = new Obbligazione_scadenzarioBulk();
+        ObbligazioneBulk obbligazioneBulk = new ObbligazioneBulk();
+        obbligazioneBulk.setPg_obbligazione(Long.valueOf(1));
+
+        obbligazione_scadenzarioBulk.setObbligazione(obbligazioneBulk);
+        compensoBulk.setObbligazioneScadenzario(obbligazione_scadenzarioBulk);
+
+        try {
+            crudCompensoBP.setModel(null, compensoBulk);
+            Assert.isTrue(crudCompensoBP.isCigModificabile());
+        } catch (BusinessProcessException e) {
+        }
+    }
+
+    @Test
+    public void compensoSeven() {
+        CRUDCompensoBP crudCompensoBP = new CRUDCompensoBP();
+        final CompensoBulk compensoBulk = new CompensoBulk();
+        final Tipo_trattamentoBulk tipo_trattamentoBulk = new Tipo_trattamentoBulk();
+        tipo_trattamentoBulk.setCd_trattamento("TEST");
+        tipo_trattamentoBulk.setTipoDebitoSiope(Tipo_trattamentoBulk.TIPO_DEBITO_COMMERCIALE);
+        compensoBulk.setTipoTrattamento(tipo_trattamentoBulk);
+
+        Obbligazione_scadenzarioBulk obbligazione_scadenzarioBulk = new Obbligazione_scadenzarioBulk();
+        ObbligazioneBulk obbligazioneBulk = new ObbligazioneBulk();
+        obbligazioneBulk.setPg_obbligazione(Long.valueOf(1));
+        obbligazioneBulk.getContratto().setPg_contratto(Long.valueOf(1));
+
+        final CigBulk cigBulk = new CigBulk();
+        cigBulk.setCdCig("");
+        obbligazioneBulk.getContratto().setCig(cigBulk);
+        obbligazione_scadenzarioBulk.setObbligazione(obbligazioneBulk);
+        compensoBulk.setObbligazioneScadenzario(obbligazione_scadenzarioBulk);
+
+        try {
+            crudCompensoBP.setModel(null, compensoBulk);
+            Assert.isTrue(!crudCompensoBP.isCigModificabile());
         } catch (BusinessProcessException e) {
         }
     }
