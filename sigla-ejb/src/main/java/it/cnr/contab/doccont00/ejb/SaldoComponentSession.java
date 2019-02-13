a869c6e7364e3b5932952dc3b320c92392c99f5a
raffaele.pagano <raffaele.pagano@cnr.it>
2019-02-13 11:12:59 +0100

    Piano Economico: anomalie

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/SaldoComponentSession.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/SaldoComponentSession.java
@@ -39,6 +39,6 @@ void checkDispPianoEconomicoProgetto(UserContext userContext, Pdg_variazioneBulk
 String getMessaggioSfondamentoPianoEconomico(UserContext userContext, Pdg_variazioneBulk pdgVariazione) throws ComponentException,java.rmi.RemoteException;
 void checkPdgPianoEconomico(UserContext userContext, Var_stanz_resBulk variazione) throws ComponentException,java.rmi.RemoteException;
 void checkPdgPianoEconomico(UserContext userContext, Pdg_variazioneBulk pdgVariazione) throws ComponentException,java.rmi.RemoteException;
-java.math.BigDecimal getStanziamentoAssestatoProgetto(it.cnr.jada.UserContext param0, ProgettoBulk param1, String param2, String param3) throws it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
+java.math.BigDecimal getStanziamentoAssestatoProgetto(it.cnr.jada.UserContext param0, ProgettoBulk param1, String param2, Integer param3, String param4) throws it.cnr.jada.comp.ComponentException,java.rmi.RemoteException;
 
 }
