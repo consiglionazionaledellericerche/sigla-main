e478bbf76808ceed86fadc776307c4663aee5eda
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2019-02-14 15:07:56 +0100

    Fix

---
+++ sigla-ws-client/src/main/java/it/cnr/contab/sdi/INonRecapitabile.java
@@ -0,0 +1,8 @@
+package it.cnr.contab.sdi;
+
+public interface INonRecapitabile {
+    public String getIdentificativoSdI();
+    public String getNote();
+    public String getMessageId();
+    public String getNomeFile();
+}
---
+++ sigla-ws-client/src/main/java/it/cnr/contab/sdi/IScarto.java
@@ -0,0 +1,8 @@
+package it.cnr.contab.sdi;
+
+public interface IScarto {
+    public String getIdentificativoSdI();
+    public String getMessageId();
+    public String getNomeFile();
+    public IListaErrori getListaErrori();
+}
