d6c21d49414de1f0f77d015d4e9406373a007360
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2019-02-14 15:07:56 +0100

    Fix

---
+++ sigla-ws-client/src/main/java/it/cnr/contab/sdi/IMancataConsegna.java
@@ -0,0 +1,10 @@
+package it.cnr.contab.sdi;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public interface IMancataConsegna {
+    public String getIdentificativoSdI();
+    public String getDescrizione();
+    public String getMessageId();
+    public String getNomeFile();
+}
