9c5871968eacddabc7bfb10f2803e2d019127a3f
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2019-02-14 15:04:48 +0100

    Fix bug lettura messaggi PEC fatture elettroniche

--- sigla-ws-client/src/main/java/it/cnr/contab/sdi/IRicevutaConsegna.java
+++ sigla-ws-client/src/main/java/it/cnr/contab/sdi/IRicevutaConsegna.java
@@ -1,6 +1,10 @@
 package it.cnr.contab.sdi;
 
+import javax.xml.datatype.XMLGregorianCalendar;
+
 public interface IRicevutaConsegna {
     public String getIdentificativoSdI();
     public String getMessageId();
+    public String getNomeFile();
+    public XMLGregorianCalendar getDataOraConsegna();
 }
