9ef57ea477683b72dcc5bd5596360c9f65cb027b
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2019-02-19 17:09:32 +0100

    Corretta generazione nota di credito automatica da SDI

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/AccertamentoComponentSessionBean.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/AccertamentoComponentSessionBean.java
@@ -267,6 +267,24 @@ public class AccertamentoComponentSessionBean extends it.cnr.jada.ejb.CRUDCompon
 			throw uncaughtError(param0,componentObj,e);
 		}
 	}
+	public it.cnr.contab.doccont00.core.bulk.IScadenzaDocumentoContabileBulk modificaScadenzaInAutomatico(it.cnr.jada.UserContext param0,it.cnr.contab.doccont00.core.bulk.IScadenzaDocumentoContabileBulk param1,java.math.BigDecimal param2,boolean param3, Boolean aggiornaCalcoloAutomatico) throws it.cnr.jada.comp.ComponentException,javax.ejb.EJBException {
+		pre_component_invocation(param0,componentObj);
+		try {
+			it.cnr.contab.doccont00.core.bulk.IScadenzaDocumentoContabileBulk result = ((AccertamentoComponent)componentObj).modificaScadenzaInAutomatico(param0,param1,param2,param3, aggiornaCalcoloAutomatico);
+			component_invocation_succes(param0,componentObj);
+			return result;
+		} catch(it.cnr.jada.comp.NoRollbackException e) {
+			component_invocation_succes(param0,componentObj);
+			throw e;
+		} catch(it.cnr.jada.comp.ComponentException e) {
+			component_invocation_failure(param0,componentObj);
+			throw e;
+		} catch(RuntimeException e) {
+			throw uncaughtRuntimeException(param0,componentObj,e);
+		} catch(Error e) {
+			throw uncaughtError(param0,componentObj,e);
+		}
+	}
 	public it.cnr.jada.bulk.OggettoBulk stampaConBulk(it.cnr.jada.UserContext param0,it.cnr.jada.bulk.OggettoBulk param1) throws it.cnr.jada.comp.ComponentException,javax.ejb.EJBException {
 		pre_component_invocation(param0,componentObj);
 		try {
