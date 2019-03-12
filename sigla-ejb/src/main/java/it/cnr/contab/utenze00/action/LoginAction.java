f3567ab184c3112b75cd40b806c37eff061d5c4b
marco.spasiano <marco.spasiano@cnr.it>
2019-03-12 19:52:42 +0100

    add log on Utente multiplo

--- sigla-ejb/src/main/java/it/cnr/contab/utenze00/action/LoginAction.java
+++ sigla-ejb/src/main/java/it/cnr/contab/utenze00/action/LoginAction.java
@@ -196,6 +196,13 @@ public class LoginAction extends it.cnr.jada.util.action.BulkAction {
             Forward forward = doLogin(context, GestioneLoginComponent.VALIDA_FASE_INIZIALE_UTENTE_MULTIPLO);
             if (forward == null)
                 forward = initializeWorkspace(context);
+            Optional.ofNullable(context)
+                    .filter(HttpActionContext.class::isInstance)
+                    .map(HttpActionContext.class::cast)
+                    .flatMap(httpActionContext -> Optional.ofNullable(httpActionContext.getParameter("main.utente_multiplo")))
+                    .ifPresent(s -> log.warn("Utente Multiplo User: {}", s));
+
+
             Optional.ofNullable(context.getBusinessProcess())
                     .filter(FormBP.class::isInstance)
                     .map(FormBP.class::cast)
@@ -229,7 +236,7 @@ public class LoginAction extends it.cnr.jada.util.action.BulkAction {
 
     /**
      * L'utente sta tentando di entrare senza autenticazione LDAP
-     * nonostante per lui sia obbligatorio, verifichiamo che ciè² sia ancora possibile
+     * nonostante per lui sia obbligatorio, verifichiamo che ciò sia ancora possibile
      *
      * @param context
      * @return
