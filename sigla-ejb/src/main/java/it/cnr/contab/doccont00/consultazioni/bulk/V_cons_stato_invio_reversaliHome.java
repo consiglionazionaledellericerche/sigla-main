584be7ba746354ef918032ceac1c9be8a7a9f21d
marco.spasiano <marco.spasiano@cnr.it>
2019-04-03 14:15:48 +0200

    Add consultazione non acquisiti

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/V_cons_stato_invio_reversaliHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/V_cons_stato_invio_reversaliHome.java
@@ -10,9 +10,11 @@ import it.cnr.contab.config00.bulk.Configurazione_cnrBulk;
 import it.cnr.contab.config00.bulk.Configurazione_cnrHome;
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativa_enteBulk;
 import it.cnr.contab.doccont00.core.bulk.Numerazione_doc_contBulk;
+import it.cnr.contab.doccont00.core.bulk.ReversaleBulk;
 import it.cnr.contab.doccont00.service.ContabiliService;
 import it.cnr.contab.service.SpringUtil;
 import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.contab.util.enumeration.EsitoOperazione;
 import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
 import it.cnr.jada.comp.ApplicationException;
@@ -20,6 +22,7 @@ import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.Persistent;
 import it.cnr.jada.persistency.PersistentCache;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.FindClause;
 import it.cnr.jada.persistency.sql.SQLBuilder;
 
 import javax.persistence.PersistenceException;
@@ -76,5 +79,32 @@ public class V_cons_stato_invio_reversaliHome extends BulkHome {
 		} catch (ApplicationException e) {			
 		}
 		return super.completeBulkRowByRow(userContext, persistent);		
-	}	
+	}
+
+
+	public SQLBuilder selectByClauseForNonAcqisiti(UserContext usercontext, V_cons_stato_invio_reversaliBulk v_cons_stato_invio_reversaliBulk,
+												   CompoundFindClause compoundfindclause) throws PersistencyException {
+		SQLBuilder sqlBuilder = createSQLBuilder();
+		if (compoundfindclause == null) {
+			if (v_cons_stato_invio_reversaliBulk != null)
+				compoundfindclause = v_cons_stato_invio_reversaliBulk.buildFindClauses(null);
+		} else {
+			compoundfindclause = CompoundFindClause.and(compoundfindclause, v_cons_stato_invio_reversaliBulk.buildFindClauses(Boolean.FALSE));
+		}
+		sqlBuilder.addClause(compoundfindclause);
+		Configurazione_cnrHome configurazione_cnrHome = Optional.ofNullable(getHomeCache().getHome(Configurazione_cnrBulk.class))
+				.filter(Configurazione_cnrHome.class::isInstance)
+				.map(Configurazione_cnrHome.class::cast)
+				.orElseThrow(() -> new PersistenceException("Home Configurazione_cnrHome non trovata!"));
+		Unita_organizzativa_enteBulk ente = (Unita_organizzativa_enteBulk) getHomeCache().getHome(Unita_organizzativa_enteBulk.class).findAll().get(0);
+		//	Se uo 999.000 in scrivania: visualizza tutto l'elenco e se uo speciale
+		if (!((CNRUserContext) usercontext).getCd_unita_organizzativa().equals( ente.getCd_unita_organizzativa()) &&
+				!configurazione_cnrHome.isUOSpecialeTuttaSAC(CNRUserContext.getCd_unita_organizzativa(usercontext))){
+			sqlBuilder.addSQLClause("AND","CD_CDS",SQLBuilder.EQUALS,CNRUserContext.getCd_cds(usercontext));
+		}
+		sqlBuilder.addClause(FindClause.AND, "esercizio", SQLBuilder.EQUALS, CNRUserContext.getEsercizio(usercontext));
+		sqlBuilder.addClause(FindClause.AND, "esitoOperazione", SQLBuilder.EQUALS, EsitoOperazione.NON_ACQUISITO.value());
+		sqlBuilder.addClause(FindClause.AND, "stato", SQLBuilder.NOT_EQUALS, ReversaleBulk.STATO_REVERSALE_ANNULLATO);
+		return sqlBuilder;
+	}
 }
