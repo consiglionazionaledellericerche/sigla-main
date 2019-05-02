131684b2ff126c86def4be2d20507d545566ae6c
marco.spasiano <marco.spasiano@cnr.it>
2019-05-02 18:06:51 +0200

    FIX seleziona contesto CDR null

--- sigla-ejb/src/main/java/it/cnr/contab/util/enumeration/StatoVariazioneSostituzione.java
+++ sigla-ejb/src/main/java/it/cnr/contab/util/enumeration/StatoVariazioneSostituzione.java
@@ -5,25 +5,16 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public enum EsitoOperazione {
-    ACQUISITO("ACQUISITO", "ACQUISITO"),
-    NON_ACQUISITO("NON ACQUISITO", "NON_ACQUISITO"),
+public enum StatoVariazioneSostituzione {
+    DA_VARIARE("DA VARIARE", "DA_VARIARE"),
+    VARIAZIONE_DEFINITIVA("VARIAZIONE DEFINITIVA", "VARIAZIONE_DEFINITIVA"),
+    VARIAZIONE_TRASMESSA("VARIAZIONE TRASMESSA", "VARIAZIONE_TRASMESSA"),
     VARIATO("VARIATO", "VARIATO"),
-    NON_VARIATO("NON VARIATO", "NON_VARIATO"),
-    ANNULLATO("ANNULLATO", "ANNULLATO"),
-    NON_ANNULLATO("NON ANNULLATO", "NON_ANNULLATO"),
-    SOSTITUITO("SOSTITUITO", "SOSTITUITO"),
-    NON_SOSTITUITO("NON SOSTITUITO", "NON_SOSTITUITO"),
-    RISCOSSO("RISCOSSO", "RISCOSSO"),
-    PAGATO("PAGATO", "PAGATO"),
-    STORNATO("STORNATO", "STORNATO"),
-    REGOLARIZZATO("REGOLARIZZATO", "REGOLARIZZATO"),
-    NON_REGOLARIZZATO("NON REGOLARIZZATO", "NON_REGOLARIZZATO"),
-    NON_ESEGUIBILE("NON ESEGUIBILE", "NON_ESEGUIBILE");
+    NON_VARIATO("NON VARIATO", "NON_VARIATO");
 
     private final String label, value;
 
-    private EsitoOperazione(String label, String value) {
+    StatoVariazioneSostituzione(String label, String value) {
         this.value = value;
         this.label = label;
     }
@@ -37,18 +28,18 @@ public enum EsitoOperazione {
     }
 
     public static String getValueFromLabel(String label) {
-        for (EsitoOperazione esito : EsitoOperazione.values()) {
+        for (StatoVariazioneSostituzione esito : StatoVariazioneSostituzione.values()) {
             if (esito.label.equals(label))
                 return esito.value;
         }
-        throw new IllegalArgumentException("Esito no found for label: " + label);
+        throw new IllegalArgumentException("Stato variazione sostituzione no found for label: " + label);
     }
 
-    public final static Map<String,String> KEYS = Arrays.asList(EsitoOperazione.values())
+    public final static Map<String,String> KEYS = Arrays.asList(StatoVariazioneSostituzione.values())
             .stream()
             .collect(Collectors.toMap(
-                    EsitoOperazione::value,
-                    EsitoOperazione::label,
+                    StatoVariazioneSostituzione::value,
+                    StatoVariazioneSostituzione::label,
                     (oldValue, newValue) -> oldValue,
                     Hashtable::new
             ));
