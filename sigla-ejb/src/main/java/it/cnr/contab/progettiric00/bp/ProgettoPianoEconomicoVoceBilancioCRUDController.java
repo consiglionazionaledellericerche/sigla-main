ccfbdd7ca49def333154478df8253ee58e1c91d8
raffaele.pagano <raffaele.pagano@cnr.it>
2019-05-17 13:18:03 +0200

    Aggiornamenti

--- sigla-ejb/src/main/java/it/cnr/contab/progettiric00/bp/ProgettoPianoEconomicoVoceBilancioCRUDController.java
+++ sigla-ejb/src/main/java/it/cnr/contab/progettiric00/bp/ProgettoPianoEconomicoVoceBilancioCRUDController.java
@@ -1,7 +1,11 @@
 package it.cnr.contab.progettiric00.bp;
 
+import java.math.BigDecimal;
 import java.rmi.RemoteException;
+import java.util.Optional;
 
+import it.cnr.contab.progettiric00.core.bulk.Ass_progetto_piaeco_voceBulk;
+import it.cnr.contab.progettiric00.core.bulk.ProgettoBulk;
 import it.cnr.contab.progettiric00.core.bulk.Progetto_piano_economicoBulk;
 import it.cnr.contab.util.Utility;
 import it.cnr.jada.action.ActionContext;
@@ -14,12 +18,23 @@ public class ProgettoPianoEconomicoVoceBilancioCRUDController extends SimpleProg
 		super(name, modelClass, listPropertyName, parent);
 	}
 
-	public void validateForDelete(ActionContext context, OggettoBulk detail) throws ValidationException
-	{
+	@Override
+	public void validateForDelete(ActionContext context, OggettoBulk detail) throws ValidationException {
 		super.validateForDelete(context, detail);
 
 		if (!detail.isToBeCreated()) {
 			try {
+				if (Optional.ofNullable(getParentController())
+						.flatMap(el->Optional.ofNullable(el.getParentController()))
+						.flatMap(el->Optional.ofNullable(el.getModel()))
+			            .filter(ProgettoBulk.class::isInstance)
+			            .map(ProgettoBulk.class::cast)
+			            .map(el -> el.isROProgettoForStato())
+			            .orElse(Boolean.FALSE))
+		        	Optional.ofNullable(detail).filter(Ass_progetto_piaeco_voceBulk.class::isInstance).map(Ass_progetto_piaeco_voceBulk.class::cast)
+		        	.filter(el->el.getSaldoSpesa().getAssestato().compareTo(BigDecimal.ZERO)==0)
+		        	.orElseThrow(()->new ValidationException("Non è possibile disassociare voci di bilancio movimentate. E' possibile solo tramite rimodulazione!"));
+
 				Utility.createProgettoRicercaComponentSession().validaCancellazioneVoceAssociataPianoEconomico(
 					context.getUserContext(),
 					(Progetto_piano_economicoBulk)getParentModel(),
