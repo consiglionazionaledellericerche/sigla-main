80b978eb9e611b9d18d2587f024e29c5d4bc312c
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2019-07-03 16:45:47 +0200

    Rilascio integrazione tra Flusso Acquisti e Contratti.

--- sigla-ejb/src/main/java/it/cnr/contab/config00/ejb/ContrattoComponentSessionBean.java
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/ejb/ContrattoComponentSessionBean.java
@@ -359,5 +359,21 @@ public class ContrattoComponentSessionBean extends it.cnr.jada.ejb.CRUDComponent
 			throw uncaughtError(param0,componentObj,e);
 		}
 	}	
-	
+	public ContrattoBulk creaContrattoDaFlussoAcquisti(UserContext userContext, ContrattoBulk contratto) throws it.cnr.jada.comp.ComponentException,java.rmi.RemoteException {
+		try {
+			ContrattoBulk result = ((ContrattoComponent)componentObj).creaContrattoDaFlussoAcquisti(userContext, contratto);
+			component_invocation_succes(userContext,componentObj);
+			return result;
+		} catch(it.cnr.jada.comp.NoRollbackException e) {
+			component_invocation_succes(userContext,componentObj);
+			throw e;
+		} catch(it.cnr.jada.comp.ComponentException e) {
+			component_invocation_failure(userContext,componentObj);
+			throw e;
+		} catch(RuntimeException e) {
+			throw uncaughtRuntimeException(userContext,componentObj,e);
+		} catch(Error e) {
+			throw uncaughtError(userContext,componentObj,e);
+		}
+	}	
 }
