5319495cdce794482acf442b45c2b4e6753ea3cb
marco.spasiano <marco.spasiano@cnr.it>
2020-01-16 18:20:45 +0100

    Add version REST service

---
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/VersionResource.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2020  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.web.rest.resource.util;
+
+import it.cnr.contab.util.Utility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+import java.util.stream.Collectors;
+
+@WebListener
+@Path("/version")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class VersionResource implements ServletContextListener {
+    public static final String IMPLEMENTATION_VERSION = "Implementation-Version";
+    public static final String SPECIFICATION_VERSION = "Specification-Version";
+    private static Map<Object, Object> ATTRIBUTES;
+    private final Logger logger = LoggerFactory.getLogger(VersionResource.class);
+
+    @GET
+    public Response get(HttpServletRequest request) throws Exception {
+        if (Optional.ofNullable(ATTRIBUTES).isPresent())
+            return Response.ok(ATTRIBUTES).build();
+        return Response.noContent().build();
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext ctx = servletContextEvent.getServletContext();
+        try {
+            Manifest manifest = new Manifest(ctx.getResourceAsStream(Utility.MANIFEST_PATH));
+            ATTRIBUTES = manifest.getMainAttributes().entrySet()
+                    .stream()
+                    .filter(objectObjectEntry -> {
+                        return Optional.ofNullable(objectObjectEntry.getKey())
+                                .filter(Attributes.Name.class::isInstance)
+                                .map(Attributes.Name.class::cast)
+                                .map(attribute -> attribute.equals(new Attributes.Name(IMPLEMENTATION_VERSION))
+                                        || attribute.equals(new Attributes.Name(SPECIFICATION_VERSION)))
+                                .orElse(Boolean.FALSE);
+                    })
+                    .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
+        } catch (IOException e) {
+            logger.warn("IOException", e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
