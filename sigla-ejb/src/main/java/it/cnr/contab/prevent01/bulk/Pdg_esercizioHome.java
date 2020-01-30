8c97430cd21542f1ef93d653455bd199d4ff9064
raffaele.pagano <raffaele.pagano@cnr.it>
2020-01-30 10:42:23 +0100

    Fix Rimodulazione Progetti: limitato il controllo di chiusura del bilancio, necessario per proporre rimodulazioni anche su voci economiche presenti in bilancio, ai soli CDS partecipanti al progetto oggetto di modifica.

--- sigla-ejb/src/main/java/it/cnr/contab/prevent01/bulk/Pdg_esercizioHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/prevent01/bulk/Pdg_esercizioHome.java
@@ -30,8 +30,12 @@ import it.cnr.jada.bulk.BulkHome;
 import it.cnr.jada.persistency.IntrospectionException;
 import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.FindClause;
 import it.cnr.jada.persistency.sql.PersistentHome;
 import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.sql.SQLException;
+
 public class Pdg_esercizioHome extends BulkHome {
 	public Pdg_esercizioHome(java.sql.Connection conn) {
 		super(Pdg_esercizioBulk.class, conn);
@@ -50,5 +54,25 @@ public class Pdg_esercizioHome extends BulkHome {
 	{
 		return (Pdg_esercizioBulk)findByPrimaryKey( new Pdg_esercizioKey( new Integer( esercizioCorrente.getEsercizio().intValue() + 1), esercizioCorrente.getCd_centro_responsabilita()));
 	
+	}
+
+	/**
+	 * Indica che tutti i CDR utilizzatori del progetto abbiano chiuso i piani di gestione
+	 *
+	 * @param esercizio esercizio di bilancio
+	 * @param pgProgetto identificativo del progetto
+	 * @return true se tutti i CDR utilizzatori del progetto abbiano chiuso i piani di gestione
+	 * @throws SQLException
+	 */
+	public boolean isAllPdgpProgettoChiusi(Integer esercizio, Integer pgProgetto) throws SQLException {
+		SQLBuilder sqlPdgEsercizio = this.createSQLBuilder();
+		sqlPdgEsercizio.addClause(FindClause.AND,"esercizio",SQLBuilder.EQUALS,esercizio);
+		sqlPdgEsercizio.addClause(FindClause.AND,"stato",SQLBuilder.NOT_EQUALS,Pdg_esercizioBulk.STATO_CHIUSURA_GESTIONALE_CDR);
+		sqlPdgEsercizio.addTableToHeader("PDG_MODULO");
+		sqlPdgEsercizio.addSQLJoin("PDG_ESERCIZIO.ESERCIZIO", "PDG_MODULO.ESERCIZIO");
+		sqlPdgEsercizio.addSQLJoin("PDG_ESERCIZIO.CD_CENTRO_RESPONSABILITA", "PDG_MODULO.CD_CENTRO_RESPONSABILITA");
+		sqlPdgEsercizio.addSQLClause(FindClause.AND,"PDG_MODULO.PG_PROGETTO",SQLBuilder.EQUALS,pgProgetto);
+
+		return !sqlPdgEsercizio.executeExistsQuery(getConnection());
 	}
 }
