f20eb2e52b9b7dd999106bd377eeaa96b05c6e1c
marco.spasiano <marco.spasiano@cnr.it>
2020-02-27 09:57:03 +0100

    FIX Test on Progetti

--- sigla-backend/src/main/resources/h2/function/ibmutl001_getdbrefreshdate.sql
+++ sigla-backend/src/main/resources/h2/function/ibmutl001_getdbrefreshdate.sql
@@ -1,6 +1,6 @@
 DROP ALIAS IF EXISTS ibmutl001_getdbrefreshdate;
 CREATE ALIAS ibmutl001_getdbrefreshdate AS $$
-java.util.Date execute() throws Exception {
+java.util.Date execute(java.sql.Connection connection) throws Exception {
   return new java.util.Date();
 }
 $$;
