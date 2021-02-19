1f48cdc92dee168856b334e9fbc7640d46fbdfe3
marco.spasiano <marco.spasiano@cnr.it>
2021-02-19 16:28:43 +0000

    changed softwareVersion and releaseDate [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ source_parsers = {
 }
 
 project = 'Sistema Informativo Gestione Linee di Attività'
-release = '6.2.101'
+release = '6.2.102'
 author = u'Consiglio Nazionale delle Ricerche'
 
 show_authors = True
