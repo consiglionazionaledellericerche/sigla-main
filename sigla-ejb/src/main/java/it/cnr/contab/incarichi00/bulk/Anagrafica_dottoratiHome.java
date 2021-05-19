3a46f0f28774c7d26e4ec5cec5713b254b154825
valerio.diego <valerio.diego@cnr.it>
2021-05-19 11:16:42 +0200

    autogenerate id per le 4 interfacce

--- sigla-ejb/src/main/java/it/cnr/contab/incarichi00/bulk/Anagrafica_dottoratiHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/incarichi00/bulk/Anagrafica_dottoratiHome.java
@@ -4,13 +4,32 @@
  */
 package it.cnr.contab.incarichi00.bulk;
 import java.sql.Connection;
+
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
 public class Anagrafica_dottoratiHome extends BulkHome {
+	private static final long serialVersionUID = 1L;
+
 	public Anagrafica_dottoratiHome(Connection conn) {
 		super(Anagrafica_dottoratiBulk.class, conn);
 	}
 	public Anagrafica_dottoratiHome(Connection conn, PersistentCache persistentCache) {
 		super(Anagrafica_dottoratiBulk.class, conn, persistentCache);
+	}
+
+	@Override
+	public void initializePrimaryKeyForInsert(UserContext usercontext, OggettoBulk oggettobulk)throws PersistencyException, ComponentException {
+		try {
+			Anagrafica_dottoratiBulk anagraficaDottorati = (Anagrafica_dottoratiBulk) oggettobulk;
+			anagraficaDottorati.setId(
+					new Long(((Long)findAndLockMax( oggettobulk, "id", new Long(0) )).longValue()+1));
+			super.initializePrimaryKeyForInsert(usercontext, anagraficaDottorati);
+		} catch(it.cnr.jada.bulk.BusyResourceException e) {
+			throw new PersistencyException(e);
+		}
 	}
 }
--- sigla-ejb/src/main/java/it/cnr/contab/incarichi00/bulk/Phdtipo_dottoratiHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/incarichi00/bulk/Phdtipo_dottoratiHome.java
@@ -4,13 +4,32 @@
  */
 package it.cnr.contab.incarichi00.bulk;
 import java.sql.Connection;
+
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
 public class Phdtipo_dottoratiHome extends BulkHome {
+	private static final long serialVersionUID = 1L;
+
 	public Phdtipo_dottoratiHome(Connection conn) {
 		super(Phdtipo_dottoratiBulk.class, conn);
 	}
 	public Phdtipo_dottoratiHome(Connection conn, PersistentCache persistentCache) {
 		super(Phdtipo_dottoratiBulk.class, conn, persistentCache);
+	}
+
+	@Override
+	public void initializePrimaryKeyForInsert(UserContext usercontext, OggettoBulk oggettobulk)throws PersistencyException, ComponentException {
+		try {
+			Phdtipo_dottoratiBulk phdtipoDottorati = (Phdtipo_dottoratiBulk) oggettobulk;
+			phdtipoDottorati.setId(
+					new Long(((Long)findAndLockMax( oggettobulk, "id", new Long(0) )).longValue()+1));
+			super.initializePrimaryKeyForInsert(usercontext, phdtipoDottorati);
+		} catch(it.cnr.jada.bulk.BusyResourceException e) {
+			throw new PersistencyException(e);
+		}
 	}
 }
