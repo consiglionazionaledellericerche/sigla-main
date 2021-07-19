6c8bd08263ca0b5b47ffdb20e1ba01febb777712
Marco Spasiano <marco.spasiano@cnr.it>
2021-07-19 16:18:45 +0200

    Update StringEncrypter.java

--- sigla-ejb/src/main/java/it/cnr/contab/util/StringEncrypter.java
+++ sigla-ejb/src/main/java/it/cnr/contab/util/StringEncrypter.java
@@ -99,15 +99,7 @@ public final class StringEncrypter  {
 			throw new EncryptionException( e );
 		}
 	}
-
-	public static void main(String[] args) {
-		try {
-			System.out.println(encrypt("isin-udg@legalmail.it", "GioISINGlo.7383"));
-		} catch (EncryptionException e) {
-			e.printStackTrace();
-		}
-	}
-
+	
 	public static String decrypt( String encryptionKey, String encryptedString ) throws EncryptionException
 	{
 		KeySpec keySpec = getKeySpec(Utility.lpad(encryptionKey, 24, '0'));
