fa22552e629c8009c91eaea1299ce9eb63cb3c36
valerio.diego <valerio.diego@cnr.it>
2021-09-29 13:38:11 +0200

    Sistemato problema cds e uo TODO: assegnazione delle sole modalitaPagamento disponibili e visione Banche

--- sigla-ejb/src/main/java/it/cnr/contab/incarichi00/bulk/ScadenzarioDottoratiRataBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/incarichi00/bulk/ScadenzarioDottoratiRataBulk.java
@@ -4,11 +4,38 @@
  */
 package it.cnr.contab.incarichi00.bulk;
 
+import it.cnr.contab.config00.sto.bulk.CdsBulk;
+import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
+
 public class ScadenzarioDottoratiRataBulk extends ScadenzarioDottoratiRataBase {
 	/**
 	 * [SCADENZARIO_DOTTORATI ]
 	 **/
 	private ScadenzarioDottoratiBulk scadenzarioDottorati =  new ScadenzarioDottoratiBulk();
+	private Unita_organizzativaBulk uo = new Unita_organizzativaBulk();
+
+
+	public Unita_organizzativaBulk getUo() {
+		return uo;
+	}
+
+	public void setUo(Unita_organizzativaBulk uo) {
+		this.uo = uo;
+	}
+
+
+
+	public CdsBulk getCds() {
+		return cds;
+	}
+
+	public void setCds(CdsBulk cds) {
+		this.cds = cds;
+	}
+
+	private CdsBulk cds = new CdsBulk();
+
+
 	/**
 	 * Created by BulkGenerator 2.0 [07/12/2009]
 	 * Table name: SCADENZARIO_DOTTORATI_RATA
