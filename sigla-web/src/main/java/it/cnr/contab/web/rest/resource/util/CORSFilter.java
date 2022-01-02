161821466d74ead7d6aec12e8a1ccf5946403179
marco.spasiano <marco.spasiano@cnr.it>
2022-01-02 10:56:04 +0100

    Add OPTIONS Method

--- sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/CORSFilter.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/CORSFilter.java
@@ -3,8 +3,9 @@ package it.cnr.contab.web.rest.resource.util;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 import java.io.IOException;
-
+@Provider
 public class CORSFilter implements ContainerResponseFilter {
 
     @Override
