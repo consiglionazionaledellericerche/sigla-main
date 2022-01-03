ca1e0953a8a619048a9f0e2f3c1f6541d5184810
marco.spasiano <marco.spasiano@cnr.it>
2022-01-03 19:32:26 +0100

    Add OPTIONS Method

--- sigla-web/src/main/java/it/cnr/contab/web/rest/config/SameSiteFilter.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/config/SameSiteFilter.java
@@ -8,8 +8,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
+@WebFilter
 public class SameSiteFilter implements javax.servlet.Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
