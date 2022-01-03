d8ff90634d77d1a14f0019dcc0334d7fcc3dfc8e
marco.spasiano <marco.spasiano@cnr.it>
2022-01-03 20:08:26 +0100

    Add OPTIONS Method

--- sigla-web/src/main/java/it/cnr/contab/web/rest/config/SameSiteFilter.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/config/SameSiteFilter.java
@@ -2,6 +2,7 @@ package it.cnr.contab.web.rest.config;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -10,18 +11,14 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.ext.Provider;
 
-@WebFilter("/restapi/login")
-public class SameSiteFilter implements javax.servlet.Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(request, response);
-        addSameSiteAttribute((HttpServletResponse) response); // add SameSite=strict cookie attribute
-    }
+@Provider
+public class SameSiteFilter implements ContainerResponseFilter {
 
     private void addSameSiteAttribute(HttpServletResponse response) {
         Collection<String> headers = response.getHeaders("Set-Cookie");
@@ -36,8 +33,10 @@ public class SameSiteFilter implements javax.servlet.Filter {
         }
     }
 
-    @Override
-    public void destroy() {
 
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        final Map<String, NewCookie> cookies = responseContext.getCookies();
+        cookies.size();
     }
 }
