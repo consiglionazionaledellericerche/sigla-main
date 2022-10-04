1ef6d5b44111575e8255c5ea1753325b2b01a05a
marco.spasiano <marco.spasiano@cnr.it>
2022-10-04 11:56:32 +0200

    New feature: Add Infinispan spring cache and REST srvice for functions tree

--- sigla-ejb/src/main/java/it/cnr/contab/config00/bulk/Configurazione_cnrBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/bulk/Configurazione_cnrBulk.java
@@ -186,6 +186,8 @@ public class Configurazione_cnrBulk extends Configurazione_cnrBase {
         super();
     }
 
+    public final static String PK_ESERCIZIO_SPECIALE = "ESERCIZIO_SPECIALE";
+    public final static String SK_ESERCIZIO_PARTENZA = "ESERCIZIO_PARTENZA";
     public Configurazione_cnrBulk(java.lang.String cd_chiave_primaria, java.lang.String cd_chiave_secondaria, java.lang.String cd_unita_funzionale, java.lang.Integer esercizio) {
         super(cd_chiave_primaria, cd_chiave_secondaria, cd_unita_funzionale, esercizio);
     }
---
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/resource/config00/ContextResource.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2022  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.web.rest.resource.config00;
+
+import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
+import it.cnr.contab.service.ContextService;
+import it.cnr.contab.service.SpringUtil;
+import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.contab.web.rest.local.config00.ContextLocal;
+import it.cnr.contab.web.rest.model.PreferitiDTOBulk;
+import it.cnr.contab.web.rest.resource.util.AbstractResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.util.Pair;
+
+import javax.ejb.Stateless;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Stateless
+public class ContextResource implements ContextLocal {
+    private final Logger LOGGER = LoggerFactory.getLogger(ContextResource.class);
+    @Context
+    SecurityContext securityContext;
+
+    @Override
+    public Response esercizi(HttpServletRequest request, String cds) throws Exception {
+        try {
+            CNRUserContext userContext = AbstractResource.getUserContext(securityContext, request);
+            final List<Integer> esercizi = SpringUtil.getBean(ContextService.class)
+                    .esercizi(userContext, userContext.getUser(), Optional.ofNullable(userContext.getCd_cds()).orElse(cds));
+            return Response.status(Response.Status.OK).entity(esercizi).build();
+        } catch (BadRequestException _ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+
+    @Override
+    public Response findUnitaOrganizzativeAbilitate(HttpServletRequest request, String cds) throws Exception {
+        try {
+            CNRUserContext userContext = AbstractResource.getUserContext(securityContext, request);
+            LOGGER.info("GET UO for User: {} CdS: {}", userContext.getUser(), cds);
+            final List<Pair<String, String>> result = SpringUtil.getBean(ContextService.class)
+                    .listaUnitaOrganizzativeAbilitate(userContext, userContext.getUser(), userContext.getEsercizio(), cds)
+                    .stream()
+                    .sorted((x,y) -> x.getCd_unita_organizzativa().compareTo(y.getCd_unita_organizzativa()))
+                    .map(x -> Pair.of(x.getCd_unita_organizzativa(), x.getDs_unita_organizzativa()))
+                    .distinct()
+                    .collect(Collectors.toList());
+            return Response.status(Response.Status.OK).entity(result).build();
+        } catch (BadRequestException _ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+
+    @Override
+    public Response findCdSAbilitati(HttpServletRequest request, String uo) throws Exception {
+        try {
+            CNRUserContext userContext = AbstractResource.getUserContext(securityContext, request);
+            LOGGER.info("GET CdS for User: {} UO: {}", userContext.getUser(), uo);
+            final List<Pair<String, String>> result = SpringUtil.getBean(ContextService.class)
+                    .listaCdSAbilitati(userContext, userContext.getUser(), userContext.getEsercizio(), uo)
+                    .stream()
+                    .sorted((x,y) -> x.getCd_unita_organizzativa().compareTo(y.getCd_unita_organizzativa()))
+                    .map(x -> Pair.of(x.getCd_unita_organizzativa(), Optional.ofNullable(x.getDs_unita_organizzativa()).orElse(x.getCd_unita_organizzativa())))
+                    .distinct()
+                    .collect(Collectors.toList());
+            return Response.status(Response.Status.OK).entity(result).build();
+        } catch (BadRequestException _ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+
+    @Override
+    public Response findCdR(HttpServletRequest request, String uo) throws Exception {
+        try {
+            CNRUserContext userContext = AbstractResource.getUserContext(securityContext, request);
+            LOGGER.info("GET CdR for User: {} UO: {}", userContext.getUser(), uo);
+            final List<Pair<String, String>> result = SpringUtil.getBean(ContextService.class)
+                    .listaCdR(userContext, userContext.getUser(), userContext.getEsercizio(), uo)
+                    .stream()
+                    .sorted((x,y) -> x.getCd_centro_responsabilita().compareTo(y.getCd_centro_responsabilita()))
+                    .map(x -> Pair.of(x.getCd_centro_responsabilita(), x.getDs_cdr()))
+                    .distinct()
+                    .collect(Collectors.toList());
+            return Response.status(Response.Status.OK).entity(result).build();
+        } catch (BadRequestException _ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+
+    @Override
+    public Response findPreferiti(HttpServletRequest request) throws Exception {
+        try {
+            CNRUserContext userContext = AbstractResource.getUserContext(securityContext, request);
+            LOGGER.info("GET Preferiti for User: {}", userContext.getUser());
+            return Response.status(Response.Status.OK).entity(
+                    SpringUtil.getBean(ContextService.class)
+                            .listaPreferiti(userContext)
+            ).build();
+        } catch (BadRequestException _ex) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+}
