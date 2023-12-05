9f09b9e555cda52b1021a5311e7e839ced457d35
marco.spasiano <marco.spasiano@cnr.it>
2023-12-05 16:37:59 +0100

    Aggiornamento della versione di jada alla 7.0.46

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/ConsConfrontoEntSpeComponentSession.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/ConsConfrontoEntSpeComponentSession.java
@@ -23,10 +23,11 @@ import javax.ejb.Remote;
 
 import it.cnr.jada.UserContext;
 import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.ejb.GenericComponentSession;
 import it.cnr.jada.persistency.IntrospectionException;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
 @Remote
-public interface ConsConfrontoEntSpeComponentSession	 {
+public interface ConsConfrontoEntSpeComponentSession extends GenericComponentSession {
 		it.cnr.jada.util.RemoteIterator findConsultazioneModulo(it.cnr.jada.UserContext param0,java.lang.String param1,java.lang.String param2,it.cnr.jada.persistency.sql.CompoundFindClause param3,it.cnr.jada.persistency.sql.CompoundFindClause param4) throws it.cnr.jada.comp.ComponentException, RemoteException, IntrospectionException;	
 }
 
--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/ConsSospesiEntSpeComponentSession.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/ejb/ConsSospesiEntSpeComponentSession.java
@@ -24,10 +24,11 @@ import javax.ejb.Remote;
 import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.ejb.GenericComponentSession;
 import it.cnr.jada.persistency.IntrospectionException;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
 @Remote
-public interface ConsSospesiEntSpeComponentSession	 {
+public interface ConsSospesiEntSpeComponentSession extends GenericComponentSession {
 	it.cnr.jada.util.RemoteIterator findConsSospesiSpesa(UserContext param0,String param1,String param2,CompoundFindClause param3,CompoundFindClause param4) throws ComponentException, RemoteException, IntrospectionException;
 	it.cnr.jada.util.RemoteIterator findConsSospesiEntrata(UserContext param0,String param1,String param2,CompoundFindClause param3,CompoundFindClause param4) throws ComponentException, RemoteException, IntrospectionException;
 	}
