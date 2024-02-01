a208bb2c99657368b5cd888fd499564ad30bc718
marco.spasiano <marco.spasiano@cnr.it>
2024-02-01 13:00:28 +0100

    Gestito errore in caso di variazione del CUP di un mandato pagato.

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/bp/CRUDMandatoVariazioneBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/bp/CRUDMandatoVariazioneBP.java
@@ -36,23 +36,6 @@ public class CRUDMandatoVariazioneBP extends CRUDMandatoBP {
         super(function + "Tr");
     }
 
-    public void setDaVariare(ActionContext actionContext) throws it.cnr.jada.action.BusinessProcessException {
-        final MandatoBulk mandatoBulk = Optional.ofNullable(getModel())
-                .filter(MandatoBulk.class::isInstance)
-                .map(MandatoBulk.class::cast)
-                .orElseThrow(() -> new BusinessProcessException("Mandato non trovato!"));
-
-        mandatoBulk.setStatoVarSos(StatoVariazioneSostituzione.DA_VARIARE.value());
-        mandatoBulk.setToBeUpdated();
-        try {
-            final OggettoBulk oggettoBulk = createComponentSession().modificaConBulk(actionContext.getUserContext(), mandatoBulk);
-            commitUserTransaction();
-            basicEdit(actionContext, oggettoBulk, true);
-        } catch (ComponentException | RemoteException e) {
-            throw handleException(e);
-        }
-    }
-
     @Override
     public boolean isInputReadonly() {
         return false;
