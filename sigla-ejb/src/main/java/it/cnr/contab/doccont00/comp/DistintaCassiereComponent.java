34ca72afac6e4b65f017c89bfe983c45ef9711bc
marco.spasiano <marco.spasiano@cnr.it>
2024-11-05 15:29:46 +0100

    BUG: in caso di pagamenti di fatture commerciali senza ritenute l'importo siope comunicato era sempre zero.

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/comp/DistintaCassiereComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/comp/DistintaCassiereComponent.java
@@ -6265,7 +6265,10 @@ public class DistintaCassiereComponent extends
 
     private BigDecimal calcolaImportoNettoSiope(boolean splitpayment, V_mandato_reversaleBulk vMandatoReversaleBulk, BigDecimal importoLordo) {
         if (splitpayment) {
-            if (importoLordo.equals(BigDecimal.ZERO) || vMandatoReversaleBulk.getIm_ritenute().equals(BigDecimal.ZERO)) {
+            if (vMandatoReversaleBulk.getIm_ritenute().equals(BigDecimal.ZERO)) {
+                return importoLordo;
+            }
+            if (importoLordo.equals(BigDecimal.ZERO)) {
                 return BigDecimal.ZERO;
             }
             final BigDecimal percentuale = importoLordo.multiply(BigDecimal.TEN.multiply(BigDecimal.TEN)).divide(vMandatoReversaleBulk.getIm_documento_cont(), 5, RoundingMode.HALF_UP);
